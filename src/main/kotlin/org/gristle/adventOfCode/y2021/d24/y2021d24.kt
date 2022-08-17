4c4004949378ab7a2a77d19a928227f4a0d69e91
Neil Banman <neil.banman@gmail.com>
2022-08-17 11:28:41 -0700

    Solution

--- src/main/kotlin/org/gristle/adventOfCode/y2021/d24/y2021d24.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2021/d24/y2021d24.kt
@@ -1,13 +1,60 @@
 package org.gristle.adventOfCode.y2021.d24
 
 import org.gristle.adventOfCode.utilities.*
+import java.util.LinkedList
+import kotlin.math.max
+import kotlin.math.min
 
 object Y2021D24 {
     private val input = readInput("y2021/d24")
+    
+    data class Step(val order: Int, val x: Int, val y: Int)
+    data class PairedSteps(val push: Step, val pop: Step)
+    
+    val steps = input
+        .chunked(18)
+        .mapIndexed { index, lines ->
+            val xInc = lines[5].substringAfterLast(" ").toInt()
+            val yInc = lines[15].substringAfterLast(" ").toInt()
+            Step(index, xInc, yInc)
+        }.let { steps -> 
+            val pairedSteps= mutableListOf<PairedSteps>()
+            val orderStack = LinkedList<Step>()
+            steps.forEach { step ->
+                if (step.x >= 0) {
+                    orderStack.push(step)
+                } else {
+                    pairedSteps.add(PairedSteps(orderStack.pop(), step))
+                }
+            }
+            pairedSteps
+        }.sortedBy { it.push.order }
 
-    fun part1() = "To be implemented"
+    fun solve(forMin: Boolean): String {
+        var z = 0L
+        var lastIncrease = 0
+        val modelNumber = MutableList(steps.size * 2) { 0 }
+        steps.forEach { step ->
+            val increaseZ = step.push.y + 26 * z
+            val pushMax = ((increaseZ + 9) % 26).toInt() 
+            val popMax = -step.pop.x + 9 
+            val intersection = if (forMin) {
+                max(pushMax, popMax) - 8
+            } else {
+                min(pushMax, popMax)
+            }
+            modelNumber[step.push.order] = 9 - (pushMax - intersection)
+            modelNumber[step.pop.order] = 9 - (popMax - intersection)
+            repeat(step.push.order - lastIncrease - 1) { z /= 26 }
+            z = increaseZ + modelNumber[step.push.order]
+            lastIncrease = step.push.order
+        }
+        
+        return modelNumber.joinToString("")
+    }
+    fun part1() = solve(false)
 
-    fun part2() = "To be implemented"
+    fun part2() = solve(true)
 }
 
 fun main() {
