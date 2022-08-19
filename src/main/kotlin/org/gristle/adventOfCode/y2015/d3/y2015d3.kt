d58e8dd3b44863e7a5368214d32bc75e20223be6
Neil Banman <neil.banman@gmail.com>
2022-08-19 13:57:37 -0700

    Use shared function for parsing the input and finding the next Coord.

--- src/main/kotlin/org/gristle/adventOfCode/y2015/d3/y2015d3.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2015/d3/y2015d3.kt
@@ -1,36 +1,28 @@
 package org.gristle.adventOfCode.y2015.d3
 
-import org.gristle.adventOfCode.utilities.*
+import org.gristle.adventOfCode.utilities.Coord
+import org.gristle.adventOfCode.utilities.elapsedTime
+import org.gristle.adventOfCode.utilities.readRawInput
 
 object Y2015D3 {
     private val input = readRawInput("y2015/d3")
 
+    fun Coord.move(c: Char) = when (c) {
+        '^' -> north()
+        '>' -> east()
+        'v' -> south()
+        else -> west()
+    }
+
     fun part1() = input.fold(mutableListOf(Coord(0, 0))) { acc, c ->
-        val last = acc.last()
-        acc.add(
-            when (c) {
-                '^' -> last.north()
-                '>' -> last.east()
-                'v' -> last.south()
-                else -> last.west()
-            }
-        )
-        acc
+        acc.apply { add(acc.last().move(c)) }
     }.toSet().size
 
     fun part2() = input
         .foldIndexed(mutableListOf(Coord(0, 0)) to mutableListOf(Coord(0, 0))) { index, acc, c ->
             val roboSanta = index % 2 == 1
             val listToAdd = if (roboSanta) acc.second else acc.first
-            val last = listToAdd.last()
-            listToAdd.add(
-                when (c) {
-                    '^' -> last.north()
-                    '>' -> last.east()
-                    'v' -> last.south()
-                    else -> last.west()
-                }
-            )
+            listToAdd.add(listToAdd.last().move(c))
             if (roboSanta) acc.first to listToAdd else listToAdd to acc.second
         }.let { it.first + it.second }
         .toSet()
