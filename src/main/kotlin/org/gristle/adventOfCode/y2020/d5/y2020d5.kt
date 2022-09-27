b8ecd704a6c4c2573a20184b47546d1999b13801
Neil Banman <neil.banman@gmail.com>
2022-09-27 10:09:11 -0700

    Removed unnecessary Ticket class, added comments.

--- src/main/kotlin/org/gristle/adventOfCode/y2020/d5/y2020d5.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2020/d5/y2020d5.kt
@@ -1,38 +1,33 @@
 package org.gristle.adventOfCode.y2020.d5
 
-import org.gristle.adventOfCode.utilities.*
+import org.gristle.adventOfCode.utilities.elapsedTime
+import org.gristle.adventOfCode.utilities.readInput
 
 object Y2020D5 {
     private val input = readInput("y2020/d5")
 
-    class Ticket(seatNumber: Int) {
-        private val row = seatNumber.shr(3)
-        private val column = seatNumber.and(7)
-        val seatId = row * 8 + column
-    }
-
-    private val tickets = input
+    // The seat id is Row * 8 + Column. Since there are 8 columns this means that the seatIDs are sequential from
+    // 0 to 1016, and the FBLR code is just a binary number with 'F' and 'L' meaning '0' and 'B' and 'R' meaning 1.
+    private val seatIds = input
         .map { line ->
-            val seatNumber = line.reversed().foldIndexed(0) { index, acc, c ->
+            line.foldIndexed(0) { index, acc, c ->
                 acc + when(c) {
                     'F' -> 0
                     'L' -> 0
-                    else -> 1.shl(index)
+                    else -> 1.shl(9 - index)
                 }
             }
-            Ticket(seatNumber)
-        }
+        }.sorted() // Pt1 needs the largest, and Pt2 needs a full sort, so just go ahead and sort the seatIds
 
-    fun part1() = tickets.maxOf { it.seatId }
+    fun part1() = seatIds.last() // Since sorted, this is the highest seat ID on a boarding pass.
 
-    fun part2(): Int {
-        val sortedIds = tickets.map { it.seatId }.sorted()
-        val offset = sortedIds.first()
-        for (i in sortedIds.indices) {
-            if (offset + i != sortedIds[i]) return sortedIds[i] - 1
-        }
-        return -1
-    }
+    // The seatIds should all be contiguous. Yours is missing, so look for the first non-contiguous seatId in the
+    // sorted list of seatIds. Yours would be the seatId immediately below that.
+    fun part2() = seatIds
+        .asSequence() // Convert to sequence..
+        .zipWithNext() // then convert sequence to include both the previous seatId and the next seatId
+        .first { (prev, next) -> prev + 1 != next } // find the first instance where the nextSeatId is not contiguous
+        .second - 1 // return that, minus 1 (since your missing ticket should be contiguous)
 }
 
 fun main() {
