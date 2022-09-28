0f11bebf818527abd7315721c98ef8119fd06b85
Neil Banman <neil.banman@gmail.com>
2022-09-28 16:10:31 -0700

    refactored, 3x speedup in part2

--- src/main/kotlin/org/gristle/adventOfCode/y2021/d12/y2021d12.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2021/d12/y2021d12.kt
@@ -1,63 +1,61 @@
 package org.gristle.adventOfCode.y2021.d12
 
-import org.gristle.adventOfCode.utilities.*
+import org.gristle.adventOfCode.utilities.elapsedTime
+import org.gristle.adventOfCode.utilities.readInput
 import java.util.*
 
 object Y2021D12 {
     private val input = readInput("y2021/d12")
+    
+    // build a map of edges from each location
+    private val edges = buildMap<String, MutableList<String>> { 
+        input
+            .map { it.split('-') }
+            .forEach { (a, b) ->
+                computeIfAbsent(a) { mutableListOf() }.add(b)
+                if (a != "start" && b != "end") computeIfAbsent(b) { mutableListOf() }.add(a)
+            }
+    } as Map<String, List<String>>
 
-    class Graph {
-        val edges = mutableMapOf<String, MutableList<String>>()
-
-        override fun toString(): String {
-            return "Graph(edges=$edges)"
-        }
-    }
-
-    val graph = Graph()
-
-    init {
-        input.map { it.split('-') }.forEach { ls ->
-            val a = ls.first()
-            val b = ls.last()
-            val edgesA = graph.edges.computeIfAbsent(a) { mutableListOf() }
-            val edgesB = graph.edges.computeIfAbsent(b) { mutableListOf() }
-            if (b != "start" && a != "end") edgesA.add(b)
-            if (a != "start" && b != "end") edgesB.add(a)
-        }
-    }
-
-    fun findPaths(allowOneExtra: Boolean = false): Int {
-        fun List<String>.noDoubleVisits(): Boolean {
-            return filter { it[0].isLowerCase() }
-                .let { it.distinct().size == it.size }
-        }
+    // Custom BFS that searches entire space, looking for number of possible paths rather than shortest path.
+    // Uses custom state object to efficiently conform with part2 navigation rules.
+    private fun findPaths(allowOneExtra: Boolean = false): Int {
+        
+        // Uses custom state object that tracks whether a small cave has been visited twice. Speeds up process
+        // 3x over previous method of checking for duplicates in the visited list.
+        class State(val visits: List<String>, val visitedTwice: Boolean = false)
 
         // bfs
         var count = 0
-        val q: Deque<List<String>> = LinkedList()
-        q.add(listOf("start"))
+        val q: Deque<State> = ArrayDeque()
+        q.add(State(listOf("start")))
         while (q.isNotEmpty()) {
             val v = q.poll()
-            if (v.last() == "end") {
-                count ++
+            if (v.visits.last() == "end") {
+                count++
+            } else {
+                val newPaths = edges
+                    .getValue(v.visits.last())
+                    .mapNotNull { edge ->
+                        when {
+                            edge[0].isUpperCase() || edge !in v.visits -> State(v.visits + edge, v.visitedTwice)
+                            allowOneExtra && !v.visitedTwice -> State(v.visits + edge, true)
+                            else -> null
+                        }
+                    }
+                q.addAll(newPaths)
             }
-            val n = graph.edges[v.last()]!!.filter { edge ->
-                edge[0].isUpperCase()
-                        || v.find { it == edge } == null
-                        || (allowOneExtra && v.noDoubleVisits())
-
-            }
-            val newPaths = n.map { v + it }
-            q.addAll(newPaths)
         }
         return count
     }
+    
+    fun part1() = findPaths()
+    fun part2() = findPaths(true)
 }
 
 fun main() {
     var time = System.nanoTime()
-    println("Part 1: ${Y2021D12.findPaths()} (${elapsedTime(time)}ms)") // 4104
+    println("Part 1: ${Y2021D12.part1()} (${elapsedTime(time)}ms)") // 4104 (61ms old and new)
     time = System.nanoTime()
-    println("Part 2: ${Y2021D12.findPaths(true)} (${elapsedTime(time)}ms)") // 119760
+    println("Part 2: ${Y2021D12.part2()} (${elapsedTime(time)}ms)") // 119760 (417ms old, 130ms new)
 }
