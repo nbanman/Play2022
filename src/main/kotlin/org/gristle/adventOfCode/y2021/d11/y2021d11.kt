a0d93ce8d8a61238f9956c0704a27f19ef1cfb18
Neil Banman <neil.banman@gmail.com>
2022-09-28 13:59:39 -0700

    Refactored to use sequences a la Todd Ginsburg (https://todd.ginsberg.com/post/advent-of-code/2021/day11/)

--- src/main/kotlin/org/gristle/adventOfCode/y2021/d11/y2021d11.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2021/d11/y2021d11.kt
@@ -5,52 +5,39 @@ import org.gristle.adventOfCode.utilities.*
 object Y2021D11 {
     private val input = readRawInput("y2021/d11")
 
-    val grid = input.toGrid().mapToGrid { Character.getNumericValue(it) }
+    private val grid = input.toGrid().mapToGrid { Character.getNumericValue(it) }
+    
+    private fun Grid<Int>.stepSequence(): Sequence<Int> = sequence {
+        // cave is mutable state internal to the sequence. The grid will keep updating each iteration.
+        val cave = toMutableGrid()
+        // outer loop runs forever. The yield turns this into a generator. Each time, it increases the energy
+        // level of each octopus, then runs an inner loop that handles the flashing, then yields the number of
+        // flashes
+        do {
+            cave.indices.forEach { index -> cave[index] = cave[index] + 1 } // increases energy level 
+            do { // inner loop to handle flashing
+                // gets indices of all octopi ready to flash
+                val flasherIndices = cave.withIndex().filter { it.value > 9 }.map { it.index } 
 
-    private var flashes = 0
-
-    private fun Grid<Int>.takeStep(): Grid<Int> {
-        fun Grid<Int>.flashIndices() = mapIndexedNotNull { index, i ->
-            if (i >= 10) index else null
-        }
-
-        val incGrid = mapToMutableGrid { it + 1 }
-        var flashIndices = incGrid.flashIndices()
-        while (flashIndices.isNotEmpty()) {
-            flashIndices.forEach { flashIndex ->
-                val unflashedNeighbors = incGrid
-                    .getNeighborIndices(flashIndex, true)
-                    .filter { incGrid[it] != -1 }
-                unflashedNeighbors.forEach { index ->
-                    incGrid[index]++
-                }
-                incGrid[flashIndex] = -1
-                flashes++
-            }
-            flashIndices = incGrid.flashIndices()
-        }
-        for (i in incGrid.indices) { incGrid[i] = incGrid[i].let { if (it == - 1) 0 else it } }
-        return incGrid
+                // for each flashing octopus, reset energy to 0
+                flasherIndices.forEach { index -> cave[index] = 0 }
+                
+                // for each flashing octopus, get its neighbors, filter out those that have already flashed,
+                // and add one to the energy
+                flasherIndices
+                    .flatMap { cave.getNeighborIndices(it, true) }
+                    .filter { cave[it] != 0 }
+                    .forEach { cave[it] = cave[it] + 1 }
+            } while (flasherIndices.isNotEmpty()) // run the inner loop until no more octopi flash
+            yield(cave.count { it == 0 }) // emit the number of octopi that flashed this turn
+        } while (true)
     }
+    
+    fun part1() = grid.stepSequence().take(100).sum() // sum up all the flashes that occured in 1st 100 turns
 
-    fun part1(): Int {
-        (1..100).fold(grid) { acc, _ ->
-            val next = acc.takeStep()
-            next
-        }
-        return flashes
-    }
-
-    fun part2(): Int {
-        var counter = 0
-        var inced = grid
-        while (inced.any { it != 0 } ) {
-            counter++
-            inced = inced.takeStep()
-        }
-        return counter
-    }
-
+    // look for the first turn where every octopus has flashed (ie, been reset). Get the index of that turn and 
+    // add one to compensate for zero-indexing.
+    fun part2() = grid.stepSequence().indexOfFirst { it == grid.size } + 1
 }
 
 fun main() {
