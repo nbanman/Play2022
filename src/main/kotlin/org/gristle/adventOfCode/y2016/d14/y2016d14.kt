9c551137955a1f75389ffd6339d99a4fbe6acb1e
Neil Banman <neil.banman@gmail.com>
2022-09-29 16:51:33 -0700

    fixed so that part2 returns correct answer!

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d14/y2016d14.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d14/y2016d14.kt
@@ -2,7 +2,6 @@ package org.gristle.adventOfCode.y2016.d14
 
 import org.gristle.adventOfCode.utilities.elapsedTime
 import org.gristle.adventOfCode.utilities.md5
-import org.gristle.adventOfCode.utilities.terminate
 import java.util.*
 
 // Refactored!
@@ -14,7 +13,7 @@ object Y2016D14 {
     private val threes = """([\da-f])\1{2}""".toRegex()
     private val fives = """([\da-f])\1{4}""".toRegex()
 
-    private data class Marker(val index: Int, val hash: String, val three: Char?, val five: List<Char>) {
+    private data class Marker(val index: Int, val hash: String, val three: Char?, val fives: List<Char>) {
         companion object {
             fun of(index: Int, hash: String): Marker {
                 val three = threes.find(hash)?.value?.get(0)
@@ -26,27 +25,33 @@ object Y2016D14 {
 
     fun solve(hashing: (String) -> String): Int {
         // rolling list of 1,000 hashes plus useful metadata
-        val hashes = LinkedList<Marker>()
+        val markers = ArrayDeque<Marker>(1000)
 
         // extension function that dumps a value if the hash exceeds 1,000
-        fun LinkedList<Marker>.addHash(marker: Marker) {
+        fun Deque<Marker>.addMarker(marker: Marker) {
+            if (size == 1000) poll()
             add(marker)
-            if (size == 1001) poll()
         }
 
         // set of cromulent hashes
         val counted = mutableSetOf<Marker>()
 
+        // this generates hashes for each index, storing them in a rolling list of 1,000 hashes. When fives are
+        // encountered, it looks through the list and adds the hashes with a corresponding three to the count set.
+        // The sequence continues for 1,000 iterations after the 64th cromulent hash is added. This is because 
+        // hashes do not get added sequentially, but when their corresponding five is called. However, after 1,000
+        // calls, there's no chance that it will ever be added because it's been rolled off.
         generateSequence(0) { it + 1 }
             .onEach { index ->
                 val marker = Marker.of(index, hashing(SALT + index))
-                marker.five.forEach { fiveChar ->
-                    counted.addAll(hashes.filter { it.three == fiveChar })
+                marker.fives.forEach { fiveChar ->
+                    counted.addAll(markers.filter { it.three == fiveChar })
                 }
-                hashes.addHash(marker)
-            }.terminate { counted.size >= 64 }
-
-        return counted.maxOf { it.index }
+                markers.addMarker(marker)
+            }.first { index -> counted.size >= 64 && index >= counted.elementAt(63).index + 1000 }
+        
+        // Sort by index, then return the index of the 64th hash.
+        return counted.sortedBy { it.index }[63].index
     }
 
     fun part1() = solve { it.md5() }
@@ -58,5 +63,5 @@ fun main() {
     var time = System.nanoTime()
     println("Part 1: ${Y2016D14.part1()} (${elapsedTime(time)}ms)") // 18626 (124ms)
     time = System.nanoTime()
-    println("Part 2: ${Y2016D14.part2()} (${elapsedTime(time)}ms)") // 20092 (11175ms)
+    println("Part 2: ${Y2016D14.part2()} (${elapsedTime(time)}ms)") // 20092 (11571ms)
 }
