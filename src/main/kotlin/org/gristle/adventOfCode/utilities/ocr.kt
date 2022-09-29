73615febc7c1fac61cf6ee2a269dfd3694015661
Neil Banman <neil.banman@gmail.com>
2022-09-29 12:10:12 -0700

    added comments

--- src/main/kotlin/org/gristle/adventOfCode/utilities/ocr.kt
+++ src/main/kotlin/org/gristle/adventOfCode/utilities/ocr.kt
@@ -1,5 +1,7 @@
 package org.gristle.adventOfCode.utilities
 
+// letterForms is each character rotated 90 degrees with newlines stripped out. The 90 degree rotation makes it
+// easier to parse letters because they can be easily separated. 
 private val letterForms = buildMap<String, Char> {
     // 6 height letters
     put("#####...#..#..#..######.", 'A')
@@ -38,16 +40,17 @@ private val letterForms = buildMap<String, Char> {
     put("###......##..#.....##...#....##....#...##.....#..##......###", 'Z')
 }
 
-fun Grid<Char>.ocr() = rotate90()
-    .addRight(Grid(width, 1) { '\n' })
-    .joinToString("")
-    .replace(' ', '.')
+fun Grid<Char>.ocr() = rotate90() // rotation makes for easy separation of each letter
+    .addRight(Grid(width, 1) { '\n' }) // newline helps maintain shape of data through string transforms
+    .joinToString("") // change into string to make replacements and splits easier
+    .replace(' ', '.') // conform spaces into dots
+    // Fixes bug where some 'Y' characters do not have a space between it and the next letter
     .replace("....##\n...#..\n###...\n...#..\n....##", "....##\n...#..\n###...\n...#..\n....##\n......\n")
-    .split("""^(\.{6,}\n)+""".toRegex(RegexOption.MULTILINE))
-    .filter { it.isNotBlank() }
-    .map { it.replace("\n", "") }
-    .map { letterForms[it] ?: '?' }
-    .joinToString("")
+    .split("""^(\.{6,}\n)+""".toRegex(RegexOption.MULTILINE)) // split each letter up into its own string
+    .filter { it.isNotBlank() } // remove blank strings (found at end)
+    .map { it.replace("\n", "") } // newlines not needed at this point
+    .map { letterForms[it] ?: '?' } // map to Char
+    .joinToString("") // put all Chars together into a String
 
 @JvmName("ocrBoolean")
 fun Grid<Boolean>.ocr() = mapToGrid { if (it) '#' else '.' }.ocr() 
