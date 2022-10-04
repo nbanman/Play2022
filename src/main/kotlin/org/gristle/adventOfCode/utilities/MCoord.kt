89bf94d29652382847a990691ef77f09e98815fd
Neil Banman <neil.banman@gmail.com>
2022-10-04 14:03:36 -0700

    added operator functions to Xyz class

--- src/main/kotlin/org/gristle/adventOfCode/utilities/MCoord.kt
+++ src/main/kotlin/org/gristle/adventOfCode/utilities/MCoord.kt
@@ -24,7 +24,7 @@ open class MCoord(val coordinates: List<Int>) {
 
     operator fun unaryMinus() = MCoord(coordinates.map { -it  })
 
-    private fun operate(other: MCoord, identity: Int, operation: (Int, Int) -> Int): MCoord {
+    fun operate(other: MCoord, identity: Int, operation: (Int, Int) -> Int): MCoord {
         val (larger, smaller) = if (dimensions > other.dimensions) {
             this.coordinates to other.coordinates
         } else {
@@ -85,6 +85,28 @@ class Xyz(x: Int, y: Int, z: Int) : MCoord(listOf(x, y, z)) {
     val x = coordinates[0]
     val y = coordinates[1]
     val z = coordinates[2]
+
+    fun operate(other: Xyz, identity: Int, operation: (Int, Int) -> Int): Xyz {
+        val (larger, smaller) = if (dimensions > other.dimensions) {
+            this.coordinates to other.coordinates
+        } else {
+            other.coordinates to this.coordinates
+        }
+        val (x, y, z) = (this.coordinates zip other.coordinates)
+            .map { operation(it.first, it.second) } +
+                larger.drop(smaller.size).map { identity * it }
+        return Xyz(x, y, z)
+    }
+
+    operator fun plus(other: Xyz) = operate(other, 1) { a, b -> a + b }
+
+    operator fun minus(other: Xyz) = operate(other, -1) { a, b -> a - b }
+
+    operator fun times(other: Xyz) = operate(other, 0) { a, b -> a * b }
+
+    operator fun div(other: Xyz) = operate(other, 0) { a, b -> a / b }
+
+    operator fun rem(other: Xyz) = operate(other, 0) { a, b -> a % b }
 
     override fun toString(): String {
         return "Xyz(x=$x, y=$y, z=$z)"
