e5656617138c17351f00ce0bc67a9c202fe097cd
Neil Banman <neil.banman@gmail.com>
2022-10-06 16:47:26 -0700

    refactored to share repeated code; added comments

--- src/main/kotlin/org/gristle/adventOfCode/y2020/d10/Y2020D10.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2020/d10/Y2020D10.kt
@@ -4,33 +4,34 @@ import org.gristle.adventOfCode.utilities.elapsedTime
 import org.gristle.adventOfCode.utilities.readRawInput
 
 class Y2020D10(input: String) {
+    // parse adapters from outlet, sort from lowest rating
     private val adapters = input.lines().map { it.toInt() }.sorted()
+
+    // include charging outlet and end device
     private val allDevices = listOf(0) + adapters + (adapters.last() + 3)
 
-    fun part1() = allDevices.drop(1).foldIndexed(Triple(0, 0, 0)) { index, acc, i ->
-        when {
-            i - allDevices[index] == 3 -> Triple(acc.first, acc.second, acc.third + 1)
-            i - allDevices[index] == 1 -> Triple(acc.first + 1, acc.second, acc.third)
-            else -> Triple(acc.first, acc.second + 1, acc.third)
-        }
-    }.let { it.first * it.third }
+    // get list of joltage differences between devices
+    private val differences = allDevices
+        .zipWithNext() // create previous/next pairs
+        .map { (prev, next) -> next - prev } // map delta from prev device
 
-    fun part2(): Long {
-        return allDevices
-            .drop(1) // start from second device (first adapter)
-            .mapIndexed { index, i -> i - allDevices[index] } // map delta from prev device
-            .joinToString("") // join them all up before splitting in a different way
-            .split('3') // both devices 3 apart must be in chain so don't permute
-            .map { // each string of 1s represents devices one away from each other.
-                // the last '1' is required though b/c it's 3 away from the next device
-                when (it.length) { // convert to number of permutations for the optional devices
-                    4 -> 7
-                    3 -> 4
-                    2 -> 2
-                    else -> 1
-                }
-            }.fold(1L) { acc, i -> acc * i } // multiply them by each other
-    }
+    fun part1() = differences
+        .count { it == 3 } // count number of 3-jolt differences
+        .let { jolt3s -> jolt3s * (differences.size - jolt3s) } // multiply # of 3-jolt diffs w/# of 1-jolt diffs
+
+    fun part2() = differences
+        .joinToString("") // join differences to one string before splitting in a different way
+        .split('3') // both devices 3 apart must be in chain so don't permute
+        .map { // each string of 1s represents devices one away from each other.
+            // the last '1' in string is required though b/c it's 3 away from the next device
+            when (it.length) { // convert to number of permutations for the optional devices
+                4 -> 7 // 1111, 1101, 1011, 1001, 0111, 0101, 0011 (0001 not allowed b/c that's 4 apart)
+                3 -> 4 // 111, 101, 110, 001  
+                2 -> 2 // 11, 01
+                else -> 1 // 1
+            }
+        }.fold(1L) { acc, i -> acc * i } // multiply them by each other
+
 }
 
 fun main() {
