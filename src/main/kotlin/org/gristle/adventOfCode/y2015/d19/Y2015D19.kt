0a82f70bc54fa86fe4fffc1cb59eaf558cd11d33
Neil Banman <neil.banman@gmail.com>
2022-10-11 10:08:21 -0700

    replace for...break pattern with first {} pattern

--- src/main/kotlin/org/gristle/adventOfCode/y2015/d19/Y2015D19.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2015/d19/Y2015D19.kt
@@ -4,52 +4,50 @@ import org.gristle.adventOfCode.utilities.elapsedTime
 import org.gristle.adventOfCode.utilities.groupValues
 import org.gristle.adventOfCode.utilities.readRawInput
 
-class Y2015D19(input: String) {
+class Y2015D19(private val input: String) {
     data class Rule(val element: String, val replacement: String) {
         val size = replacement.count { it in 'A'..'Z' }
     }
 
-    private val work = input
     private val moleculePattern = """^[A-z]+${'$'}""".toRegex(RegexOption.MULTILINE)
-    private val molecule = moleculePattern.find(work)?.value
+    private val molecule = moleculePattern.find(input)?.value
         ?: throw Exception("No molecule found in input string")
 
     private val rulePattern = """([A-Z][a-z]?) => (\w+)""".toRegex()
-    private val rules = work
+    private val rules = input
         .groupValues(rulePattern)
         .map { Rule(it[0], it[1]) }
         .sortedByDescending { it.size }
 
-    private val ePattern = """e => (\w+)""".toRegex()
-    private val starts = work
-        .groupValues(ePattern)
-        .map { it[0] }
+    fun part1(): Int {
+        val elements = """[A-Z][a-z]?"""
+            .toRegex()
+            .findAll(molecule)
+            .toList()
 
-    private val elements = """[A-Z][a-z]?"""
-        .toRegex()
-        .findAll(molecule)
-        .toList()
+        return elements
+            .flatMap { mr ->
+                rules
+                    .filter { it.element == mr.value }
+                    .map { rule ->
+                        molecule.substring(0, mr.range.first) + rule.replacement + molecule.substring(mr.range.last + 1)
+                    }
+            }.distinct()
+            .size
+    }
 
-    private val part1Mutations = elements.flatMap { mr ->
-        rules
-            .filter { it.element == mr.value }
-            .map { rule ->
-                molecule.substring(0, mr.range.first) + rule.replacement + molecule.substring(mr.range.last + 1)
-            }
-    }.distinct()
+    fun part2(): Int {
+        val elementPattern = """e => (\w+)""".toRegex()
+        val starts = input
+            .groupValues(elementPattern)
+            .map { it[0] }
 
-    fun part1() = part1Mutations.size
-
-    fun part2() = generateSequence(molecule) {
-        var next = ""
-        for (rule in rules) {
-            if (it.contains(rule.replacement)) {
-                next = it.replaceFirst(rule.replacement, rule.element)
-                break
-            }
-        }
-        next
-    }.indexOfFirst { starts.contains(it) } + 1
+        return generateSequence(molecule) { molecule ->
+            rules
+                .first { rule -> molecule.contains(rule.replacement) } // find first applicable rule
+                .let { rule -> molecule.replaceFirst(rule.replacement, rule.element) } // apply rule to molecule; yield
+        }.indexOfFirst { starts.contains(it) } + 1
+    }
 }
 
 fun main() {
