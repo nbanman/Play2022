64993a003d1da7cd3f7d72ff3c6479a26eca54c9
Neil Banman <neil.banman@gmail.com>
2022-10-11 23:04:17 -0700

    refactor to sequence-based approach

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d20/Y2016D20.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d20/Y2016D20.kt
@@ -3,39 +3,31 @@ package org.gristle.adventOfCode.y2016.d20
 import org.gristle.adventOfCode.utilities.elapsedTime
 import org.gristle.adventOfCode.utilities.groupValues
 import org.gristle.adventOfCode.utilities.readRawInput
+import kotlin.math.max
 
-// Not refactored; ugly but fast
 class Y2016D20(input: String) {
 
-    data class LRange(val start: Long, val end: Long)
+    // parse input into ranges and sort the ranges by the start of the range. Using Long instead of UInt to 
+    // avoid overflow issues.
+    private val ranges = input
+        .groupValues("""(\d+)-(\d+)""", String::toLong)
+        .map { LongRange(it[0], it[1]) }
+        .sortedBy(LongRange::first)
 
-    val p1 = input
-        .groupValues("""(\d+)-(\d+)""")
-        .map { LRange(it[0].toLong(), it[1].toLong()) }
-        .sortedBy { it.start }
-
-    fun part1(): Long {
-        var push = 0L
-        for (range in p1) {
-            if (push < range.start) {
-                return push
-            }
-            push = maxOf(push, range.end + 1L)
+    // delivers a sequence of ips not blocked by the whitelist by starting at 0
+    private val ipSequence = sequence {
+        var ip = 0L // start at zero
+        ranges.forEach { range -> // for each range...
+            (ip until range.first).forEach { yield(it) } // yield all values b/t ip and range.first
+            ip = max(ip, range.last + 1) // ip cannot be w/in range so bump ip to the end of the range + 1
         }
-        return -1
+        // if last range did not go to 4294967295, this would continue to yield numbers until that max was reached
+        yieldAll(LongRange(ip, UInt.MAX_VALUE.toLong()))
     }
 
-    fun part2(): Long {
-        var push = 0L
-        var ips = 0L
-        for (range in p1) {
-            if (push < range.start) {
-                ips += range.start - push
-            }
-            push = maxOf(push, range.end + 1L)
-        }
-        return ips
-    }
+    fun part1() = ipSequence.first() // gets first (i.e., lowest) unblocked ip
+
+    fun part2() = ipSequence.count() // counts total unblocked ips
 }
 
 fun main() {
