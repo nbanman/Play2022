b0812f017ad16f5a88e126d1041a056d11c85bd4
Neil Banman <neil.banman@gmail.com>
2022-10-12 14:12:35 -0700

    tweaks

--- src/main/kotlin/org/gristle/adventOfCode/y2017/d16/Y2017D16.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2017/d16/Y2017D16.kt
@@ -7,26 +7,29 @@ import org.gristle.adventOfCode.utilities.readRawInput
 // not refactored!
 class Y2017D16(input: String) {
 
-    private data class Com(val name: String, val arg1: String, val arg2: String) {
+    private data class Command(val name: String, val arg1: String, val arg2: String) {
         fun execute(line: String): String {
-            return when(name) {
+            return when (name) {
                 "s" -> {
                     val split = line.length - arg1.toInt()
                     line.substring(split) + line.substring(0, split)
                 }
+
                 "x" -> {
                     exchange(line, arg1.toInt(), arg2.toInt())
                 }
+
                 "p" -> {
                     val indexA = line.indexOf(arg1)
                     val indexB = line.indexOf(arg2)
                     exchange(line, indexA, indexB)
                 }
-                else -> { line }
+
+                else -> throw IllegalArgumentException("Bad input: $name")
             }
         }
 
-        fun exchange(line: String, indexA: Int, indexB: Int): String {
+        private fun exchange(line: String, indexA: Int, indexB: Int): String {
             return line
                 .replace(line[indexA], 'z')
                 .replace(line[indexB], line[indexA])
@@ -35,27 +38,25 @@ class Y2017D16(input: String) {
     }
 
     private fun danceParty(start: String) =
-        commands.fold(start) { acc, command ->
-            command.execute(acc)
-        }
+        commands.fold(start) { acc, command -> command.execute(acc) }
 
     private val pattern = """([psx])([a-p\d]+)(?:/([a-p\d]+))?""".toRegex()
     private val commands = input
         .groupValues(pattern)
-        .map { Com(it[0], it[1], it[2]) }
+        .map { Command(it[0], it[1], it[2]) }
 
     private val start = "abcdefghijklmnop"
-    private val p1 = danceParty(start)
+    private val part1 = danceParty(start)
 
-    fun part1() = p1
+    fun part1() = part1
 
     fun part2(): String {
-        val register = mutableListOf(start, p1)
+        val register = mutableSetOf(start, part1)
         while (true) {
             val new = danceParty(register.last())
             if (new in register) break else register.add(new)
         }
-        return register[1_000_000_000 % register.size]
+        return register.elementAt(1_000_000_000 % register.size)
     }
 }
 
