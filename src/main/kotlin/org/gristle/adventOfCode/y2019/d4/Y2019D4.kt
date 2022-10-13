684a8ca099a23b8b88df976d7fb748126222574d
Neil Banman <neil.banman@gmail.com>
2022-10-13 16:37:41 -0700

    tweaks

--- src/main/kotlin/org/gristle/adventOfCode/y2019/d4/Y2019D4.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2019/d4/Y2019D4.kt
@@ -5,29 +5,27 @@ import org.gristle.adventOfCode.utilities.readRawInput
 
 class Y2019D4(input: String) {
 
-    private val range = input.split('-').let { it[0].toInt()..it[1].toInt() }
+    private val range = input
+        .split('-')
+        .map(String::toInt)
+        .let { it[0]..it[1] }
 
-    private inline fun CharSequence.allIndexed(predicate: (index: Int, Char) -> Boolean): Boolean {
-        forEachIndexed { index, c -> if (!predicate(index, c)) return false }
-        return true
+    private val part1Passwords = range
+        .filter { password ->
+            val passString = password.toString()
+            val zippedPassString = passString.zipWithNext()
+            zippedPassString.all { it.first <= it.second }
+                    && zippedPassString.any { it.first == it.second }
+        }.map(Int::toString)
+
+    fun part1() = part1Passwords.size
+
+    fun part2(): Int {
+        val pattern = Regex("""(\d)\1+""")
+        return part1Passwords
+            .filter { password -> pattern.findAll(password).any { it.value.length == 2 } }
+            .size
     }
-
-    fun part1() = range.count { password ->
-        val passString = password.toString()
-        val zippedPassString = passString.zipWithNext()
-        passString.allIndexed { index, c -> index == 0 || passString[index - 1] <= c }
-                && zippedPassString.all { it.first <= it.second }
-                && zippedPassString.any { it.first == it.second }
-    }
-
-    val pattern = Regex("""(\d)\1+""")
-
-    fun part2() = range.count { password ->
-        val passString = password.toString()
-        passString.allIndexed { index, c -> index == 0 || passString[index - 1] <= c }
-                && pattern.findAll(passString).any { it.value.length == 2 }
-    }
-
 }
 
 fun main() {
