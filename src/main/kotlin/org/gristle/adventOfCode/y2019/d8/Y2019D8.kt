27efa5bdc272f2bf6f0d11a5599ba3623a9d26ec
Neil Banman <neil.banman@gmail.com>
2022-10-14 14:18:52 -0700

    tweaks

--- src/main/kotlin/org/gristle/adventOfCode/y2019/d8/Y2019D8.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2019/d8/Y2019D8.kt
@@ -5,25 +5,25 @@ import org.gristle.adventOfCode.utilities.elapsedTime
 import org.gristle.adventOfCode.utilities.ocr
 import org.gristle.adventOfCode.utilities.readRawInput
 
+private typealias Layer = String
 class Y2019D8(input: String) {
     private val width = 25
     private val height = 6
 
     private val layers = input.chunked(width * height) // Split the data into a list of equal sized layers
 
-    fun part1(): Int? {
-        return layers
-            .minByOrNull { layer -> layer.count { it == '0' } }  // Get the layer with the fewest 0s
-            ?.let { layer -> layer.count { it == '1' } * layer.count { it == '2' } } // Get product of # of 1s and 2s
-    }
+    fun part1() = layers
+        .minByOrNull { layer -> layer.count { it == '0' } }  // Get the layer with the fewest 0s
+        ?.let { layer -> layer.count { it == '1' } * layer.count { it == '2' } } // Get product of # of 1s and 2s
+        ?: throw IllegalArgumentException("'layers' has no elements")
 
     fun part2(): String {
         // Takes the layers and an index value representing a specific pixel. For that index, it looks
         // through the layers sequentially and finds the first non-transparent pixel (i.e., it ignores '0's).
         // Then it returns '#' for white pixels ('1') and ' ' for black pixels ('0').
-        fun List<String>.visiblePixel(index: Int) = if (first { it[index] != '2' }[index] == '1') '#' else ' '
+        fun List<Layer>.visiblePixel(index: Int): Char = if (first { it[index] != '2' }[index] == '1') '#' else ' '
 
-        // Make a representing the visible pixels of the image. Then run OCR to turn the grid into a
+        // Make a Grid representing the visible pixels of the image. Then run OCR to turn the grid into a
         // string of letters.
         return Grid(width, height) { i -> layers.visiblePixel(i) }.ocr()
     }
