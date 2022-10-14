1c3420bbbef975b272a6f5b669d373945e3c034a
Neil Banman <neil.banman@gmail.com>
2022-10-14 10:15:18 -0700

    tweaks and comments

--- src/main/kotlin/org/gristle/adventOfCode/y2019/d8/Y2019D8.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2019/d8/Y2019D8.kt
@@ -1,9 +1,9 @@
 package org.gristle.adventOfCode.y2019.d8
 
+import org.gristle.adventOfCode.utilities.Grid
 import org.gristle.adventOfCode.utilities.elapsedTime
 import org.gristle.adventOfCode.utilities.ocr
 import org.gristle.adventOfCode.utilities.readRawInput
-import org.gristle.adventOfCode.utilities.toGrid
 
 class Y2019D8(input: String) {
 
@@ -12,16 +12,18 @@ class Y2019D8(input: String) {
     fun part1(): Int? {
         return layers
             .minByOrNull { layer -> layer.count { it == '0' } }  // Get the layer with the fewest 0s
-            ?.let { layer -> layer.count { it == '1' } * layer.count { it == '2' } } // Get the product of # of 1s and 2s
+            ?.let { layer -> layer.count { it == '1' } * layer.count { it == '2' } } // Get product of # of 1s and 2s
     }
 
     fun part2(): String {
-        // Make a list the same size as a layer. In each entry, grab the values in each of the layers at that index and
-        // use the first non-transparent value.
-        return layers[0]
-            .mapIndexed { i, _ -> layers.map { it[i] }.first { it != '2' } == '1' }
-            .toGrid(25)
-            .ocr()
+        // Takes the layers and an index value representing a specific pixel. For that index, it looks
+        // through the layers sequentially and finds the first non-transparent pixel (i.e., it ignores '0's).
+        // Then it returns '#' for white pixels ('1') and ' ' for black pixels ('0').
+        fun List<String>.visiblePixel(index: Int) = if (first { it[index] != '2' }[index] == '1') '#' else ' '
+
+        // Make a representing the visible pixels of the image. Then run OCR to turn the grid into a
+        // string of letters.
+        return Grid(25 * 6, 25) { i -> layers.visiblePixel(i) }.ocr()
     }
 }
 
