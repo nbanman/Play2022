14ad404443e983d752643c8f38ef91e3cb4b4130
Neil Banman <neil.banman@gmail.com>
2022-10-15 11:28:45 -0700

    represent the cavern as a custom object backed by a mutable map that lazily generates terrain values as needed, instead of previous Grid that relied on arbitrary padding to make sure A* had room to explore. Slower but more elegant!

--- src/main/kotlin/org/gristle/adventOfCode/y2018/d22/Y2018D22.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2018/d22/Y2018D22.kt
@@ -10,46 +10,59 @@ class Y2018D22(input: String) {
     private val gv = input.groupValues(pattern, String::toInt).first()
 
     private val depth = gv[0]
+    private val start = Coord.ORIGIN
     private val target = Coord(gv[1], gv[2])
 
-    // the cavern size has some padding so that an explorer can "overshoot" the target and head back if that ends
-    // up being faster due to circumventing difficult terrain patterns.
-    private val width = target.x + 201
-    private val height = target.y + 201
-
-    // obtains geologic index value from the cavern index and the cavern itself
-    private fun geoIndex(coord: Coord, cave: Grid<Long>) = when {
-        coord == target || coord == Coord.ORIGIN -> 0L
-        coord.y == 0 -> coord.x * 16807L
-        coord.x == 0 -> coord.y * 48271L
-        else -> cave[coord.west()].erosionLevel() * cave[coord.north()].erosionLevel()
-    }
-
-    // helper function used to convert geologic index to erosion level
-    private fun Long.erosionLevel() = (this + depth) % 20183
-
     // the type of terrain found in each location of the cavern
-    enum class CavernType { ROCKY, WET, NARROW }
+    enum class Terrain { ROCKY, WET, NARROW }
 
-    // cavern is the representation of the terrain in the cavern. First a mutable grid is created, then the 
-    // apply block fills in the geologic index. You can't do it in a constructor because apart from some defaults
-    // each spot is dependent in a bootstrapping fashion on the previously calculated  geologic indexes of spots 
-    // to the north and west. Afterwards, the grid is mapped to CavernType by converting the geoIndexes to erosion 
-    // levels and modding 3.
-    private val cavern = MutableGrid(width, height) { -1L }
-        .apply {
-            for (i in indices) {
-                this[i] = geoIndex(Coord(i % width, i / width), this)
-            }
-        }.mapToGrid {
-            when (it.erosionLevel() % 3) {
-                0L -> CavernType.ROCKY
-                1L -> CavernType.WET
-                else -> CavernType.NARROW
+    /**
+     * Object used to provide the terrain of any position in the cavern. Internally it uses a mutable map to hold
+     * geologic index values. These values are calculated lazily upon request. Prior implementation used a Grid
+     * that calculated all values in advance. This was faster, but was a less general solution because the Grid
+     * required padding on the right and bottom to allow the explorer to overshoot the target in search of more
+     * efficient terrain to navigate. This version is theoretically limited only by the number of elements allowed
+     * in a Map object.
+     */
+    class Cavern(private val start: Coord, private val target: Coord, private val depth: Int) {
+        // helper function used to convert geologic index to erosion level
+        private fun Long.erosionLevel() = (this + depth) % 20183
+
+        // helper function used to convert geologic index to terrain 
+        private fun Long.terrain() = when (this.erosionLevel() % 3) {
+            0L -> Terrain.ROCKY
+            1L -> Terrain.WET
+            else -> Terrain.NARROW
+        }
+
+        // map to hold the geologic index of the cavern. From this, the terrain for any location can be
+        // calculated. It remains mutable because the values are calculated lazily as needed using the geoIndex
+        // function. It starts with 0 at both start and target, per the instructions.
+        private val geoIndexMap = mutableMapOf<Coord, Long>().apply {
+            put(start, 0)
+            put(target, 0)
+        }
+
+        private fun geoIndex(pos: Coord): Long {
+            return geoIndexMap[pos] ?: let { // if value stored in map, return value. Otherwise...
+                geoIndexMap[pos] = when { // ...calculate value and assign it to the map using provided rules
+                    pos.y == 0 -> pos.x * 16807L
+                    pos.x == 0 -> pos.y * 48271L
+                    else -> geoIndex(pos.west()).erosionLevel() * geoIndex(pos.north()).erosionLevel() // recursive!
+                }
+                geoIndexMap.getValue(pos) // provide the newly assigned value
             }
         }
 
-    fun part1() = cavern.subGrid(Coord.ORIGIN, target).sumOf(CavernType::ordinal)
+        // sole public getter provides the terrain for a given position
+        operator fun get(pos: Coord): Terrain = geoIndex(pos).terrain()
+    }
+
+    // initialize the cavern
+    private val cavern = Cavern(start, target, depth)
+
+    // for each position in the rectangle formed by the start and the target, add up the risk level
+    fun part1() = Coord.rectangleFrom(start, target).sumOf { cavern[it].ordinal }
 
     enum class Tool { GEAR, TORCH, NEITHER }
 
@@ -59,7 +72,7 @@ class Y2018D22(input: String) {
      * For a given proposed new location and current state, provides what tool will have to be used and the
      * time that it will take to change any tool, if necessary.
      */
-    private fun changeGear(neighborTerrain: CavernType, state: State): Tool {
+    private fun changeTool(neighborTerrain: Terrain, state: State): Tool {
         val stateTerrain = cavern[state.pos]
         // if terrain is the same, no tool change needed and the weight is 1
         return if (stateTerrain == neighborTerrain) {
@@ -77,26 +90,26 @@ class Y2018D22(input: String) {
     }
 
     /**
-     * Run A* algorithm to find shortest path, using a State object that tracks both the position and the current tool.
-     * The heuristic is the manhattan distance to the target. Edges are found by looking at neighboring positions
-     * and calculating what tool change, if necessary, needs to be made.
+     * Run A* algorithm to find the shortest path, using a State object that tracks both the position and the current
+     * tool. The heuristic is the manhattan distance to the target. Edges are found by looking at neighboring
+     * positions and calculating what tool change, if necessary, needs to be made.
      */
     fun part2() = Graph
         .aStar(
-            startId = State(Coord.ORIGIN, Tool.TORCH),
+            startId = State(start, Tool.TORCH),
             heuristic = { state -> state.pos.manhattanDistance(target).toDouble() },
             defaultEdges = { state ->
-                cavern
-                    .getNeighborIndices(state.pos) // get four neighbors of the location as grid indices
-                    .map { neighborIndex -> // for each neighbor index...
-                        val neighborCoord = cavern.coordOf(neighborIndex) // ...get the coordinates
-                        val newTool = changeGear(cavern[neighborCoord], state) // ...get the new tool if necessary 
+                state.pos
+                    .getNeighbors() // get four neighbors of the location as grid indices
+                    .filter { it.x >= 0 && it.y >= 0 } // avoid negative positions
+                    .map { neighbor -> // for each neighbor index...
+                        val newTool = changeTool(cavern[neighbor], state) // ...get the new tool if necessary 
                         val weight = if (state.tool != newTool) 8.0 else 1.0 // calculate weight incl tool change
                         // add 7 weight if the neighbor is the target and the target is not Torch, to account for
                         // ending up as torch
-                        val endMod = if (neighborCoord == target && newTool != Tool.TORCH) 7 else 0
+                        val endMod = if (neighbor == target && newTool != Tool.TORCH) 7 else 0
                         // wrap together in an Edge object
-                        Graph.Edge(State(neighborCoord, newTool), weight + endMod)
+                        Graph.Edge(State(neighbor, newTool), weight + endMod)
                     }
             }
         ).steps()
@@ -105,9 +118,9 @@ class Y2018D22(input: String) {
 fun main() {
     var time = System.nanoTime()
     val c = Y2018D22(readRawInput("y2018/d22"))
-    println("Class creation: ${elapsedTime(time)}ms")
+    println("Class creation: ${elapsedTime(time)}ms") // (35ms) (98ms grid)
     time = System.nanoTime()
-    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // 5637
+    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // 5637 (27ms) (1ms grid)
     time = System.nanoTime()
-    println("Part 2: ${c.part2()} (${elapsedTime(time)}ms)") // 969
+    println("Part 2: ${c.part2()} (${elapsedTime(time)}ms)") // 969 (1057ms) (491ms grid)
 }
