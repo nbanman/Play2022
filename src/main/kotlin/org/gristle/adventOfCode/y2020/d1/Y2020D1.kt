0887e1025e03ddddefe19f71d10b72228eadfcd2
Neil Banman <neil.banman@gmail.com>
2022-10-17 11:32:47 -0700

    tweaks

--- src/main/kotlin/org/gristle/adventOfCode/y2020/d1/Y2020D1.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2020/d1/Y2020D1.kt
@@ -1,33 +1,24 @@
 package org.gristle.adventOfCode.y2020.d1
 
 import org.gristle.adventOfCode.utilities.elapsedTime
+import org.gristle.adventOfCode.utilities.getPairs
 import org.gristle.adventOfCode.utilities.lines
 import org.gristle.adventOfCode.utilities.readRawInput
 
 class Y2020D1(input: String) {
 
-    private val entries = input.lines().map { it.toInt() }
+    private val entries = input.lines().map(String::toInt)
     private val entrySet = entries.toSet()
-    private val pairEntries = entries.filter { it <= 1010 }
 
-    fun part1(): Int {
-        entrySet.forEach {
-            if (entrySet.contains(2020 - it)) return it * (2020 - it)
-        }
-        throw IllegalStateException("entrySet does not contain correct input")
-    }
+    fun part1() = entries // for each entry...
+        .first { entrySet.contains(2020 - it) } // find the first one where there is another entry that adds to 2020 
+        .let { it * (2020 - it) } // multiply the two together
 
-    fun part2(): Int {
-        for (x in pairEntries.indices) {
-            for (y in pairEntries.indices) {
-                if (x == y) continue
-                val third = 2020 - (pairEntries[x] + pairEntries[y])
-                if (entrySet.contains(third))
-                    return third * pairEntries[x] * pairEntries[y]
-            }
-        }
-        throw IllegalStateException("entrySet does not contain correct input")
-    }
+    fun part2() = entries
+        .getPairs() // convert entries into a list of paired combinations of entries
+        // find first pair where there is another entry that adds to 2020
+        .first { (first, second) -> entrySet.contains(2020 - first - second) }
+        .let { (first, second) -> first * second * (2020 - first - second) } // multiply the three together
 }
 
 fun main() {
