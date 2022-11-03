74fc6cb32f293f77ce880f9dd102880ed9f34e5c
Neil Banman <neil.banman@gmail.com>
2022-11-03 08:25:54 -0700

    streamlined groupValues functions

--- src/main/kotlin/org/gristle/adventOfCode/utilities/parsing.kt
+++ src/main/kotlin/org/gristle/adventOfCode/utilities/parsing.kt
@@ -3,13 +3,7 @@ package org.gristle.adventOfCode.utilities
 /**
  * Convenience method to obtain the group values of a findall regex search of a string.
  */
-fun String.groupValues(pattern: String): List<List<String>> {
-    return pattern
-        .toRegex()
-        .findAll(this)
-        .toList()
-        .map { it.groupValues.drop(1) }
-}
+fun String.groupValues(pattern: String): List<List<String>> = groupValues(pattern.toRegex())
 
 /**
  * Convenience method to obtain the group values of a findall regex search of a string.
@@ -25,22 +19,18 @@ fun String.groupValues(pattern: Regex): List<List<String>> {
  * Convenience method to obtain the group values of a findall regex search of a string,
  * with a way to map the strings to something else if they are all of the same type.
  */
-fun <R> String.groupValues(pattern: String, transform: (String) -> R): List<List<R>> {
-    return groupValues(pattern)
-        .map { matchList ->
-            matchList.map {transform(it)}
-        }
-}
+inline fun <R> String.groupValues(pattern: String, transform: (String) -> R): List<List<R>> =
+    groupValues(pattern.toRegex(), transform)
 
 /**
  * Convenience method to obtain the group values of a findall regex search of a string,
  * with a way to map the strings to something else if they are all of the same type.
  */
-fun <R> String.groupValues(pattern: Regex, transform: (String) -> R): List<List<R>> {
-    return groupValues(pattern)
-        .map { matchList ->
-            matchList.map {transform(it)}
-        }
+inline fun <R> String.groupValues(pattern: Regex, transform: (String) -> R): List<List<R>> {
+    return pattern
+        .findAll(this)
+        .toList()
+        .map { it.groupValues.drop(1).map(transform) }
 }
 
 /**
