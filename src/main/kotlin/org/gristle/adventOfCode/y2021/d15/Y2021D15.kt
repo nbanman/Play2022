3e848878f5afe1341a704a816bf3e8f35c619ce4
Neil Banman <neil.banman@gmail.com>
2022-11-03 12:56:17 -0700

    comments and tweaks

--- src/main/kotlin/org/gristle/adventOfCode/y2021/d15/Y2021D15.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2021/d15/Y2021D15.kt
@@ -1,6 +1,7 @@
 package org.gristle.adventOfCode.y2021.d15
 
 import org.gristle.adventOfCode.utilities.*
+import org.gristle.adventOfCode.utilities.Graph.steps
 
 class Y2021D15(input: String) {
 
@@ -16,31 +17,38 @@ class Y2021D15(input: String) {
     private fun shortestPath(cavern: Grid<Int>): Int {
         val heuristic = { i: Int -> cavern.coordOf(i).manhattanDistance(cavern.lastCoord()).toDouble() }
         val defaultEdges = { i: Int -> cavern.getNeighborIndices(i).map { Graph.Edge(it, cavern[it].toDouble()) } }
-        return Graph.aStar(startId = 0, heuristic = heuristic, defaultEdges = defaultEdges)
-            .last() // aStar function provides list of all nodes in path, so it's last one we care about
-            .weight
-            .toInt()
+        return Graph.aStar(0, heuristic = heuristic, defaultEdges = defaultEdges).steps()
     }
 
+    // Runs A* on the given cavern
     fun part1() = shortestPath(initialCavern)
 
+    // Expands cavern per part2 rules and runs A* on it.
     fun part2(): Int {
-        // Adds *n* to the existing risk, with 10 rolling back to 1.
+        // Utility fun adds *n* to the existing risk, with 10 rolling back to 1.
         fun Int.addRisk(n: Int) = (this + n - 1) % 9 + 1
 
-        val expandedCavern = initialCavern
-            .addRight(initialCavern.mapToGrid { it.addRisk(1) })
-            .addRight(initialCavern.mapToGrid { it.addRisk(2) })
-            .addRight(initialCavern.mapToGrid { it.addRisk(3) })
-            .addRight(initialCavern.mapToGrid { it.addRisk(4) })
-            .let { fatCavern ->
-                fatCavern
-                    .addDown(fatCavern.mapToGrid { it.addRisk(1) })
-                    .addDown(fatCavern.mapToGrid { it.addRisk(2) })
-                    .addDown(fatCavern.mapToGrid { it.addRisk(3) })
-                    .addDown(fatCavern.mapToGrid { it.addRisk(4) })
-            }
+        // width and height of expanded cavern
+        val expandedWidth = initialCavern.width * 5
+        val expandedHeight = initialCavern.height * 5
+        // create the expanded cavern
+        val expandedCavern = Grid(expandedWidth, expandedHeight) { index ->
+            // x and y coord of the cell being populated in the expanded cavern 
+            val x = index % expandedWidth
+            val y = index / expandedWidth
 
+            // corresponding "base" x and y in the initial cavern
+            val xBase = x % initialCavern.width
+            val yBase = y % initialCavern.height
+
+            // adds risk to the expanded parts of the cavern per part2 rules
+            val addedRisk = x / initialCavern.width + y / initialCavern.height
+
+            // populates expanded cavern by taking the risk at the base part of the cavern and adding risk
+            // per part2 rules
+            initialCavern[Coord(xBase, yBase)].addRisk(addedRisk)
+        }
+        // run A*
         return shortestPath(expandedCavern)
     }
 }
@@ -50,7 +58,7 @@ fun main() {
     val c = Y2021D15(readRawInput("y2021/d15"))
     println("Class creation: ${elapsedTime(time)}ms")
     time = System.nanoTime()
-    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // 602 (141ms Dij) (113 aStar)
+    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // 602 (78ms Dij) (64 aStar)
     time = System.nanoTime()
-    println("Part 2: ${c.part2()} (${elapsedTime(time)}ms)") // 2935 (555ms Dij) (465 aStar)
+    println("Part 2: ${c.part2()} (${elapsedTime(time)}ms)") // 2935 (461ms Dij) (327 aStar)
 }
