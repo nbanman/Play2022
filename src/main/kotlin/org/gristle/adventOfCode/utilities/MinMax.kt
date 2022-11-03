92ef0ba087925444d875ab9ab76f48dfefb2c52c
Neil Banman <neil.banman@gmail.com>
2022-11-03 15:14:13 -0700

    efficiency tweak

--- src/main/kotlin/org/gristle/adventOfCode/utilities/MinMax.kt
+++ src/main/kotlin/org/gristle/adventOfCode/utilities/MinMax.kt
@@ -10,13 +10,13 @@ fun <E : Comparable<E>> Pair<E, E>.max() = if (first >= second) first else secon
 fun <E : Comparable<E>> Iterable<E>.minMax(): Pair<E, E> {
     var min = first()
     var max = first()
-    drop(1).forEach { if (it < min) min = it else if (it > max) max = it }
+    forEach { if (it < min) min = it else if (it > max) max = it }
     return min to max
 }
 
 fun <E : Comparable<E>> minMax(vararg items: E): Pair<E, E> {
     var min = items.first()
     var max = items.first()
-    items.drop(1).forEach { if (it < min) min = it else if (it > max) max = it }
+    items.forEach { if (it < min) min = it else if (it > max) max = it }
     return min to max
 }
