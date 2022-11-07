43c9e319b971e92439b500db07c0f390fe9ee128
Neil Banman <neil.banman@gmail.com>
2022-11-07 13:09:23 -0800

    tweaks

--- src/main/kotlin/org/gristle/adventOfCode/y2020/d12/Y2020D12.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2020/d12/Y2020D12.kt
@@ -7,62 +7,58 @@ class Y2020D12(input: String) {
     // Holds instruction info
     data class Instruction(val action: Char, val amount: Int)
 
-    // State interface allows use of two different State objects with different values/functions to work with the 
+    // Interface allows use of two different State objects with different values/functions to work with the 
     // solve function.
     interface ShipState {
+        val pos: Coord
+        fun manhattanDistance() = pos.manhattanDistance()
         fun nextState(instruction: Instruction): ShipState
-
-        fun manhattanDistance(): Int
     }
 
     // State used for part 1. Holds the current position and direction. 
-    data class DirShipState(val pos: Coord = Coord.ORIGIN, val dir: Nsew = Nsew.EAST) : ShipState {
+    data class DirShipState(override val pos: Coord, val dir: Nsew) : ShipState {
         // Provides a new state based on part 1 execution of instructions.
-        override fun nextState(instruction: Instruction): DirShipState = when (instruction.action) {
+        override fun nextState(instruction: Instruction) = when (instruction.action) {
             'N' -> copy(pos = pos.north(instruction.amount))
             'S' -> copy(pos = pos.south(instruction.amount))
             'E' -> copy(pos = pos.east(instruction.amount))
             'W' -> copy(pos = pos.west(instruction.amount))
             'L' -> copy(dir = dir.left(instruction.amount / 90))
             'R' -> copy(dir = dir.right(instruction.amount / 90))
-            else -> copy(pos = pos.move(dir, instruction.amount))
+            'F' -> copy(pos = pos.move(dir, instruction.amount))
+            else -> throw IllegalArgumentException("Invalid Instruction: $instruction")
         }
-
-        override fun manhattanDistance(): Int = pos.manhattanDistance()
     }
 
     // State used for part 2. Holds the current position and waypoint coordinates.
-    data class WaypointShipState(
-        val pos: Coord = Coord.ORIGIN,
-        val waypoint: Coord = Coord(10, -1)
-    ) : ShipState {
+    data class WaypointShipState(override val pos: Coord, val waypoint: Coord) : ShipState {
         // Provides a new state based on part 2 execution of instructions.
-        override fun nextState(instruction: Instruction): WaypointShipState = when (instruction.action) {
+        override fun nextState(instruction: Instruction) = when (instruction.action) {
             'N' -> copy(waypoint = waypoint.north(instruction.amount))
             'S' -> copy(waypoint = waypoint.south(instruction.amount))
             'E' -> copy(waypoint = waypoint.east(instruction.amount))
             'W' -> copy(waypoint = waypoint.west(instruction.amount))
             'L' -> copy(waypoint = (1..(instruction.amount / 90)).fold(waypoint) { acc, _ -> Coord(acc.y, -acc.x) })
             'R' -> copy(waypoint = (1..(instruction.amount / 90)).fold(waypoint) { acc, _ -> Coord(-acc.y, acc.x) })
-            else -> copy(pos = (1..instruction.amount).fold(pos) { acc, _ -> acc + waypoint })
+            'F' -> copy(pos = (1..instruction.amount).fold(pos) { acc, _ -> acc + waypoint })
+            else -> throw IllegalArgumentException("Invalid Instruction: $instruction")
         }
-
-        override fun manhattanDistance(): Int = pos.manhattanDistance()
     }
 
     // parses input into list of Instructions
-    private val instructions = input.lines().map { Instruction(it.first(), it.drop(1).toInt()) }
+    private val instructions: List<Instruction> = input
+        .lines()
+        .map { Instruction(it.first(), it.drop(1).toInt()) }
 
     // for both parts, start with initial state, execute instructions on each successive state, then take the
     // final location and find the distance from the origin.
-    private fun solve(initialShipState: ShipState) =
-        instructions
-            .fold(initialShipState, ShipState::nextState)
-            .manhattanDistance()
+    private fun solve(initialShipState: ShipState): Int = instructions
+        .fold(initialShipState, ShipState::nextState)
+        .manhattanDistance()
 
-    fun part1() = solve(DirShipState())
+    fun part1() = solve(DirShipState(Coord.ORIGIN, Nsew.EAST))
 
-    fun part2() = solve((WaypointShipState()))
+    fun part2() = solve(WaypointShipState(Coord.ORIGIN, Coord(10, -1)))
 }
 
 fun main() {
