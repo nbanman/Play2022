e189be40b0d5161d09535475e762026c732df991
Neil Banman <neil.banman@gmail.com>
2022-11-14 09:23:50 -0800

    template

---
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/template.kt
@@ -0,0 +1,19 @@
+package org.gristle.adventOfCode.y2022
+
+import org.gristle.adventOfCode.utilities.elapsedTime
+import org.gristle.adventOfCode.utilities.readInput
+
+object Template {
+    private val input = readInput("y2022/d")
+
+    fun part1() = "To be implemented"
+
+    fun part2() = "To be implemented"
+}
+
+fun main() {
+    var time = System.nanoTime()
+    println("Part 1: ${Template.part1()} (${elapsedTime(time)}ms)") // 
+    time = System.nanoTime()
+    println("Part 2: ${Template.part2()} (${elapsedTime(time)}ms)") // 
+}
