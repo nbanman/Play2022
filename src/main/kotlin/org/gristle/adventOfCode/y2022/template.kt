417b7937008273f40f89aa36c269f2f228d87116
Neil Banman <neil.banman@gmail.com>
2022-11-15 09:58:28 -0800

    use Stopwatch to track time

--- src/main/kotlin/org/gristle/adventOfCode/y2022/template.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/template.kt
@@ -1,6 +1,6 @@
 package org.gristle.adventOfCode.y2022
 
-import org.gristle.adventOfCode.utilities.elapsedTime
+import org.gristle.adventOfCode.utilities.Stopwatch
 import org.gristle.adventOfCode.utilities.readStrippedInput
 
 class Template(input: String) {
@@ -12,11 +12,9 @@ class Template(input: String) {
 }
 
 fun main() {
-    var time = System.nanoTime()
-    val c = Template(readStrippedInput("y2022/d"))
-    println("Class creation: ${elapsedTime(time)}ms")
-    time = System.nanoTime()
-    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // 
-    time = System.nanoTime()
-    println("Part 2: ${c.part2()} (${elapsedTime(time)}ms)") // 
+    val timer = Stopwatch(start = true)
+    val solver = Template(readStrippedInput("y2022/d"))
+    println("Class creation: ${timer.lap()}ms")
+    println("Part 1: ${solver.part1()} (${timer.lap()}ms)") // 
+    println("Part 2: ${solver.part2()} (${timer.lap()}ms)") // 
 }
