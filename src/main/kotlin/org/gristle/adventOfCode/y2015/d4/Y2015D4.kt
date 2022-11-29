d8b263d7ef358bcf1bb4511c5067cd23b5c950ca
Neil Banman <neil.banman@gmail.com>
2022-11-29 09:52:05 -0800

    added Stopwatch for timing

--- src/main/kotlin/org/gristle/adventOfCode/y2015/d4/Y2015D4.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2015/d4/Y2015D4.kt
@@ -1,11 +1,11 @@
 package org.gristle.adventOfCode.y2015.d4
 
-import org.gristle.adventOfCode.utilities.elapsedTime
+import org.gristle.adventOfCode.utilities.Stopwatch
 import org.gristle.adventOfCode.utilities.md5
 import org.gristle.adventOfCode.utilities.readRawInput
 
 class Y2015D4(private val input: String) {
-    fun solve(digitLength: Int) = generateSequence(1) { it + 1 }
+    private fun solve(digitLength: Int) = generateSequence(1) { it + 1 }
         .first { i -> (input + i).md5().take(digitLength).all { it == '0' } }
 
     fun part1() = solve(5)
@@ -14,11 +14,9 @@ class Y2015D4(private val input: String) {
 }
 
 fun main() {
-    var time = System.nanoTime()
+    val timer = Stopwatch(true)
     val c = Y2015D4(readRawInput("y2015/d4"))
-    println("Class creation: ${elapsedTime(time)}ms")
-    time = System.nanoTime()
-    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // 117946
-    time = System.nanoTime()
-    println("Part 2: ${c.part2()} (${elapsedTime(time)}ms)") // 3938038
+    println("Class creation: ${timer.lap()}ms")
+    println("Part 1: ${c.part1()} (${timer.lap()}ms)") // 117946
+    println("Part 2: ${c.part2()} (${timer.lap()}ms)") // 3938038
 }
