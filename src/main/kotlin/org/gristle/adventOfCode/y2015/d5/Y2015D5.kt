92c0901a5129023bdd292a162a4076e8ad1446dd
Neil Banman <neil.banman@gmail.com>
2022-11-29 10:13:55 -0800

    reorganized; added comments

--- src/main/kotlin/org/gristle/adventOfCode/y2015/d5/Y2015D5.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2015/d5/Y2015D5.kt
@@ -7,21 +7,21 @@ import org.gristle.adventOfCode.utilities.readRawInput
 class Y2015D5(input: String) {
     private val strings = input.lines()
 
-    private val regexVowels = Regex("[aeiou]")
-    private fun String.atLeast3Vowels() = regexVowels.findAll(this).count() >= 3
+    // Create Regex patterns for below functions
+    private val vowels = Regex("[aeiou]")
+    private val repeatedLetter = Regex("""([a-z])\1""")
+    private val forbidden = Regex("ab|cd|pq|xy")
+    private val repeatedDuo = Regex("""([a-z]{2}).*\1""")
+    private val repeated1Between = Regex("""([a-z]).\1""")
 
-    private val regexRepeat = Regex("""([a-z])\1""")
-    private fun String.atLeastOneTwice() = regexRepeat.containsMatchIn(this)
+    // Functions evaluate specific criteria
+    private fun String.atLeast3Vowels() = vowels.findAll(this).count() >= 3
+    private fun String.atLeastOneTwice() = repeatedLetter.containsMatchIn(this)
+    private fun String.noForbiddenStrings() = !forbidden.containsMatchIn(this)
+    private fun String.atLeastTwoTwice() = repeatedDuo.containsMatchIn(this)
+    private fun String.repeatsWithOneBetween() = repeated1Between.containsMatchIn(this)
 
-    private val regexForbidden = Regex("ab|cd|pq|xy")
-    private fun String.noForbiddenStrings() = !regexForbidden.containsMatchIn(this)
     private fun String.isNicePart1() = atLeast3Vowels() && atLeastOneTwice() && noForbiddenStrings()
-
-    private val regex2Twice = Regex("""([a-z]{2}).*\1""")
-    private fun String.atLeastTwoTwice() = regex2Twice.containsMatchIn(this)
-
-    private val regexRepeatWith1Between = Regex("""([a-z]).\1""")
-    private fun String.repeatsWithOneBetween() = regexRepeatWith1Between.containsMatchIn(this)
     private fun String.isNicePart2() = atLeastTwoTwice() && repeatsWithOneBetween()
 
     fun part1() = strings.count { it.isNicePart1() }
@@ -33,7 +33,7 @@ fun main() {
     val timer = Stopwatch(true)
     val c = Y2015D5(readRawInput("y2015/d5"))
     println("Class creation: ${timer.lap()}ms")
-    println("Part 1: ${c.part1()} (${timer.lap()}ms)") // 255
-    println("Part 2: ${c.part2()} (${timer.lap()}ms)") // 55
+    println("\tPart 1: ${c.part1()} (${timer.lap()}ms)") // 255
+    println("\tPart 2: ${c.part2()} (${timer.lap()}ms)") // 55
     println("Total time: ${timer.elapsed()}ms")
 }
