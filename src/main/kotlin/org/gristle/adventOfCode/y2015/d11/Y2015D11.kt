eed7b23f4b470a8762c15339c7e19adb9e2b1809
Neil Banman <neil.banman@gmail.com>
2022-11-29 14:54:07 -0800

    stopwatch + refactor

--- src/main/kotlin/org/gristle/adventOfCode/y2015/d11/Y2015D11.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2015/d11/Y2015D11.kt
@@ -1,6 +1,6 @@
 package org.gristle.adventOfCode.y2015.d11
 
-import org.gristle.adventOfCode.utilities.elapsedTime
+import org.gristle.adventOfCode.utilities.Stopwatch
 import org.gristle.adventOfCode.utilities.readRawInput
 
 class Y2015D11(input: String) {
@@ -12,47 +12,41 @@ class Y2015D11(input: String) {
         return s.substring(0, changeIndex) + (s[changeIndex] + 1) + (1..zs).fold("") { acc, _ -> acc + 'a' }
     }
 
-    private fun hasStraight(s: String): Boolean {
-        return s.windowed(3).any { trip ->
-            ('a'..'z').contains(trip[0])
+    private fun hasStraight(s: String): Boolean = s
+        .windowed(3)
+        .any { trip ->
+            trip[0].isLowerCase()
                     && trip[1] - trip[0] == 1
                     && trip[2] - trip[1] == 1
         }
-    }
 
-    private fun noConfusion(s: String): Boolean {
-        return !"""[iol]"""
-            .toRegex()
-            .containsMatchIn(s)
-    }
+    private fun noConfusion(s: String): Boolean = !"""[iol]"""
+        .toRegex()
+        .containsMatchIn(s)
 
-    private fun twoPairs(s: String): Boolean {
-        return """([a-z])\1"""
-            .toRegex()
-            .findAll(s)
-            .toList()
-            .let {
-                it.size > 1
-            }
-    }
+    private fun twoPairs(s: String): Boolean = """([a-z])\1"""
+        .toRegex()
+        .findAll(s)
+        .count()
+        .let { it > 1 }
 
-    private val nextTwo = generateSequence(input, ::increment) // sequence starts with password and increments by one
-        .drop(1) // do not consider the current password
-        .filter(::isValid) // only consider valid passwords
-        .take(2) // grab the first two
-        .toList() // terminate sequence
+    private fun nextPassword(password: String) =
+        generateSequence(password, ::increment) // sequence starts with password and increments by one
+            .drop(1) // do not consider the current password
+            .first(::isValid) // get first valid password
 
-    fun part1() = nextTwo.first()
+    // Answer to part 1 is reused for part 2, so make it lazy so that the calculation counts towards part 1 time.
+    private val firstPassword: String by lazy { nextPassword(input) }
 
-    fun part2() = nextTwo.last()
+    fun part1() = firstPassword
+    fun part2() = nextPassword(firstPassword)
 }
 
 fun main() {
-    var time = System.nanoTime()
+    val timer = Stopwatch(true)
     val c = Y2015D11(readRawInput("y2015/d11"))
-    println("Class creation: ${elapsedTime(time)}ms")
-    time = System.nanoTime()
-    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // hxbxxyzz
-    time = System.nanoTime()
-    println("Part 2: ${c.part2()} (${elapsedTime(time)}ms)") // hxcaabcc
+    println("Class creation: ${timer.lap()}ms")
+    println("Part 1: ${c.part1()} (${timer.lap()}ms)") // hxbxxyzz
+    println("Part 2: ${c.part2()} (${timer.lap()}ms)") // hxcaabcc
+    println("Total time: ${timer.elapsed()}ms")
 }
