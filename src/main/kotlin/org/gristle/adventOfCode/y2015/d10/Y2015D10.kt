732b5e30541d38b0f9b31c8b7492536668cbe241
Neil Banman <neil.banman@gmail.com>
2022-11-29 14:28:44 -0800

    stopwatch

--- src/main/kotlin/org/gristle/adventOfCode/y2015/d10/Y2015D10.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2015/d10/Y2015D10.kt
@@ -1,6 +1,6 @@
 package org.gristle.adventOfCode.y2015.d10
 
-import org.gristle.adventOfCode.utilities.elapsedTime
+import org.gristle.adventOfCode.utilities.Stopwatch
 import org.gristle.adventOfCode.utilities.readRawInput
 
 class Y2015D10(private val input: String) {
@@ -31,11 +31,10 @@ class Y2015D10(private val input: String) {
 }
 
 fun main() {
-    var time = System.nanoTime()
+    val timer = Stopwatch(true)
     val c = Y2015D10(readRawInput("y2015/d10"))
-    println("Class creation: ${elapsedTime(time)}ms")
-    time = System.nanoTime()
-    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // 492982
-    time = System.nanoTime()
-    println("Part 2: ${c.part2()} (${elapsedTime(time)}ms)") // 6989950
+    println("Class creation: ${timer.lap()}ms")
+    println("Part 1: ${c.part1()} (${timer.lap()}ms)") // 492982
+    println("Part 2: ${c.part2()} (${timer.lap()}ms)") // 6989950
+    println("Total time: ${timer.elapsed()}ms")
 }
