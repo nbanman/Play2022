b4d448282c3b0671a0001ef85a02ba37f3acab76
Neil Banman <neil.banman@gmail.com>
2022-11-28 21:52:04 -0800

    use transform argument with zipWithNext instead of separate map fun use Stopwatch for timer

--- src/main/kotlin/org/gristle/adventOfCode/y2021/d1/Y2021D1.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2021/d1/Y2021D1.kt
@@ -1,28 +1,24 @@
 package org.gristle.adventOfCode.y2021.d1
 
-import org.gristle.adventOfCode.utilities.elapsedTime
+import org.gristle.adventOfCode.utilities.Stopwatch
 import org.gristle.adventOfCode.utilities.readRawInput
 
 class Y2021D1(input: String) {
-
     private val measurements = input.lines().map(String::toInt)
 
-    private fun List<Int>.countIncreased() = windowed(2).count { (a, b) -> a < b }
+    private fun List<Int>.countIncreased() = zipWithNext().count { (a, b) -> a < b }
 
     fun part1() = measurements.countIncreased()
 
     fun part2() = measurements
-        .windowed(3)
-        .map { it.sum() }
+        .windowed(3) { it.sum() }
         .countIncreased()
 }
 
 fun main() {
-    var time = System.nanoTime()
+    val timer = Stopwatch()
     val c = Y2021D1(readRawInput("y2021/d1"))
-    println("Class creation: ${elapsedTime(time)}ms")
-    time = System.nanoTime()
-    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // 1342
-    time = System.nanoTime()
-    println("Part 2: ${c.part2()} (${elapsedTime(time)}ms)") // 1378
+    println("Class creation: ${timer.lap()}ms")
+    println("Part 1: ${c.part1()} (${timer.lap()}ms)") // 1342
+    println("Part 2: ${c.part2()} (${timer.lap()}ms)") // 1378
 }
