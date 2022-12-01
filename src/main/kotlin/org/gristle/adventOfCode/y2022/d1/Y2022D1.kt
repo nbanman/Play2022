627fe188ccd9932c63cf8c0dda7984a3a899a86d
Neil Banman <neil.banman@gmail.com>
2022-12-01 14:49:50 -0800

    PriorityQueue implementation for kicks

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d1/Y2022D1.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d1/Y2022D1.kt
@@ -1,26 +1,24 @@
 package org.gristle.adventOfCode.y2022.d1
 
-import org.gristle.adventOfCode.utilities.Stopwatch
-import org.gristle.adventOfCode.utilities.getInput
-import org.gristle.adventOfCode.utilities.getInts
+import org.gristle.adventOfCode.utilities.*
 
 class Y2022D1(input: String) {
 
     private val calories = input
         .split("\n\n")
         .map { it.getInts().sum() }
-        .sortedDescending()
+        .toPriorityQueueDescending()
 
-    fun part1() = calories.first()
+    fun part1(): Int = calories.peek()
 
-    fun part2() = calories.take(3).sum()
+    fun part2() = calories.poll(3).sum()
 }
 
 fun main() {
     val timer = Stopwatch(start = true)
     val solver = Y2022D1(getInput(1, 2022))
     println("Class creation: ${timer.lap()}ms")
-    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 
-    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 
-    println("Total time: ${timer.elapsed()}ms")
+    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 71300
+    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 209691
+    println("Total time: ${timer.elapsed()}ms") // 63ms
 }
