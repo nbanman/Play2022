33ab703a6b541d13ede20916fc135c1691784eb5
Neil Banman <neil.banman@gmail.com>
2022-12-01 12:44:38 -0800

    push input creation to individual day code

--- src/main/kotlin/org/gristle/adventOfCode/generate/Generate.kt
+++ src/main/kotlin/org/gristle/adventOfCode/generate/Generate.kt
@@ -2,61 +2,59 @@ package org.gristle.adventOfCode.generate
 
 import org.gristle.adventOfCode.utilities.readRawInput
 import org.gristle.adventOfCode.utilities.writeToFile
-import java.net.URL
 import java.time.LocalDate
 
+fun makeDay(day: Int, year: Int = LocalDate.now().year) {
+    readRawInput("generate", "template")
+        .replace("[[YEAR]]", year.toString())
+        .replace("[[DAY]]", day.toString())
+        .writeToFile("src/main/kotlin/org/gristle/adventOfCode/y${year}/d$day/Y${year}D$day.kt")
+}
+
 fun main(args: Array<String>) {
-    val day: String
-    val year: String
+//    for (day in 1..25) {
+//        makeDay(day)
+//    }
+//    return
+    val day: Int
+    val year: Int
+    val thisYear = LocalDate.now().year
 
     when (args.size) {
         0 -> {
-            println("Please provide a day and optionally a year, separated by spaces.")
-            return
+            println("Enter Day:")
+            day = readln().toInt()
+            println("Enter Year, or Enter for [$thisYear]")
+            year = readln().let { if (it == "") thisYear else it.toInt() }
         }
 
         1 -> {
-            val dayInt = args.first().toIntOrNull() ?: -1
-            if (dayInt !in 1..31) {
+            day = args.first().toIntOrNull() ?: -1
+            if (day !in 1..31) {
                 println("The day must be an integer between 1 and 31.")
                 return
-            } else {
-                day = dayInt.toString()
             }
-            year = LocalDate.now().year.toString()
+            year = LocalDate.now().year
         }
 
         else -> {
-            val dayInt = args.first().toIntOrNull() ?: -1
-            if (dayInt !in 1..31) {
+            day = args.first().toIntOrNull() ?: -1
+            if (day !in 1..31) {
                 println("The day must be an integer between 1 and 31.")
                 return
-            } else {
-                day = dayInt.toString()
             }
-            val yearInt = (args[1].toIntOrNull() ?: -1).let {
+
+            year = (args[1].toIntOrNull() ?: -1).let {
                 if (it == -1) {
                     println("The year must be an integer.")
                     return
                 }
                 if (it < 100) it + 2000 else it
             }
-            year = yearInt.toString()
         }
     }
-    // Make kt file
-    readRawInput("generate", "template")
-        .replace("[[YEAR]]", year)
-        .replace("[[DAY]]", day)
-        .writeToFile("src/main/kotlin/org/gristle/adventOfCode/y${year}/d$day/Y${year}D$day.kt")
-
-    // Make input.txt file
-
-    // First get from web
-    val url = URL("https://adventofcode.com/2022/day/1/input")
-    val con = url.openConnection()
-    con.setRequestProperty("User-Agent")
-
 
 }
+
+
 
