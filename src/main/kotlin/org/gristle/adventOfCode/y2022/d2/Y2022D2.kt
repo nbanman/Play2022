4244407cd875be1240c683f77930216cc215a281
Neil Banman <neil.banman@gmail.com>
2022-11-30 22:54:33 -0800

    day2 templates

---
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d2/Y2022D2.kt
@@ -0,0 +1,21 @@
+package org.gristle.adventOfCode.y2022.d2
+
+import org.gristle.adventOfCode.utilities.Stopwatch
+import org.gristle.adventOfCode.utilities.readStrippedInput
+
+class Y2022D2(input: String) {
+
+
+    fun part1() = "To be implemented"
+
+    fun part2() = "To be implemented"
+}
+
+fun main() {
+    val timer = Stopwatch(start = true)
+    val solver = Y2022D2(readStrippedInput("y2022/d2"))
+    println("Class creation: ${timer.lap()}ms")
+    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 
+    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 
+    println("Total time: ${timer.elapsed()}ms")
+}
