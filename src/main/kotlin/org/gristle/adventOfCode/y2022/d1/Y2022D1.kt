4f752b198dad694c154a961985fff08bb126a6dd
nbanman <ci#51ytiu>
2022-11-30 21:09:28 -0800

    First pass

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d1/Y2022D1.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d1/Y2022D1.kt
@@ -1,20 +1,23 @@
 package org.gristle.adventOfCode.y2022.d1
 
 import org.gristle.adventOfCode.utilities.Stopwatch
+import org.gristle.adventOfCode.utilities.getInts
 import org.gristle.adventOfCode.utilities.readStrippedInput
 
 class Y2022D1(input: String) {
 
-    fun part1() = "To be implemented"
+    private val calories = input.split("\n\n").map { it.getInts().sum() }
 
-    fun part2() = "To be implemented"
+    fun part1() = calories.max()
+
+    fun part2() = calories.sortedDescending().take(3).sum()
 }
 
 fun main() {
     val timer = Stopwatch(true)
     val solver = Y2022D1(readStrippedInput("y2022/d1"))
     println("Class creation: ${timer.lap()}ms")
-    println("Part 1: ${solver.part1()} (${timer.lap()}ms)") // 
-    println("Part 2: ${solver.part2()} (${timer.lap()}ms)") // 
+    println("Part 1: ${solver.part1()} (${timer.lap()}ms)") // 71300
+    println("Part 2: ${solver.part2()} (${timer.lap()}ms)") // 209691
     println("Total time: ${timer.elapsed()}ms")
 }
