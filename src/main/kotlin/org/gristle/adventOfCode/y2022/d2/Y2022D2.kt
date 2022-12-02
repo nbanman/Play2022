63b087a30a15d0f757ebfd3d240e4383cd13af7d
Neil Banman <neil.banman@gmail.com>
2022-12-01 16:16:09 -0800

    trying out some ideas for pregaming

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d2/Y2022D2.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d2/Y2022D2.kt
@@ -1,21 +1,32 @@
 package org.gristle.adventOfCode.y2022.d2
 
-import org.gristle.adventOfCode.utilities.Stopwatch
-import org.gristle.adventOfCode.utilities.getInput
+import org.gristle.adventOfCode.utilities.*
 
 class Y2022D2(input: String) {
 
+    private val lines = input.lines().map { line ->
+        line
+    }
+    private val nestedLines = input.blankSplit().map {
+        it.lines().map { line ->
+            line
+        }
+    }
+    private val pattern = Regex("""""")
+
     private val parsed = input
-        
+        .groupValues(pattern)
 
-    fun part1() = "To be implemented"
+    fun part1() = parsed
 
-    fun part2() = "To be implemented"
+    fun part2() = parsed
 }
 
 fun main() {
     val timer = Stopwatch(start = true)
-    val solver = Y2022D2(getInput(2, 2022))
+    val input = getInput(2, 2022)
+    val testInput = listOf("""""")
+    val solver = Y2022D2(input)
     println("Class creation: ${timer.lap()}ms")
     println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 
     println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 
