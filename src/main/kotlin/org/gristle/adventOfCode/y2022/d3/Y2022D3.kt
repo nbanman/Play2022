39eb378b292f25de9130532e0bb05fe96d41c5c1
Neil Banman <neil.banman@gmail.com>
2022-12-02 23:04:17 -0800

    prettied up

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d3/Y2022D3.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d3/Y2022D3.kt
@@ -5,19 +5,33 @@ import org.gristle.adventOfCode.utilities.getInput
 
 class Y2022D3(input: String) {
 
-    private val parsed = input
-        
+    private val rucksacks = input.lines()
 
-    fun part1() = "To be implemented"
+    private fun Char.priority() = if (isLowerCase()) this - 'a' + 1 else this - 'A' + 27
 
-    fun part2() = "To be implemented"
+    fun part1() = rucksacks.sumOf { sack ->
+        sack
+            .chunked(sack.length / 2) // splits each sack into two halves
+            .map(String::toSet) // turn each half into a set
+            .let { (first, second) -> first.intersect(second).first() } // get the Char that's in both sets
+            .priority() // get the priority of that char
+    }
+
+    fun part2() = rucksacks
+        .map(String::toSet) // turn each string into a Set<Char>
+        .chunked(3) // chunk in groups of three
+        .sumOf { chunk ->  // sum each chunk by the priority of the char shared by all three
+            chunk.reduce(Set<Char>::intersect) // get char shared by all three
+                .first()
+                .priority() // get priority
+        }
 }
 
 fun main() {
     val timer = Stopwatch(start = true)
     val solver = Y2022D3(getInput(3, 2022))
     println("Class creation: ${timer.lap()}ms")
-    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 
-    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 
+    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 7428
+    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 2650
     println("Total time: ${timer.elapsed()}ms")
 }
