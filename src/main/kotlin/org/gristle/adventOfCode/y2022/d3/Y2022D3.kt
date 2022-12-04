4b227721f3b764aa16ec122559f4dc61cff8bcfc
Neil Banman <neil.banman@gmail.com>
2022-12-04 14:36:32 -0800

    tweaks

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d3/Y2022D3.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d3/Y2022D3.kt
@@ -9,18 +9,19 @@ class Y2022D3(input: String) {
 
     private fun Char.priority() = if (isLowerCase()) this - 'a' + 1 else this - 'A' + 27
 
-    private fun String.toBitSet() = fold(0L) { acc, c -> acc or (1L shl c.priority()) }
+    // for each character, obtain priority n and then place a '1' bit n to the left of 1.
+    private fun CharSequence.toBitSet() = fold(0L) { acc, c -> acc or (1L shl c.priority()) }
 
     fun part1() = rucksacks.sumOf { sack ->
-        sack.chunked(sack.length / 2) // splits each sack into two halves
-            .map { it.toBitSet() } // turn each half into a set
-            .let { (a, b) -> (a and b).countTrailingZeroBits() }
+        sack
+            .chunked(sack.length / 2) { it.toBitSet() } // splits each sack into two halves turn each half into a bitset
+            .let { (a, b) -> (a and b).countTrailingZeroBits() } // find common bit and convert to priority
     }
 
     fun part2() = rucksacks
-        .map { it.toBitSet() } // turn each string into a Set<Char>
+        .map { it.toBitSet() } // turn each string into a bitset
         .chunked(3) // chunk in groups of three
-        .sumOf { it.reduce(Long::and).countTrailingZeroBits() } // get char shared by all three 
+        .sumOf { it.reduce(Long::and).countTrailingZeroBits() } // find common bit and convert to priority
 }
 
 fun main() {
