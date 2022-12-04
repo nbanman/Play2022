a7894d6aab3033dd2f886a7bb6e38b3f94be6082
Neil Banman <neil.banman@gmail.com>
2022-12-04 13:22:00 -0800

    initial commit

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d4/Y2022D4.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d4/Y2022D4.kt
@@ -1,23 +1,27 @@
 package org.gristle.adventOfCode.y2022.d4
 
-import org.gristle.adventOfCode.utilities.Stopwatch
-import org.gristle.adventOfCode.utilities.getInput
+import org.gristle.adventOfCode.utilities.*
 
 class Y2022D4(input: String) {
 
-    private val parsed = input
-        
+    private val ranges = input
+        .groupValues("""(\d+)-(\d+),(\d+)-(\d+)""", String::toInt)
+        .map { it[0]..it[1] to it[2]..it[3] }
 
-    fun part1() = "To be implemented"
+    fun part1() = ranges.count { (left, right) ->
+        left.containsAll(right) || right.containsAll(left)
+    }
 
-    fun part2() = "To be implemented"
+    fun part2() = ranges.count { (left, right) ->
+        left.overlaps(right)
+    }
 }
 
 fun main() {
     val timer = Stopwatch(start = true)
     val solver = Y2022D4(getInput(4, 2022))
     println("Class creation: ${timer.lap()}ms")
-    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 
-    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 
+    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 605
+    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 914
     println("Total time: ${timer.elapsed()}ms")
 }
