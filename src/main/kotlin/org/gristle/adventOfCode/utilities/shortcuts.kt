8161107b7e995736cb73ff9980590982bf56ec5a
Neil Banman <neil.banman@gmail.com>
2022-12-04 13:21:50 -0800

    added Intrange functions

--- src/main/kotlin/org/gristle/adventOfCode/utilities/shortcuts.kt
+++ src/main/kotlin/org/gristle/adventOfCode/utilities/shortcuts.kt
@@ -59,6 +59,21 @@ fun Int.reversed(bits: Int): Int {
 }
 
 /**
+ * Returns true if an IntRange is a superset of another IntRange; otherwise false.
+ */
+fun IntRange.containsAll(other: IntRange): Boolean =
+    first <= other.first && last >= other.last
+
+/**
+ * Returns true if two IntRanges overlap; otherwise false.
+ */
+fun IntRange.overlaps(other: IntRange): Boolean = if (first <= other.first) {
+    last >= other.first
+} else {
+    other.last >= first
+}
+
+/**
  * Shortcut for groupingBy { it }.eachCount()
  */
 fun <E> Iterable<E>.eachCount() = groupingBy { it }.eachCount()
