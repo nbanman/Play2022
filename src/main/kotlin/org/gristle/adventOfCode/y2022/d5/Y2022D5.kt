9021b1b3d52fb9103922b50b76967f5b99a71cda
Neil Banman <neil.banman@gmail.com>
2022-12-05 13:50:26 -0800

    Use Triple instead of data class for Instruction. Use List<String>.transpose() instead of loops to create crate stacks.

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d5/Y2022D5.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d5/Y2022D5.kt
@@ -1,40 +1,28 @@
 package org.gristle.adventOfCode.y2022.d5
 
-import org.gristle.adventOfCode.utilities.Stopwatch
-import org.gristle.adventOfCode.utilities.blankSplit
-import org.gristle.adventOfCode.utilities.getInput
-import org.gristle.adventOfCode.utilities.groupValues
+import org.gristle.adventOfCode.utilities.*
 import java.util.*
 
 private typealias Stacks = List<ArrayDeque<Char>>
+private typealias Instruction = Triple<Int, Int, Int>
 
 class Y2022D5(input: String) {
-
+    // Split the input into two strings, the first describing the crate arrangement, and the second giving
+    // rearrangement instructions.
     private val parsed = input.blankSplit()
 
-    // Get instructions
-    data class Instruction(val amount: Int, val fromStack: Int, val toStack: Int)
+    // Build crate stacks
+    private val crateStacks = parsed[0]
+        .lines()
+        .dropLast(1)
+        .transpose()
+        .filter { it.last().isLetter() }
+        .map { ArrayDeque(it.trimStart().toList()) }
 
+    // Get instructions
     val instructions = parsed[1]
         .groupValues("""move (\d+) from (\d+) to (\d+)""", String::toInt)
         .map { gv -> Instruction(gv[0], gv[1] - 1, gv[2] - 1) }
-
-    // Build crate stacks
-    private val crates = parsed[0]
-        .lines()
-        .dropLast(1)
-
-    private val stacks: Stacks = List((crates.first().length + 1) / 4) { ArrayDeque<Char>() }
-        .apply {
-            crates.forEach { line ->
-                for (index in 1..line.length step 4) {
-                    val crateValue = line[index]
-                    // note that for building the stacks up we use the add command, instead of push. That puts the 
-                    // element at the bottom, not the top.
-                    if (crateValue != ' ') get(index / 4).add(crateValue)
-                }
-            }
-        }
 
     /**
      * Solve by cloning the stacks, following the instructions to rearrange the crates, then outputting the top
@@ -44,9 +32,9 @@ class Y2022D5(input: String) {
      */
     fun solve(rearrange: (stacks: Stacks, instruction: Instruction) -> Unit): String {
         // stacks are mutable, so clone the stacks so that they can be reused 
-        val localStacks = stacks.map { it.clone() }
-        instructions.forEach { instruction -> rearrange(localStacks, instruction) }
-        return buildString { localStacks.forEach { stack -> append(stack.first) } }
+        val stacks = crateStacks.map { it.clone() }
+        instructions.forEach { instruction -> rearrange(stacks, instruction) }
+        return buildString { stacks.forEach { stack -> append(stack.first) } }
     }
 
     /**
@@ -55,16 +43,18 @@ class Y2022D5(input: String) {
     fun move(amount: Int, fromStack: ArrayDeque<Char>, toStack: ArrayDeque<Char>) {
         repeat(amount) { toStack.push(fromStack.pop()) }
     }
-    
-    fun part1() = solve { stacks, inst -> move(inst.amount, stacks[inst.fromStack], stacks[inst.toStack]) }
+
+    fun part1() = solve { stacks, (amount, fromStack, toStack) ->
+        move(amount, stacks[fromStack], stacks[toStack])
+    }
 
     // Same as part1 except instead of moving directly from one stack to another, we put them in a separate
     // stack to reverse the order that they are moved twice (thus canceling each other out and maintaining the 
     // original order).
-    fun part2() = solve { stacks, inst ->
+    fun part2() = solve { stacks, (amount, fromStack, toStack) ->
         val holdingBay = ArrayDeque<Char>()
-        move(inst.amount, stacks[inst.fromStack], holdingBay) // move to holding stack
-        move(inst.amount, holdingBay, stacks[inst.toStack]) // move from holding stack to new stack
+        move(amount, stacks[fromStack], holdingBay) // move to holding stack
+        move(amount, holdingBay, stacks[toStack]) // move from holding stack to new stack
     }
 }
 
