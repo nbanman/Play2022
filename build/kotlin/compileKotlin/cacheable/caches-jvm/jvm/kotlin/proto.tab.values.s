f60a77a66c37e539d574bf5c48d91c6125ef5d67
Neil Banman <neil.banman@gmail.com>
2022-12-04 21:53:48 -0800

    cleaned up and comments added

--- build/kotlin/compileKotlin/cacheable/caches-jvm/jvm/kotlin/proto.tab.values.s
+++ build/kotlin/compileKotlin/cacheable/caches-jvm/jvm/kotlin/proto.tab.values.s
@@ -1,1 +1,1 @@
-ршм╬Ч╩Я╥Н╡Л╙Д≥я≥Ы©в╤ц░ЩrЩrЩrЩrиsцsщtлqб~й\цPчOБOВOКOжOИOИOИOИOИOЙOъOъOъO
+ршм╬Ч╩Я╥Н╡Л╙Д≥я≥Ы©в╤ц░ЩrЩrЩrЩrиsцsщtлqб~й\цPчOБOВOКOжOИOИOИOИOИOЙOъOъOъOщPхRжQЬQчQкQэUОSОSОSОSОSОSОSОSОSОSШSаUаUШTКSЗTЗTЗT
--- src/main/kotlin/org/gristle/adventOfCode/y2022/d5/Y2022D5.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d5/Y2022D5.kt
@@ -8,86 +8,77 @@ import java.util.*
 
 class Y2022D5(input: String) {
 
-    data class Instruction(val amount: Int, val fromStack: Int, val toStack: Int)
-
     private val parsed = input.blankSplit()
+
+    // Get instructions
+    data class Instruction(val amount: Int, val fromStack: Int, val toStack: Int)
 
     val instructions = parsed[1]
         .groupValues("""move (\d+) from (\d+) to (\d+)""", String::toInt)
         .map { gv -> Instruction(gv[0], gv[1] - 1, gv[2] - 1) }
 
-
-    val crates = parsed[0]
+    // Build crate stacks
+    private val crates = parsed[0]
         .lines()
         .dropLast(1)
-        .reversed()
         .map { line ->
             line
                 .chunked(4)
-                .map { chunk -> chunk.firstOrNull() { it.isUpperCase() } }
+                .map { chunk -> chunk.firstOrNull { it.isUpperCase() } }
         }
 
-    private val stacks: List<Deque<Char>> = List(crates.first().size) { ArrayDeque<Char>() }
-
-    private val stacks2: List<Deque<Char>> = List(crates.first().size) { ArrayDeque<Char>() }
+    private val stacks = List(crates.first().size) { ArrayDeque<Char>() }
 
     init {
+        // note that for building the stacks up we use the add command, instead of push. That puts the element at
+        // the bottom, not the top.
         crates.forEach { line ->
             line.forEachIndexed { index, c ->
-                if (c != null) {
-                    stacks[index].push(c)
-                    stacks2[index].push(c)
-                }
+                if (c != null) stacks[index].add(c)
             }
         }
     }
 
     fun part1(): String {
+        // stacks are mutable, so clone the stacks so that they can be reused from starting position in part 2
+        val stacks1 = List(stacks.size) {
+            stacks[it].clone()
+        }
+        instructions.forEach { (amount, fromStack, toStack) -> // for each instruction
+            repeat(amount) { // do the following n number of times
+                stacks1[toStack].push(stacks1[fromStack].pop()) // make the move from one stack to another
+            }
+        }
+        return buildString {
+            stacks1.forEach { stack -> append(stack.first) }
+        }
+    }
+
+    fun part2(): String {
+        // Same as part1 except instead of moving directly from one stack to another, we put them in a separate
+        // stack to reverse the order that they are moved twice (thus canceling each other out and maintaining the 
+        // original order).
         instructions.forEach { (amount, fromStack, toStack) ->
-            repeat(amount) {
-                stacks[toStack].push(stacks[fromStack].pop())
+            val holdingBay: Deque<Char> = ArrayDeque()
+            repeat(amount) {// move to holding stack
+                holdingBay.push(stacks[fromStack].pop())
+            }
+            repeat(amount) {// move from holding stack to new stack
+                stacks[toStack].push(holdingBay.pop())
             }
         }
         return buildString {
             stacks.forEach { stack -> append(stack.first) }
         }
     }
-
-    fun part2(): String {
-        println("start: $stacks2")
-        instructions.forEach { (amount, fromStack, toStack) ->
-            val holdingBay: Deque<Char> = ArrayDeque<Char>()
-            repeat(amount) {
-                holdingBay.push(stacks2[fromStack].pop())
-            }
-            println("holding bay: $holdingBay")
-            repeat(amount) {
-                stacks2[toStack].push(holdingBay.pop())
-            }
-        }
-        return buildString {
-            stacks2.forEach { stack -> append(stack.first) }
-        }
-    }
 }
 
 fun main() {
-    val input = listOf(
-        getInput(5, 2022),
-        """    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
-
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2""",
-    )
+    val input = getInput(5, 2022)
     val timer = Stopwatch(start = true)
-    val solver = Y2022D5(input[0])
+    val solver = Y2022D5(input)
     println("Class creation: ${timer.lap()}ms")
-    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 
-    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 
+    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // ZSQVCCJLL
+    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // QZFJRWHGS
     println("Total time: ${timer.elapsed()}ms")
 }
