66b301dd823c3dc406db4575c9be59d73038d4c1
Neil Banman <neil.banman@gmail.com>
2022-12-05 21:49:21 -0800

    got rid of part 2 for Day 25

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d25/Y2022D25.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d25/Y2022D25.kt
@@ -20,7 +20,6 @@ fun main() {
     val timer = Stopwatch(start = true)
     val solver = Y2022D25(input[1])
     println("Class creation: ${timer.lap()}ms")
-    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 
-    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 
+    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") //
     println("Total time: ${timer.elapsed()}ms")
 }
