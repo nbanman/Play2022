7d2de5ebe13ae1fa89edae9937a6005745006cff
Neil Banman <neil.banman@gmail.com>
2022-12-06 22:46:18 -0800

    initial commit

---
+++ build.gradle_test.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.20"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.8.0-Beta"
+    id("org.jetbrains.kotlinx.benchmark") version "0.4.6"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
+    implementation(kotlin("script-runtime"))
+    implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.6")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+sourceSets.all {
+    java.setSrcDirs(listOf("$name/src"))
+    resources.setSrcDirs(listOf("$name/resources"))
+}
+
+allOpen {
+    annotation("org.openjdk.jmh.annotations.State")
+}
+
+benchmark {
+    configurations {
+        named("main") {
+            iterationTime = 5
+            iterationTimeUnit = "sec"
+
+        }
+    }
+    targets {
+        register("main") {
+            this as kotlinx.benchmark.gradle.JvmBenchmarkTarget
+            jmhVersion = "1.21"
+        }
+    }
+}
--- src/main/kotlin/org/gristle/adventOfCode/y2022/d7/Y2022D7.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d7/Y2022D7.kt
@@ -5,22 +5,87 @@ import org.gristle.adventOfCode.utilities.getInput
 
 class Y2022D7(input: String) {
 
-    private val parsed = input.lines()
+    class Directory(
+        val name: String,
+        val parent: Directory? = null,
+        val directories: MutableMap<String, Directory> = mutableMapOf(),
+        val files: MutableMap<String, Int> = mutableMapOf()
+    ) {
+        val size: Int by lazy {
+            files.values.sum() + directories.values.sumOf { it.size }
+        }
 
-    fun part1() = "To be implemented"
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
 
-    fun part2() = "To be implemented"
+            other as Directory
+
+            if (name != other.name) return false
+            if (parent != other.parent) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = name.hashCode()
+            result = 31 * result + (parent?.hashCode() ?: 0)
+            return result
+        }
+    }
+
+    private val changeDir = Regex("""\$ cd [A-z]""")
+    private val upDir = Regex("""\$ cd \.\.""")
+    private val homeDirectory = Directory("/")
+    var currentDirectory = homeDirectory
+    private val allDirectories = mutableSetOf(homeDirectory)
+    private fun String.lastWord() = split(" ").last()
+
+    init {
+        input.lines().forEach { line ->
+            when {
+                changeDir.containsMatchIn(line) -> {
+                    currentDirectory = currentDirectory.directories[line.lastWord()]
+                        ?: throw IllegalArgumentException("$line refers to directory that hasn't been created!")
+                }
+
+                upDir.containsMatchIn(line) -> {
+                    currentDirectory = currentDirectory.parent ?: homeDirectory
+                }
+
+                line[0] == 'd' -> {
+                    val dirName = line.lastWord()
+                    val newDir = Directory(dirName, currentDirectory)
+                    currentDirectory.directories[dirName] = newDir
+                    allDirectories.add(newDir)
+                }
+
+                line[0].isDigit() -> {
+                    val (fileSize, fileName) = line.split(" ")
+                    currentDirectory.files[fileName] = fileSize.toInt()
+                }
+            }
+        }
+    }
+
+
+    fun part1() = allDirectories.filter { it.size <= 100000 }.sumOf(Directory::size)
+
+    fun part2() = allDirectories.filter {
+        val spaceAvailable = 70000000L - homeDirectory.size
+        val minDirSize = 30000000L - spaceAvailable
+        it.size >= minDirSize // predicate
+    }.minOf { it.size }
 }
 
 fun main() {
     val input = listOf(
         getInput(7, 2022),
-        """""",
     )
     val timer = Stopwatch(start = true)
-    val solver = Y2022D7(input[1])
+    val solver = Y2022D7(input[0])
     println("Class creation: ${timer.lap()}ms")
-    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 
-    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 
+    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 1477771
+    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 3579501
     println("Total time: ${timer.elapsed()}ms")
 }
