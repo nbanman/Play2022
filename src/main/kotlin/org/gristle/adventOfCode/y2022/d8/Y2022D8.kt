5b7cfd95561bc2bdf39d2fbc46c257ad85c8c93d
Neil Banman <neil.banman@gmail.com>
2022-12-08 09:35:48 -0800

    deduplicated code and added comments

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d8/Y2022D8.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d8/Y2022D8.kt
@@ -4,43 +4,52 @@ import org.gristle.adventOfCode.utilities.*
 
 class Y2022D8(input: String) {
 
+    // representation of the heights of all the trees in the forest. Don't mistake these two variables for each other!
     private val forest = input.toGrid()
+    private val trees = forest.coords()
 
+    // utility functions that make the coordinates aware of their tree height and whether they are in the forest
     private val Coord.treeHeight: Char get() = forest[this]
+    private val Coord.outOfForest: Boolean get() = !forest.validCoord(this)
 
-    private val Coord.isInForest: Boolean get() = forest.validCoord(this)
+    // for a given position and Nsew direction, generate coordinates radiating away from the position 
+    private fun slopeSequence(tree: Coord, slope: Nsew) =
+        generateSequence(tree.move(slope)) { it.move(slope) }
 
-    private fun isVisible(pos: Coord) = Nsew.values().any { slope ->
-        generateSequence(pos.move(slope)) { it.move(slope) }
-            .takeWhile { it.isInForest }
-            .firstOrNull { it.treeHeight >= pos.treeHeight }
-            ?.let { false }
-            ?: true
+    // determines whether a sequence should be terminated, returning true if the position is out of the forest 
+    // or if the tree at the position blocks the treehouse line of sight (LOS).
+    private fun terminating(pos: Coord, tree: Coord): Boolean =
+        pos.outOfForest || pos.treeHeight >= tree.treeHeight
+
+    // for a given position, checks all directions and returns true if any allow LOS out of the forest
+    private fun isVisible(tree: Coord) = Nsew.values().any { slope ->
+        slopeSequence(tree, slope)
+            // keep delivering coordinates until out of forest or LOS blocked
+            .first { pos -> terminating(pos, tree) }
+            .outOfForest // true if made it out of forest before LOS blocked
     }
 
-    private fun scenicScore(pos: Coord) = Nsew.values().map { slope ->
-        generateSequence(pos.move(slope)) { it.move(slope) }
-            .withIndex()
-            .first { (_, newPos) -> !newPos.isInForest || newPos.treeHeight >= pos.treeHeight }
-            .let { (index, newPos) -> index + if (newPos.isInForest) 1 else 0 }
-    }.reduce(Int::times)
+    // for a given position, counts number of visible trees in each direction, then multiplies them together
+    private fun scenicScore(treehouse: Coord) = Nsew.values()
+        .map { slope ->
+            slopeSequence(treehouse, slope)
+                // need to track both the position and the index because the index will be used to count the trees
+                // and the position will be used to add 1 if LOS is blocked (since a blocking tree 
+                // should be counted).    
+                .withIndex()
+                .first { (_, pos) -> terminating(pos, treehouse) }
+                .let { (index, pos) -> index + if (pos.outOfForest) 0 else 1 }
+        }.reduce(Int::times)
 
-    fun part1(): Int = forest.coords().count { pos -> isVisible(pos) }
+    fun part1(): Int = trees.count { tree -> isVisible(tree) }
 
-    fun part2(): Int = forest.coords().maxOf { pos -> scenicScore(pos) }
+    fun part2(): Int = trees.maxOf { treehouse -> scenicScore(treehouse) }
 }
 
 fun main() {
-    val input = listOf(
-        getInput(8, 2022),
-        """30373
-25512
-65332
-33549
-35390""",
-    )
+    val input = getInput(8, 2022)
     val timer = Stopwatch(start = true)
-    val solver = Y2022D8(input[0])
+    val solver = Y2022D8(input)
     println("Class creation: ${timer.lap()}ms")
     println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 1708
     println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 504000
