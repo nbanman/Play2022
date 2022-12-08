9563362ceebf8710f9dc80f3b4ae793d8ec6f3f3
Neil Banman <neil.banman@gmail.com>
2022-12-07 23:48:25 -0800

    initial commit

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d8/Y2022D8.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d8/Y2022D8.kt
@@ -1,26 +1,63 @@
 package org.gristle.adventOfCode.y2022.d8
 
-import org.gristle.adventOfCode.utilities.Stopwatch
-import org.gristle.adventOfCode.utilities.getInput
+import org.gristle.adventOfCode.utilities.*
 
 class Y2022D8(input: String) {
 
-    private val parsed = input.lines()
+    private val forest = input.toGrid(Char::toDigit)
 
-    fun part1() = "To be implemented"
+    private val slopes = listOf(Coord(-1, 0), Coord(0, -1), Coord(1, 0), Coord(0, 1))
 
-    fun part2() = "To be implemented"
+    private fun isVisible(pos: Coord): Boolean {
+
+        fun directionVisible(slope: Coord): Boolean {
+            var newCoord = pos + slope
+            while (forest.validCoord(newCoord)) {
+                if (forest[newCoord] >= forest[pos]) return false
+                newCoord += slope
+            }
+            return true
+        }
+
+        return slopes
+            .map { slope -> directionVisible(slope) }
+            .any { it }
+    }
+
+    private fun scenicScore(pos: Coord) = slopes
+        .map { slope ->
+            var newCoord = pos + slope
+            var trees = 0
+            while (forest.validCoord(newCoord)) {
+                trees++
+                if (forest[newCoord] >= forest[pos]) break
+                newCoord += slope
+            }
+            trees
+        }.reduce(Int::times)
+
+    fun part1(): Int = forest.coords().count { pos ->
+        pos.x == 0 || pos.y == 0 || pos.x == forest.xIndices.last
+                || pos.y == forest.yIndices.last || isVisible(pos)
+    }
+
+    fun part2(): Int = forest.coords().maxOf { pos -> scenicScore(pos) }
 }
+
 
 fun main() {
     val input = listOf(
         getInput(8, 2022),
-        """""",
+        """30373
+25512
+65332
+33549
+35390""",
     )
     val timer = Stopwatch(start = true)
-    val solver = Y2022D8(input[1])
+    val solver = Y2022D8(input[0])
     println("Class creation: ${timer.lap()}ms")
-    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 
-    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 
+    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 1708
+    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 504000
     println("Total time: ${timer.elapsed()}ms")
 }
