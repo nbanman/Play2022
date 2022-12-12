6b771373a4e20439bec87ce5b0e2861726f277f2
Neil Banman <neil.banman@gmail.com>
2022-12-11 22:43:41 -0800

    use dfs instead of bfs; adjust code to accommodate fixed dfs function

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d12/Y2022D12.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d12/Y2022D12.kt
@@ -1,24 +1,49 @@
 package org.gristle.adventOfCode.y2022.d12
 
+import org.gristle.adventOfCode.utilities.Graph
+import org.gristle.adventOfCode.utilities.Graph.steps
 import org.gristle.adventOfCode.utilities.Stopwatch
 import org.gristle.adventOfCode.utilities.getInput
+import org.gristle.adventOfCode.utilities.toGrid
 
 class Y2022D12(input: String) {
 
-    private val parsed = input.lines()
+    private val area = input.toGrid()
 
-    fun part1() = "To be implemented"
+    private fun Char.height() = when (this) {
+        'S' -> 'a'
+        'E' -> 'z'
+        else -> this
+    }.code
 
-    fun part2() = "To be implemented"
+    private fun getEdges(pos: Int): List<Int> = area
+        .getNeighborsIndexedValue(pos)
+        .filter { (_, c) -> c.height() - 1 <= area[pos].height() }
+        .map { (index, _) -> index }
+
+    private val endCondition = { pos: Int -> area[pos] == 'E' }
+
+    fun solve(starts: List<Char>): Int {
+        val startingPoints = area.withIndex().filter { (_, c) -> c in starts }.map { (index, _) -> index }
+        return startingPoints
+            .map { startId ->
+                Graph.dfs(
+                    startId = startId,
+                    endCondition = endCondition,
+                    defaultEdges = ::getEdges
+                ).steps()
+            }.filter { it > 0 }
+            .min()
+    }
+
+    fun part1() = solve(listOf('S'))
+    fun part2() = solve(listOf('S', 'a'))
 }
 
 fun main() {
-    val input = listOf(
-        getInput(12, 2022),
-        """""",
-    )
+    val input = getInput(12, 2022)
     val timer = Stopwatch(start = true)
-    val solver = Y2022D12(input[1])
+    val solver = Y2022D12(input)
     println("Class creation: ${timer.lap()}ms")
     println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 
     println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 
