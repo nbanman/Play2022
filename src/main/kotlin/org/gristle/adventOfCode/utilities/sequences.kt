d0376c7ae6b78edf5571c0d6ec79c948183d2516
Neil Banman <neil.banman@gmail.com>
2022-12-12 11:28:39 -0800

    made takeUntil inline to speed up a bit

--- src/main/kotlin/org/gristle/adventOfCode/utilities/sequences.kt
+++ src/main/kotlin/org/gristle/adventOfCode/utilities/sequences.kt
@@ -5,7 +5,7 @@ fun <T> Sequence<T>.stabilized() = zipWithNext().first { (prev, next) -> prev ==
 fun <T> Sequence<IndexedValue<T>>.stabilized() = zipWithNext()
     .first { (prev, next) -> prev.value == next.value }.first
 
-fun <T> Sequence<T>.takeUntil(predicate: (T) -> Boolean): Sequence<T> = sequence {
+inline fun <T> Sequence<T>.takeUntil(crossinline predicate: (T) -> Boolean): Sequence<T> = sequence {
     for (item in this@takeUntil) {
         yield(item)
         if (predicate(item)) break
