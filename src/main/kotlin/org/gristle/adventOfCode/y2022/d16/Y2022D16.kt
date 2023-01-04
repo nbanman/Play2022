128c32d70e3751f13083847f335f99ac186e57d9
Neil Banman <neil.banman@gmail.com>
2023-01-04 15:22:31 -0800

    works

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d16/Y2022D16.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d16/Y2022D16.kt
@@ -1,116 +1,135 @@
 package org.gristle.adventOfCode.y2022.d16
 
-import org.gristle.adventOfCode.utilities.Graph
-import org.gristle.adventOfCode.utilities.Stopwatch
-import org.gristle.adventOfCode.utilities.getInput
-import org.gristle.adventOfCode.utilities.groupValues
+import org.gristle.adventOfCode.utilities.*
 import java.util.*
+import kotlin.math.max
+import kotlin.math.min
 
 class Y2022D16(input: String) {
+    val pattern = """Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.*)""".toRegex()
     private val flowMap = mutableMapOf<String, Int>()
-    private val edgeMap = mutableMapOf<String, List<String>>()
+    private val edgeMapNoValves = mutableMapOf<String, List<Graph.Edge<String>>>()
+    private val edgeMap: Map<String, Map<String, Int>>
 
     init {
-        val pattern = """Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.*)""".toRegex()
+        // parse to edgeMapNoValves and flowRate maps
         input.groupValues(pattern).forEach { gv ->
             val name = gv[0]
             val flowRate = gv[1].toInt()
-            val tunnels = gv[2].split(", ")
+            val tunnels = gv[2].split(", ").map { Graph.Edge(it, 1.0) }
             flowMap[name] = flowRate
-            edgeMap[name] = tunnels
+            edgeMapNoValves[name] = tunnels
         }
+        // get point to point info on all the points, getting rid of 0-flow valve locations
+        edgeMapNoValves
+            .keys
+            .filter { it == "AA" || flowMap.getValue(it) > 0 }
+            .forEach { valve ->
+                edgeMapNoValves[valve] = Graph.dijkstra(valve, edges = edgeMapNoValves)
+                    .filter { it.id != "AA" && flowMap.getValue(it.id) > 0 && it.weight != 0.0 }
+                    .map { Graph.Edge(it.id, it.weight) }
+            }
+        // clean up the map
+        edgeMap = edgeMapNoValves
+            .filter { it.key == "AA" || flowMap.getValue(it.key) > 0 }
+            .map { it.key to it.value.associate { edge -> edge.vertexId to edge.weight.toInt() + 1 } }
+            .associate { it }
     }
 
-    data class State(val pos: Set<String>, val released: Int, val opened: Set<String> = emptySet()) {
-        fun combine(el: State) = State(
-            pos = pos + el.pos,
-            released = released + el.released,
-            opened = opened + el.opened
+    data class State(
+        val pos: List<Pair<String, Int>>,
+        val valves: Set<String>,
+        val flow: Int,
+        val total: Int,
+        val minute: Int,
+    )
+
+    fun solve(agents: Int, minutes: Int): Int {
+        var max = 0
+
+        val startId = State(
+            pos = List(agents) { "AA" to 0 },
+            valves = edgeMap.getValue("AA").keys,
+            flow = 0,
+            total = 0,
+            minute = minutes + 1,
         )
 
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (javaClass != other?.javaClass) return false
+        val open = PriorityQueue<Pair<State, Int>>(compareByDescending { it.second })
+        open.add(startId to 0)
 
-            other as State
+        val closed = mutableSetOf<State>()
 
-            if (pos != other.pos) return false
-            return true
-        }
+        while (open.isNotEmpty()) {
+            val (current, heuristic) = open.pollUntil { !closed.contains(it.first) } ?: break
+            val firstRoom = current.pos.first()
+            val secondRoom = current.pos.last()
+            closed.add(current)
 
-        override fun hashCode() = pos.hashCode()
-    }
-
-    fun bfs(
-        startId: State,
-        minutes: Int,
-        edges: (State, Int) -> List<State>
-    ): List<Graph.Vertex<State>> {
-        val start = Graph.StdVertex(startId, 0.0)
-        val q: Deque<Graph.Vertex<State>> = ArrayDeque()
-        q.add(start)
-        val visited = mutableMapOf(startId to start)
-        while (q.isNotEmpty()) {
-            val current = q.removeFirst()
-            if (current.weight < minutes) {
-                val currentEdges = edges(current.id, minutes - current.weight.toInt() - 1)
-                currentEdges
-                    .filter {
-                        val prev = visited[it]
-                        if (prev == null) true else {
-                            prev.id.released < it.released
-                                    && prev.id.opened.intersect(it.opened).size == prev.id.opened.size
-                        }
-
-                    }
-                    .map { Graph.StdVertex(it, current.weight + 1.0, current) }
-                    .forEach { neighbor ->
-                        visited[neighbor.id] = neighbor
-                        q.add(neighbor)
-                    }
+            val potentialFuture = heuristic + current.valves.sumOf { valve ->
+                val flowRate = flowMap.getValue(valve)
+                current.pos.maxOf { (room, timeOffset) ->
+                    flowRate * max(0, (current.minute + timeOffset - edgeMap.getValue(room).getValue(valve)))
+                }
             }
+
+            if (heuristic > max) max = heuristic
+
+            if (potentialFuture < max) continue
+
+            val distanceMap = current.valves.associateWith { valve ->
+                current
+                    .pos
+                    .mapIndexed { roomNo, (room, timeOffset) ->
+                        roomNo to edgeMap.getValue(room).getValue(valve) - timeOffset
+                    }.minBy { it.second }
+            }
+
+            val new = distanceMap.map { (valve, roomInfo) ->
+                val (roomNo, distance) = roomInfo
+                val newPos = run {
+                    if (current.pos.size == 1) {
+                        listOf(valve to 0)
+                    } else {
+                        if (roomNo == 0) { // first room moving
+                            val newFirstOffset = min(0, distance)
+                            val newSecondOffset = max(0, distance)
+                            listOf(
+                                valve to newFirstOffset,
+                                secondRoom.first to newSecondOffset
+                            )
+                        } else {
+                            val newFirstOffset = max(0, distance)
+                            val newSecondOffset = -min(0, distance)
+                            listOf(
+                                firstRoom.first to newFirstOffset,
+                                valve to newSecondOffset
+                            )
+                        }
+                    }
+                }
+                val newFlow = current.flow + flowMap.getValue(valve)
+                val newMinute = min(current.minute - distance, current.minute)
+                val newState = State(
+                    pos = newPos,
+                    valves = current.valves - valve,
+                    flow = newFlow,
+                    total = current.total + current.flow * distance,
+                    minute = newMinute
+                )
+                val newHeuristic = heuristic + flowMap.getValue(valve) * (newMinute - 1)
+
+                newState to newHeuristic
+            }
+            open.addAll(new)
         }
-        return visited.values.toList()
+
+        return max
     }
 
-    private val getEdges1 = { state: State, minutesRemaining: Int ->
-        val me = state.pos.first()
-        edgeMap.getValue(me).map { state.copy(pos = setOf(it)) } +
-                if (state.opened.contains(me) || flowMap.getValue(me) == 0) emptyList() else
-                    listOf(
-                        state.copy(
-                            released = state.released + minutesRemaining * flowMap.getValue(me),
-                            opened = state.opened + me
-                        )
-                    )
-    }
+    fun part1() = solve(1, 30)
 
-    private val getEdges2 = { state: State, minutesRemaining: Int ->
-        val me = state.pos.first()
-        val myMoves = edgeMap.getValue(me).map { state.copy(pos = setOf(it)) } +
-                if (state.opened.contains(me) || flowMap.getValue(me) == 0) emptyList() else
-                    listOf(
-                        State(
-                            setOf(me),
-                            state.released + minutesRemaining * flowMap.getValue(me),
-                            state.opened + me
-                        )
-                    )
-        val el = state.pos.last()
-        val elMoves = edgeMap.getValue(el).map { State(setOf(it), 0, emptySet()) } +
-                if (state.opened.contains(el) || flowMap.getValue(me) == 0 || me == el) emptyList() else
-                    listOf(state.copy(released = minutesRemaining * flowMap.getValue(el), opened = setOf(el)))
-        elMoves.flatMap { elMove -> myMoves.map { myMove -> myMove.combine(elMove) } }.distinct()
-    }
-
-    fun solve(minutes: Int, getEdges: (State, Int) -> List<State>): Int {
-        val combos = bfs(State(setOf("AA"), 0, emptySet()), minutes, getEdges)
-        return combos.maxOf { it.id.released }
-    }
-
-    fun part1() = solve(30, getEdges1)
-
-    fun part2() = solve(26, getEdges2)
+    fun part2() = solve(2, 26)
 }
 
 fun main() {
@@ -128,9 +147,10 @@ Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II""",
     )
     val timer = Stopwatch(start = true)
-    val solver = Y2022D16(input[1])
+    val solver = Y2022D16(input[0])
     println("Class creation: ${timer.lap()}ms")
-    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 2059 (344ms) (1651 example 104ms)
-    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // (1707 example) 
+    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 2059 (121ms)
+    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 2790 (5337ms) 
     println("Total time: ${timer.elapsed()}ms")
 }
+
