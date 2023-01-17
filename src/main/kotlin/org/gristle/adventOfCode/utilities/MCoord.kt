4bb143a7aaf5abe0ea31df5ee301053b3dbc2c11
Neil Banman <neil.banman@gmail.com>
2023-01-17 08:46:29 -0800

    added getBounds fun

--- src/main/kotlin/org/gristle/adventOfCode/utilities/MCoord.kt
+++ src/main/kotlin/org/gristle/adventOfCode/utilities/MCoord.kt
@@ -47,6 +47,12 @@ open class MCoord(val coordinates: List<Int>) {
 
     operator fun rem(other: MCoord) = operate(other, 0) { a, b -> a % b }
 
+    operator fun component1() = coordinates[0]
+    operator fun component2() = coordinates[1]
+    operator fun component3() = coordinates[2]
+    operator fun component4() = coordinates[3]
+    operator fun component5() = coordinates[4]
+
     fun getNeighbors(): List<MCoord> {
         return (0 until dimensions).fold(listOf(this)) { acc, dim ->
             val left = acc.map { it.changeInOneDimension(dim, it.coordinates[dim] - 1) }
@@ -109,4 +115,18 @@ class Xyz(val x: Int, val y: Int, val z: Int) : MCoord(listOf(x, y, z)) {
     override fun toString(): String {
         return "Xyz(x=$x, y=$y, z=$z)"
     }
+}
+
+fun Iterable<MCoord>.getBounds(padding: Int = 0): List<IntRange> {
+    val first = first().coordinates
+    val bounds = MutableList<Int>(first.size * 2) { first[it / 2] }
+    forEach { mCoord ->
+        mCoord.coordinates.forEachIndexed { index, i ->
+            val lowerIndex = index * 2
+            val upperIndex = lowerIndex + 1
+            if (i < bounds[lowerIndex]) bounds[lowerIndex] = i
+            if (i > bounds[upperIndex]) bounds[upperIndex] = i
+        }
+    }
+    return bounds.chunked(2) { it.first() - padding..it.last() + padding }
 }
