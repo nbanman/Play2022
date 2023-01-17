6a937f93aace84b44690bedbaeded294e39bc10d
Neil Banman <neil.banman@gmail.com>
2023-01-16 18:57:01 -0800

    replaced on-the-fly looking out with single bfs looking in. 6x speedup

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d18/Y2022D18.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d18/Y2022D18.kt
@@ -1,78 +1,71 @@
 package org.gristle.adventOfCode.y2022.d18
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 import org.gristle.adventOfCode.utilities.*
 
 class Y2022D18(input: String) {
 
-    private val droplets = input.lines().map { line ->
-        val (x, y, z) = line.getIntList()
-        Xyz(x, y, z)
-    }.toSet()
+    private val bounds: Triple<IntRange, IntRange, IntRange>
+    private val droplets: Set<Xyz>
 
-    private fun Xyz.adjacent() = listOf(
-        Xyz(-1, 0, 0),
-        Xyz(1, 0, 0),
-        Xyz(0, -1, 0),
-        Xyz(0, 1, 0),
-        Xyz(0, 0, -1),
-        Xyz(0, 0, 1),
-    ).map { it + this }
+    init {
+        var minX = Int.MAX_VALUE
+        var maxX = Int.MIN_VALUE
+        var minY = Int.MAX_VALUE
+        var maxY = Int.MIN_VALUE
+        var minZ = Int.MAX_VALUE
+        var maxZ = Int.MIN_VALUE
 
-    private fun getSurfaceArea(droplets: Set<Xyz>): Int {
+        droplets = input.getInts().chunked(3).map { (x, y, z) ->
+            if (x < minX) minX = x
+            if (x > maxX) maxX = x
+            if (y < minY) minY = y
+            if (y > maxY) maxY = y
+            if (z < minZ) minZ = z
+            if (z > maxZ) maxZ = z
+            Xyz(x, y, z)
+        }.toSet()
+
+        bounds = Triple(minX - 1..maxX + 1, minY - 1..maxY + 1, minZ - 1..maxZ + 1)
+    }
+
+    private fun Xyz.adjacent() = Xyz.CROSS.map { it + this }
+
+    fun part1(): Int {
         return droplets.sumOf { droplet ->
             val neighbors = droplet.adjacent()
             neighbors.count { !droplets.contains(it) }
         }
     }
 
-    private suspend fun getRealSurfaceArea(droplets: Set<Xyz>): Int = withContext(Dispatchers.Default) {
-        val xRange = droplets.minOf(Xyz::x)..droplets.maxOf(Xyz::x)
-        val yRange = droplets.minOf(Xyz::y)..droplets.maxOf(Xyz::y)
-        val zRange = droplets.minOf(Xyz::z)..droplets.maxOf(Xyz::z)
-        val endCondition = { pos: Xyz -> pos.x !in xRange || pos.y !in yRange || pos.z !in zRange }
-        val enclosed = mutableMapOf<Xyz, Boolean>()
-
-        droplets.map { droplet ->
-            async {
-                val neighbors = droplet.adjacent()
-                neighbors.count { neighbor ->
-                    when {
-                        droplets.contains(neighbor) -> false
-                        enclosed[neighbor] == true -> false
-                        endCondition(neighbor) -> true
-                        else -> {
-                            val bfs = Graph.bfs(
-                                startId = neighbor,
-                                endCondition = endCondition,
-                                defaultEdges = { pos ->
-                                    pos.adjacent().filterNot { droplets.contains(it) }
-                                }
-                            )
-                            val isEnclosed = bfs.isEmpty()
-                            bfs.forEach { enclosed[it.id] = isEnclosed }
-                            !isEnclosed
-                        }
+    fun part2(): Int {
+        val touchingAir = Graph
+            .bfs(
+                startId = Xyz(bounds.first.first, bounds.second.first, bounds.third.first),
+                defaultEdges = { pos ->
+                    pos.adjacent().filter {
+                        it.x in bounds.first
+                                && it.y in bounds.second
+                                && it.z in bounds.third
+                                && !droplets.contains(it)
                     }
                 }
-            }
-        }.sumOf { it.await() }
+            ).map { it.id }
+            .toSet()
+
+        return droplets.sumOf { droplet ->
+            val neighbors = droplet.adjacent()
+            neighbors.count { it in touchingAir }
+        }
     }
-
-    fun part1() = getSurfaceArea(droplets)
-
-    suspend fun part2() = getRealSurfaceArea(droplets)
 }
 
 fun main() = runBlocking {
     val input = getInput(18, 2022)
     val timer = Stopwatch(start = true)
     val solver = Y2022D18(input)
-    println("Class creation: ${timer.lap()}ms")
-    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 4332 (39ms)
-    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 2524 (1286ms)
-    println("Total time: ${timer.elapsed()}ms") // 1394ms
+    println("Class creation: ${timer.lap()}ms") // 58ms
+    println("\tPart 1: ${solver.part1()} (${timer.lap()}ms)") // 4332 (40ms)
+    println("\tPart 2: ${solver.part2()} (${timer.lap()}ms)") // 2524 (80ms) (1286ms original)
+    println("Total time: ${timer.elapsed()}ms") // 179ms
 }
