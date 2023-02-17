a1a389145637468213d76acc2b93dbce5407afd2
Neil Banman <neil.banman@gmail.com>
2023-02-17 13:38:52 -0800

    modified to use runningFold rather than recalculating at every step

--- src/main/kotlin/org/gristle/adventOfCode/y2015/d1/Y2015D1.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2015/d1/Y2015D1.kt
@@ -1,33 +1,17 @@
 package org.gristle.adventOfCode.y2015.d1
 
 import org.gristle.adventOfCode.utilities.Stopwatch
-import org.gristle.adventOfCode.utilities.readRawInput
+import org.gristle.adventOfCode.utilities.getInput
 
 class Y2015D1(input: String) {
-    private val floorChanges: List<Int> by lazy {
-        input.map {
-            when (it) {
-                '(' -> 1
-                ')' -> -1
-                else -> throw IllegalArgumentException("Invalid input: non-parenthesis encountered")
-            }
-        }
-    }
-
-    fun part1() = floorChanges.sum()
-
-    fun part2(): Int {
-        val floor = sequence {
-            // sum the changes, yielding the running total at each step
-            floorChanges.fold(0) { acc, i -> (acc + i).also { yield(it) } }
-        }
-        return floor.indexOfFirst { it == -1 } + 1
-    }
+    private val floorChanges: Sequence<Int> = input.asSequence().map { if (it == '(') 1 else -1 }
+    fun part1(): Int = floorChanges.sum()
+    fun part2(): Int = floorChanges.runningFold(0, Int::plus).indexOfFirst { it == -1 }
 }
 
 fun main() {
     val timer = Stopwatch(true)
-    val c = Y2015D1(readRawInput("y2015/d1"))
+    val c = Y2015D1(getInput(1, 2015))
     println("Class creation: ${timer.lap()}ms")
     println("Part 1: ${c.part1()} (${timer.lap()}ms)") // 280
     println("Part 2: ${c.part2()} (${timer.lap()}ms)") // 1797
