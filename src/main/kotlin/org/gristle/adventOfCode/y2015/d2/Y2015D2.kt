2f9d398c8116736de7a99ce3567195256a8c4397
Neil Banman <neil.banman@gmail.com>
2023-02-17 15:39:10 -0800

    removed regex, used runDay

--- src/main/kotlin/org/gristle/adventOfCode/y2015/d2/Y2015D2.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2015/d2/Y2015D2.kt
@@ -1,12 +1,12 @@
 package org.gristle.adventOfCode.y2015.d2
 
-import org.gristle.adventOfCode.utilities.Stopwatch
-import org.gristle.adventOfCode.utilities.groupValues
-import org.gristle.adventOfCode.utilities.readRawInput
+import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.utilities.getInts
+import org.gristle.adventOfCode.utilities.runDay
 
-class Y2015D2(input: String) {
+class Y2015D2(input: String) : Day {
 
-    data class Box(val l: Int, val w: Int, val h: Int) {
+    private data class Box(val l: Int, val w: Int, val h: Int) {
         private fun cubicVolume() = l * w * h
         private fun surfaceArea() = 2 * l * w + 2 * w * h + 2 * h * l
         private fun smallestSideArea() = cubicVolume() / maxOf(l, w, h)
@@ -16,19 +16,15 @@ class Y2015D2(input: String) {
     }
 
     private val boxes = input
-        .groupValues("""(\d+)x(\d+)x(\d+)""", String::toInt)
-        .map { Box(it[0], it[1], it[2]) }
+        .getInts()
+        .chunked(3)
+        .map { (l, w, h) -> Box(l, w, h) }
 
-    fun part1() = boxes.sumOf(Box::paperNeeded)
+    override fun part1() = boxes.sumOf(Box::paperNeeded)
 
-    fun part2() = boxes.sumOf(Box::ribbonNeeded)
+    override fun part2() = boxes.sumOf(Box::ribbonNeeded)
 }
-
 
 fun main() {
-    val timer = Stopwatch(true)
-    val c = Y2015D2(readRawInput("y2015/d2"))
-    println("Class creation: ${timer.lap()}ms")
-    println("Part 1: ${c.part1()} (${timer.lap()}ms)") // 1588178
-    println("Part 2: ${c.part2()} (${timer.lap()}ms)") // 3783758
-}
+    runDay(2, 2015, Y2015D2::class)
+} // 1588178, 3783758
