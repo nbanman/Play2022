b0a1efccdb3572b36a9303de35a4ab1e85181cda
Neil Banman <neil.banman@gmail.com>
2023-02-17 17:55:00 -0800

    updated to be able to add a string to use as sample input

--- src/main/kotlin/org/gristle/adventOfCode/Day.kt
+++ src/main/kotlin/org/gristle/adventOfCode/Day.kt
@@ -10,7 +10,12 @@ interface Day {
     fun part2(): Any?
 
     companion object {
-        fun <T : Any> runDay(day: Int, year: Int, kClass: KClass<T>? = null) = runBlocking {
+        fun <T : Any> runDay(
+            day: Int,
+            year: Int,
+            kClass: KClass<T>? = null,
+            sampleInput: String? = null,
+        ) = runBlocking {
             val constructor = if (kClass == null) {
                 val name = "org.gristle.adventOfCode.y$year.d$day.Y${year}D$day"
                 Class.forName(name).kotlin.constructors.first()
@@ -19,7 +24,8 @@ interface Day {
             }
             val timer = Stopwatch(true)
             println("[$year Day $day]")
-            val c = constructor.call(getInput(day, year)) as Day
+            val input = sampleInput ?: getInput(day, year)
+            val c = constructor.call(input) as Day
             println("Class creation: ${timer.lap()}ms")
             println("\tPart 1: ${c.part1()} (${timer.lap()}ms)")
             println("\tPart 2: ${c.part2()} (${timer.lap()}ms)")
