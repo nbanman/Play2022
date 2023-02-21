07153ab5f3116d868be75fcbb3bc901c217c5297
Neil Banman <neil.banman@gmail.com>
2023-02-21 12:55:07 -0800

    implemented Day

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d6/Y2016D6.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d6/Y2016D6.kt
@@ -1,27 +1,23 @@
 package org.gristle.adventOfCode.y2016.d6
 
-import org.gristle.adventOfCode.utilities.*
+import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.utilities.eachCount
+import org.gristle.adventOfCode.utilities.lines
+import org.gristle.adventOfCode.utilities.transpose
 
-class Y2016D6(input: String) {
+class Y2016D6(input: String) : Day {
     private val columns = input
         .lines(String::toList)
         .transpose()
         .map { it.eachCount() }
-    
-    fun part1() = columns
+
+    override fun part1() = columns
         .map { column -> column.maxBy { it.value }.key }
         .joinToString("")
 
-    fun part2() = columns
+    override fun part2() = columns
         .map { column -> column.minBy { it.value }.key }
         .joinToString("")
 }
 
-fun main() {
-    val timer = Stopwatch(true)
-    val c = Y2016D6(readRawInput("y2016/d6"))
-    println("Class creation: ${timer.lap()}ms")
-    println("Part 1: ${c.part1()} (${timer.lap()}ms)") // asvcbhvg
-    println("Part 2: ${c.part2()} (${timer.lap()}ms)") // odqnikqv
-    println("Total time: ${timer.elapsed()}ms")
-}
+fun main() = Day.runDay(6, 2016, Y2016D6::class) // asvcbhvg, odqnikqv
