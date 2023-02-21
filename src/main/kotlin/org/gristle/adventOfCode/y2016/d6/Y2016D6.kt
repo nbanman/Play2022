47895f315bd9348af8cfaad7348b1ed2ae5fbf7f
Neil Banman <neil.banman@gmail.com>
2023-02-21 12:56:34 -0800

    light golfing

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d6/Y2016D6.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d6/Y2016D6.kt
@@ -6,18 +6,9 @@ import org.gristle.adventOfCode.utilities.lines
 import org.gristle.adventOfCode.utilities.transpose
 
 class Y2016D6(input: String) : Day {
-    private val columns = input
-        .lines(String::toList)
-        .transpose()
-        .map { it.eachCount() }
-
-    override fun part1() = columns
-        .map { column -> column.maxBy { it.value }.key }
-        .joinToString("")
-
-    override fun part2() = columns
-        .map { column -> column.minBy { it.value }.key }
-        .joinToString("")
+    private val columns = input.lines(String::toList).transpose().map { it.eachCount() }
+    override fun part1() = columns.map { column -> column.maxBy { it.value }.key }.joinToString("")
+    override fun part2() = columns.map { column -> column.minBy { it.value }.key }.joinToString("")
 }
 
 fun main() = Day.runDay(6, 2016, Y2016D6::class) // asvcbhvg, odqnikqv
