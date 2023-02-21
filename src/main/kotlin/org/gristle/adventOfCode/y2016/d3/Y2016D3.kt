5a60cfb834a92d15f3c04656c6611aeeffc1a46d
Neil Banman <neil.banman@gmail.com>
2023-02-21 13:24:12 -0800

    implemented Day, light refactor

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d3/Y2016D3.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d3/Y2016D3.kt
@@ -1,44 +1,28 @@
 package org.gristle.adventOfCode.y2016.d3
 
-import org.gristle.adventOfCode.utilities.Stopwatch
+import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.getInts
-import org.gristle.adventOfCode.utilities.readRawInput
 import org.gristle.adventOfCode.utilities.transpose
 
-private typealias MakeTriangles = (List<List<Int>>) -> List<Y2016D3.Triangle>
-class Y2016D3(input: String) {
-    data class Triangle(val a: Int, val b: Int, val c: Int) {
-        val isValid = let {
-            val max = maxOf(a, b, c)
-            a + b + c - max > max
-        }
-    }
+private typealias Triangle = List<Int>
 
-    private val trios = input.getInts().toList().chunked(3)
-
-    private inline fun solve(makeTriangles: MakeTriangles) =
-        makeTriangles(trios).count(Triangle::isValid)
-
-    fun part1(): Int {
-        val makeTriangles: MakeTriangles = { trios -> trios.map { Triangle(it[0], it[1], it[2]) } }
-        return solve(makeTriangles)
-    }
-
-    fun part2(): Int {
-        val makeTriangles: MakeTriangles = { trios ->
-            trios
-                .transpose()
-                .flatMap { lengths -> lengths.chunked(3) { Triangle(it[0], it[1], it[2]) } }
-        }
-        return solve(makeTriangles)
-    }
+private fun Triangle.isValid(): Boolean {
+    val (a, b, c) = this
+    val max = maxOf(a, b, c)
+    return a + b + c - max > max
 }
 
-fun main() {
-    val timer = Stopwatch(true)
-    val c = Y2016D3(readRawInput("y2016/d3"))
-    println("Class creation: ${timer.lap()}ms")
-    println("Part 1: ${c.part1()} (${timer.lap()}ms)") // 1032
-    println("Part 2: ${c.part2()} (${timer.lap()}ms)") // 1838
-    println("Total time: ${timer.elapsed()}ms")
+class Y2016D3(input: String) : Day {
+
+    private val triangles = input.getInts().chunked(3)
+
+    override fun part1(): Int = triangles.count { it.isValid() }
+
+    override fun part2(): Int = triangles
+        .toList()
+        .transpose()
+        .flatMap { lengths -> lengths.chunked(3) }
+        .count { it.isValid() }
 }
+
+fun main() = Day.runDay(3, 2016, Y2016D3::class) // 1032, 1838 
