386634f4b7924ed913d3d1293922ba4b895326a2
Neil Banman <neil.banman@gmail.com>
2023-02-22 14:38:00 -0800

    refactored to use standard Graph.BFS instead of bespoke functions

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d17/Y2016D17.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d17/Y2016D17.kt
@@ -1,80 +1,73 @@
 package org.gristle.adventOfCode.y2016.d17
 
+import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.Coord
-import org.gristle.adventOfCode.utilities.elapsedTime
+import org.gristle.adventOfCode.utilities.Graph
+import org.gristle.adventOfCode.utilities.contains
 import org.gristle.adventOfCode.utilities.md5
-import org.gristle.adventOfCode.utilities.readRawInput
 
-// Not refactored, not terrible.
-class Y2016D17(private val input: String) {
+class Y2016D17(private val input: String) : Day {
 
-    private val vaultRange = 0..3
-    data class Room(val coord: Coord, val passCode: String, val vaultRange: IntRange) {
+    // state object for BFS search
+    private data class Room(val pos: Coord, val passCode: String)
 
-        private val openRange = 'b'..'f'
-        fun getNeighbors(): List<Room> {
-            val open = passCode.md5().take(4).map { it in openRange }
+    // used to translate whether a door is open given a specific MD5 hash
+    private val openRange = 'b'..'f'
 
-            val neighbors = listOf(
-                Room(Coord(coord.x - 1, coord.y), passCode + 'L', vaultRange),
-                Room(Coord(coord.x + 1, coord.y), passCode + 'R', vaultRange),
-                Room(Coord(coord.x, coord.y - 1), passCode + 'U', vaultRange),
-                Room(Coord(coord.x, coord.y + 1), passCode + 'D', vaultRange)
+    // the bottom-right room position
+    private val endPos = Coord(3, 3)
+
+    // function finds edges for a given Room node
+    private val openDoors = { room: Room ->
+
+        if (room.pos == endPos) {
+
+            // If the node is at endPos, we do not want to return any neighbors. That path is completed. Relevant for
+            // part 2 because we are not done when we first hit endPos.
+            emptyList()
+        } else {
+
+            // use MD5 on the passcode to determine which doors are open and return list of rooms who have open
+            // doors leading to them.
+            val open = room.passCode.md5().take(4).map { it in openRange }
+            listOf(
+                Room(room.pos.north(), room.passCode + 'U'),
+                Room(room.pos.south(), room.passCode + 'D'),
+                Room(room.pos.west(), room.passCode + 'L'),
+                Room(room.pos.east(), room.passCode + 'R'),
             ).filter {
-                it.coord.x in vaultRange &&
-                        it.coord.y in vaultRange &&
-                        when(it.passCode.last()) {
+                it.pos in (Coord.ORIGIN to endPos) &&
+                        when (it.passCode.last()) {
                             'L' -> open[2]
                             'R' -> open[3]
                             'U' -> open[0]
                             else -> open[1]
                         }
             }
-
-            return neighbors
-
         }
     }
 
-    private fun getPath(locations: List<Room>): String {
+    // Part one wants the fastest path, so BFS with an endCondition of reaching endPos finds it. The answer is the
+    // passcode of the first node at the endPos, minus the prelude.
+    override fun part1() = Graph
+        .bfs(
+            startId = Room(Coord.ORIGIN, input),
+            endCondition = { (pos, _) -> pos == endPos },
+            defaultEdges = { room -> openDoors(room) }
+        ).last()
+        .id
+        .passCode
+        .drop(input.length)
 
-        tailrec fun gP(locations: List<Room>): Room? {
-            val newLocations = mutableListOf<Room>()
-            locations.forEach { newLocations.addAll(it.getNeighbors()) }
-            return newLocations.find { it.coord == Coord(vaultRange.last, vaultRange.last) }
-                ?: if (newLocations.isEmpty()) {
-                    null
-                } else {
-                    gP(newLocations)
-                }
-        }
-        return gP(locations)?.passCode?.drop(locations.first().passCode.length) ?: "Invalid"
-
-    }
-
-    private fun getPath3(location: Room): Room? {
-        if (location.coord.x == 3 && location.coord.y == 3) return location
-        val neighbors = location.getNeighbors()
-        if (neighbors.isEmpty()) return null
-        val paths = neighbors.mapNotNull { getPath3(it) }
-        return paths.maxByOrNull { it.passCode.length } ?: Room(Coord(0, 0), "Invalid", vaultRange)
-    }
-
-    fun part1() = getPath(listOf(Room(Coord(0, 0), input, vaultRange)))
-
-    fun part2(): Int {
-        val room = getPath3(Room(Coord(0, 0), input, vaultRange))
-            ?: Room(Coord(0, 0), "Invalid", vaultRange)
-        return room.passCode.length - input.length
-    }
+    // Part two wants the fastest path, so BFS until no more edges are found. The answer is the weight of the last
+    // Node found at endPos.
+    override fun part2() = Graph
+        .bfs(
+            startId = Room(Coord.ORIGIN, input),
+            defaultEdges = { room -> openDoors(room) }
+        ).last { it.id.pos == endPos }
+        .weight
+        .toInt()
 }
 
-fun main() {
-    var time = System.nanoTime()
-    val c = Y2016D17(readRawInput("y2016/d17"))
-    println("Class creation: ${elapsedTime(time)}ms")
-    time = System.nanoTime()
-    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // DDRUDLRRRD
-    time = System.nanoTime()
-    println("Part 2: ${c.part2()} (${elapsedTime(time)}ms)") // 398
-}
+fun main() = Day.runDay(17, 2016, Y2016D17::class) // DDRUDLRRRD, 398
