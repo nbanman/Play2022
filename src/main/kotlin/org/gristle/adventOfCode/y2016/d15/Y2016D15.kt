7d4eb3b43445a97fb03b8d71e5d9fb5373cdb2ee
Neil Banman <neil.banman@gmail.com>
2023-02-22 11:33:24 -0800

    use sieve CRT instead of brute force

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d15/Y2016D15.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d15/Y2016D15.kt
@@ -3,22 +3,28 @@ package org.gristle.adventOfCode.y2016.d15
 import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.getInts
 
+private typealias Disc = Pair<Int, Int>
+
 class Y2016D15(input: String) : Day {
 
-    private class Disc(val number: Int, val positions: Int, val startPosition: Int) {
-        fun passes(startingSecond: Int) = (startingSecond + number + startPosition) % positions == 0
-    }
-
     // Parse input into Discs
-    private val discs = input.getInts().chunked(4) { Disc(it[0], it[1], it[3]) }
+    private val discs = input
+        .getInts()
+        .chunked(4) { it[1] to -it[0] - it[3] }
 
-    // Find the first number for which all discs pass
-    private fun solve(discs: Sequence<Disc>) = generateSequence(0) { it + 1 }
-        .first { index -> discs.all { it.passes(index) } }
+    // Uses a sieve version of CRT
+    private fun solve(discs: Sequence<Disc>): Int {
+        val (_, answer) = discs.fold(1 to 0) { (positions, offset), (dPositions, dOffset) ->
+            val nextOffset = generateSequence(offset) { it + positions }
+                .first { (it - dOffset).mod(dPositions) == 0 }
+            positions * dPositions to nextOffset
+        }
+        return answer
+    }
 
     override fun part1() = solve(discs)
 
-    override fun part2() = solve(discs + Disc(7, 11, 0))
+    override fun part2() = solve(discs + listOf(11 to -7))
 }
 
 fun main() = Day.runDay(15, 2016, Y2016D15::class) // 122318, 3208583
