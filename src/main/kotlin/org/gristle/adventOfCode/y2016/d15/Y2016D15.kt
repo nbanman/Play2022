0c18bd6cec530aeefdc3e479489ab59a68f3b75e
Neil Banman <neil.banman@gmail.com>
2023-02-22 11:45:08 -0800

    added comments and changed to use Long instead of Int so that it can work with unofficial Part 3 (https://www.reddit.com/r/adventofcode/comments/5ifvyc/2016_day_15_part_3_our_discs_got_larger/).

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d15/Y2016D15.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d15/Y2016D15.kt
@@ -1,30 +1,37 @@
 package org.gristle.adventOfCode.y2016.d15
 
 import org.gristle.adventOfCode.Day
-import org.gristle.adventOfCode.utilities.getInts
+import org.gristle.adventOfCode.utilities.getLongs
 
-private typealias Disc = Pair<Int, Int>
+private typealias Disc = Pair<Long, Long>
 
 class Y2016D15(input: String) : Day {
 
     // Parse input into Discs
     private val discs = input
-        .getInts()
+        .getLongs()
         .chunked(4) { it[1] to -it[0] - it[3] }
 
     // Uses a sieve version of CRT
-    private fun solve(discs: Sequence<Disc>): Int {
-        val (_, answer) = discs.fold(1 to 0) { (positions, offset), (dPositions, dOffset) ->
-            val nextOffset = generateSequence(offset) { it + positions }
-                .first { (it - dOffset).mod(dPositions) == 0 }
-            positions * dPositions to nextOffset
+    private fun solve(discs: Sequence<Disc>): Long {
+        val (_, answer) = discs.fold(1L to 0L) { (interval, seconds), (positions, offset) ->
+
+            // take the current number of seconds that works for the previous discs, and keep adding the current
+            // interval until it works for the next disc.
+            val nextSeconds = generateSequence(seconds) { it + interval }
+                .first { (it - offset).mod(positions) == 0L }
+
+            // values are coprime; next interval is current interval multiplied by the number of positions in next disc
+            val nextInterval = interval * positions
+
+            nextInterval to nextSeconds
         }
         return answer
     }
 
     override fun part1() = solve(discs)
 
-    override fun part2() = solve(discs + listOf(11 to -7))
+    override fun part2() = solve(discs + listOf(11L to -7L))
 }
 
-fun main() = Day.runDay(15, 2016, Y2016D15::class) // 122318, 3208583
+fun main() = Day.runDay(15, 2016, Y2016D15::class) // 122318, 3208583 (11ms creation, 5ms pt 1, 1ms pt 2)
