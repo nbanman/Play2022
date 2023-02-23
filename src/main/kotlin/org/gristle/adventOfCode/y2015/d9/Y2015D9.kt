5d153e7e645aa78a3dae102f8b6be4813177c45d
Neil Banman <neil.banman@gmail.com>
2023-02-23 09:02:20 -0800

    refactored to use standard graph for pt 2

--- src/main/kotlin/org/gristle/adventOfCode/y2015/d9/Y2015D9.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2015/d9/Y2015D9.kt
@@ -1,10 +1,14 @@
 package org.gristle.adventOfCode.y2015.d9
 
-import org.gristle.adventOfCode.utilities.*
+import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.utilities.Graph
 import org.gristle.adventOfCode.utilities.Graph.steps
+import org.gristle.adventOfCode.utilities.groupValues
+import org.gristle.adventOfCode.utilities.takeUntil
 
-class Y2015D9(private val input: String) {
+class Y2015D9(private val input: String) : Day {
 
+    // Parse input into adjacency map for Dijkstra search.
     private val edgeMap = buildMap<String, MutableList<Graph.Edge<String>>> {
         input.groupValues("""(\w+) to (\w+) = (\d+)""").forEach { gv ->
             getOrPut(gv[0]) { mutableListOf() }.add(Graph.Edge(gv[1], gv[2].toDouble()))
@@ -12,70 +16,38 @@ class Y2015D9(private val input: String) {
         }
     } as Map<String, List<Graph.Edge<String>>>
 
-    data class Leg(val startCity: String, val endCity: String, val distance: Int)
-
-    private val legs = input
-        .groupValues("""(\w+) to (\w+) = (\d+)""")
-        .flatMap { gv ->
-            listOf(
-                Leg(gv[0], gv[1], gv[2].toInt()),
-                Leg(gv[1], gv[0], gv[2].toInt())
-            )
-        }
-
+    // The edgemap keys are a set of all the cities on the graph.
     private val cities = edgeMap.keys
 
-    private fun longestDistance(
-        legs: List<Leg>,
-        remainingLegs: List<Leg>,
-        remainingCities: List<String>,
-        distance: Int = 0
-    ): Int {
-        if (remainingCities.size <= 1) return distance
-        val matchingLegs = if (legs.isEmpty()) {
-            remainingLegs
-        } else {
-            remainingLegs.filter { leg ->
-                leg.startCity == legs.last().endCity && remainingCities.contains(leg.endCity)
-            }
-        }
-        return matchingLegs.maxOf { leg ->
-            longestDistance(
-                legs + leg,
-                (remainingLegs - leg).filter { remainingLeg ->
-                    remainingCities.contains(remainingLeg.endCity) && remainingLeg.endCity != leg.endCity
-                },
-                remainingCities - leg.startCity,
-                distance + leg.distance
-            )
-        }
-    }
-
+    // The Dijkstra search does not use the adjacency map directly because the state object being tracked is the
+    // complete list of cities, and repeat city visits are not allowed. This function translates the lookup and
+    // applies the rule.
     private val defaultEdges = { id: List<String> ->
-        edgeMap[id.last()]
-            ?.filter { !id.contains(it.vertexId) }
-            ?.map { Graph.Edge(id + it.vertexId, it.weight) }
-            ?: emptyList()
+        edgeMap[id.last()] // grabs edges connected to last city visited
+            ?.filter { !id.contains(it.vertexId) } // filters out adjacent cities that have already been visited
+            ?.map { Graph.Edge(id + it.vertexId, it.weight) } // provides an edge using the adjacency list data
+            ?: throw IllegalStateException("City does not exist.")
     }
 
-    fun part1() = cities.minOf { city ->
-        Graph
-            .dijkstraSequence(
-                startId = listOf(city),
-                defaultEdges = defaultEdges
-            ).takeUntil { it.id.size == cities.size }
+    private fun tour(city: String) = Graph.dijkstraSequence(startId = listOf(city), defaultEdges = defaultEdges)
+
+    // Part one finds the shortest path so the end condition is specified with takeUntil. It runs from each city
+    // (hmm... Floyd-Warshall?) then takes the minimum of each city.
+    override fun part1() = cities.minOf { city ->
+        tour(city)
+            .takeUntil { it.id.size == cities.size }
             .steps()
     }
 
-    fun part2() = longestDistance(emptyList(), legs, cities.toList())
+    // Part two is the same as Part one except that it finds the longest path so no end condition is specified.
+    // Instead, it runs until there are no more nodes to visit, then grabs the largest weight.
+    override fun part2() = cities.maxOf { city ->
+        tour(city)
+            .filter { it.id.size == cities.size }
+            .maxOf { it.weight.toInt() }
+    }
 }
 
-fun main() {
-    var time = System.nanoTime()
-    val c = Y2015D9(readRawInput("y2015/d9"))
-    println("Class creation: ${elapsedTime(time)}ms")
-    time = System.nanoTime()
-    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // 207 (27ms Dij) (156ms old)
-    time = System.nanoTime()
-    println("Part 2: ${c.part2()} (${elapsedTime(time)}ms)") // 804 (140ms)
-}
+// Pt1: 207 (27ms Dij) (156ms old)
+// Pt2: 804 (171ms Dij) (140ms old)
+fun main() = Day.runDay(9, 2015, Y2015D9::class)
