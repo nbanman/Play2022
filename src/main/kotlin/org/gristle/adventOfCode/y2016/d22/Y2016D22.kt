b066aec7cc5a79b32a7caba4b72f19500c608394
Neil Banman <neil.banman@gmail.com>
2023-02-23 15:37:39 -0800

    implemented Day

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d22/Y2016D22.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d22/Y2016D22.kt
@@ -1,11 +1,10 @@
 package org.gristle.adventOfCode.y2016.d22
 
+import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.Coord
-import org.gristle.adventOfCode.utilities.elapsedTime
 import org.gristle.adventOfCode.utilities.groupValues
-import org.gristle.adventOfCode.utilities.readRawInput
 
-class Y2016D22(input: String) {
+class Y2016D22(input: String) : Day {
 
     private val pattern = """(\d+)-y(\d+) +(\d+)T +(\d+)""".toRegex()
 
@@ -27,17 +26,17 @@ class Y2016D22(input: String) {
             ProblemNode(coord, it[2], it[3], coord == garbageCoord)
         }
 
-    fun part1() = nodes
+    override fun part1() = nodes
         .filter { it.used != 0 }
         .sumOf { nodeA -> nodes.count { it != nodeA && it.available >= nodeA.used } }
+
+    override fun part2() =
+        "213, done by hand, as intended by puzzle author (see https://www.reddit.com/r/adventofcode/comments/5jor9q/comment/dbhwg4l/?utm_source=share&utm_medium=web2x&context=3)"
 }
 
-fun main() {
-    var time = System.nanoTime()
-    val c = Y2016D22(readRawInput("y2016/d22"))
-    println("Class creation: ${elapsedTime(time)}ms")
-    time = System.nanoTime()
-    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // 924
-    println("Part 2: 213, done by hand, as intended by puzzle author (see https://www.reddit.com/r/adventofcode/comments/5jor9q/comment/dbhwg4l/?utm_source=share&utm_medium=web2x&context=3)") // 213
-    println("(also, I gave up!)")
-}
+fun main() = Day.runDay(22, 2016, Y2016D22::class)
+
+//Class creation: 32ms
+//Part 1: 924 (20ms)
+//Part 2: 213, done by hand, as intended by puzzle author (see https://www.reddit.com/r/adventofcode/comments/5jor9q/comment/dbhwg4l/?utm_source=share&utm_medium=web2x&context=3) (0ms)
+//Total time: 53ms
