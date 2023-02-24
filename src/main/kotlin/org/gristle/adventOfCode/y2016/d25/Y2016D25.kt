0f258073658e0a7de215c04e23e4071d137b3191
Neil Banman <neil.banman@gmail.com>
2023-02-24 15:26:42 -0800

    implemented Day

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d25/Y2016D25.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d25/Y2016D25.kt
@@ -1,19 +1,14 @@
 package org.gristle.adventOfCode.y2016.d25
 
-import org.gristle.adventOfCode.utilities.elapsedTime
-import org.gristle.adventOfCode.utilities.readRawInput
+import org.gristle.adventOfCode.Day
 
-class Y2016D25(input: String) {
+class Y2016D25(input: String) : Day {
     // Reverse engineering the code ends with infinite loop that prints binary representation of d over and
     // over again, and defines d = a + 2555. So a = d - 2555. Starting from 2555, the first number that is
     // 1010..10 is 2730. So that's d.
-    fun part1() = 2730 - 2555
+    override fun part1() = 2730 - 2555
+
+    override fun part2() = null
 }
 
-fun main() {
-    var time = System.nanoTime()
-    val c = Y2016D25(readRawInput("y2016/d25"))
-    println("Class creation: ${elapsedTime(time)}ms")
-    time = System.nanoTime()
-    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // 175
-}
+fun main() = Day.runDay(25, 2016, Y2016D25::class)
