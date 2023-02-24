c4ca33aa4817f2bf1010f945031a24091420c12e
Neil Banman <neil.banman@gmail.com>
2023-02-24 15:04:36 -0800

    code implementation for pt. 2

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d22/Y2016D22.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d22/Y2016D22.kt
@@ -1,42 +1,111 @@
 package org.gristle.adventOfCode.y2016.d22
 
 import org.gristle.adventOfCode.Day
-import org.gristle.adventOfCode.utilities.Coord
-import org.gristle.adventOfCode.utilities.groupValues
+import org.gristle.adventOfCode.utilities.*
+import org.gristle.adventOfCode.utilities.Graph.steps
+
+private typealias State = Pair<Coord, Coord>
 
 class Y2016D22(input: String) : Day {
 
-    private val pattern = """(\d+)-y(\d+) +(\d+)T +(\d+)""".toRegex()
-
-    data class Node(val coord: Coord, val size: Int, val used: Int, val garbage: Boolean) {
+    // Parsing input to Nodes
+    class Node(val coord: Coord, val size: Int, val used: Int) {
         val available = size - used
-
-        override fun toString(): String {
-            return "(x:${coord.x}, y:${coord.y}, size:$size, used:$used, available:$available, garbage:$garbage)"
-        }
     }
 
-    val gv = input.groupValues(pattern) { it.toInt() }
-
-    private val garbageCoord = Coord(gv.last()[0], 0)
-
-    private val nodes = gv
-        .map {
-            val coord = Coord(it[0], it[1])
-            Node(coord, it[2], it[3], coord == garbageCoord)
-        }
+    private val nodes = let {
+        val pattern = Regex("""(\d+)-y(\d+) +(\d+)T +(\d+)""")
+        input
+            .gvs(pattern) { it.toInt() }
+            .map {
+                val coord = Coord(it[0], it[1])
+                Node(coord, it[2], it[3])
+            }.toList()
+    }
 
     override fun part1() = nodes
         .filter { it.used != 0 }
         .sumOf { nodeA -> nodes.count { it != nodeA && it.available >= nodeA.used } }
 
-    override fun part2() =
-        "213, done by hand, as intended by puzzle author (see https://www.reddit.com/r/adventofcode/comments/5jor9q/comment/dbhwg4l/?utm_source=share&utm_medium=web2x&context=3)"
+    /**
+     * Double-nested A* implementation. The problem requires moving certain "goal data" from its original location
+     * to the top-left "origin." It can only move by swapping positions with an "empty spot." The empty spot can swap
+     * positions within the bounds of the nodes, but there are some spots with too much data for swapping. These are
+     * effectively "walls."
+     *
+     * The "outer" A* heuristic is the manhattan distance * 5 from the goal data to the origin, because the best-case
+     * scenario^ takes four moves to get the empty spot in front of the goal data, and one move to move into the empty
+     * spot. The state tracks both the goal data location and the empty spot.
+     *
+     * The "outer" edges are spots adjacent to the goal data within the bounds of the Node space, excluding "walls."
+     *
+     * The actual weight of each edge needs to be calculated. I used A* for this "inner" calculation. This time the
+     * heuristic is simply manhattan distance from the empty spot to the edge location. The "inner" edges are spots
+     * adjacent to the empty spot, excluding walls and the goal data itself.
+     *
+     * ^: Not truly a best-case scenario, if the empty spot is already adjacent to the goal data. This is an edge case I
+     * leave as an exercise to the next coder.
+     */
+    override fun part2(): Int {
+        // looks through the nodes and finds the boundaries, puts them into top-left and bottom-right Coords.
+        val bounds = nodes.map(Node::coord).getBounds().let { (xR, yR) -> Coord.ORIGIN to Coord(xR.last, yR.last) }
+
+        // finds positions of nodes that cannot be moved.
+        val walls = nodes.filter { it.used > 400 }.map(Node::coord)
+
+        // utility function for finding edges, used in both inner and outer A* implementations.
+        fun Coord.adjacent(exclude: Coord? = null) = getNeighbors()
+            .filter { it in bounds && it !in walls && it != exclude }
+
+        // the data that needs to be moved to the origin, location defined by the problem
+        val goalData = Coord(bounds.second.x, 0)
+
+        // the node without any data in it
+        val empty = nodes.find { it.used == 0 }?.coord ?: throw IllegalArgumentException("No empty node found!")
+
+        // the "outer" heuristic
+        val heuristic: (State) -> Double = { (pos, _) ->
+            pos.manhattanDistance(Coord.ORIGIN) * 5.0
+        }
+
+        // finds edges for the "outer" A*. Calls an "inner" A* to find the weight for those edges.
+        val move: (State) -> List<Graph.Edge<State>> = { (pos, empty) ->
+            pos.adjacent().map { neighbor ->
+                val weight = let {
+
+                    // inner heuristic is manhattan distance. No 5x factor needed because it is simple movement.
+                    val innerHeuristic = { innerPos: Coord -> innerPos.manhattanDistance(neighbor).toDouble() }
+
+                    // edges for the inner A* uses same Coord.adjacent() function, but passes the goalData location
+                    // as a coordinate to exclude from the list of edges.
+                    val innerMove = { innerPos: Coord -> innerPos.adjacent(pos).map { Graph.Edge(it, 1.0) } }
+
+                    // runs A* to find the shortest distance (ie, weight) from the current empty spot to the spot
+                    // where the goal data will move.
+                    Graph.aStar(
+                        startId = empty,
+                        heuristic = innerHeuristic,
+                        defaultEdges = innerMove,
+                    ).steps()
+                } + 1.0 // add 1 to move the goal data into the empty spot
+
+                // the empty spot is now where the goal data used to be
+                Graph.Edge(neighbor to pos, weight)
+            }
+        }
+
+        // Runs the outer A*!
+        return Graph.aStar(
+            startId = goalData to empty,
+            heuristic = heuristic,
+            defaultEdges = move,
+        ).steps()
+    }
 }
 
 fun main() = Day.runDay(22, 2016, Y2016D22::class)
 
-//Class creation: 32ms
-//Part 1: 924 (20ms)
-//Part 2: 213, done by hand, as intended by puzzle author (see https://www.reddit.com/r/adventofcode/comments/5jor9q/comment/dbhwg4l/?utm_source=share&utm_medium=web2x&context=3) (0ms)
-//Total time: 53ms
+//Class creation: 28ms
+//Part 1: 924 (19ms)
+//Part 2: 213 (16ms)
+//Total time: 64ms
