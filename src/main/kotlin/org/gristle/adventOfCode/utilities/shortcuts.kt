1d394a58cc4afbaea493678ca7ed3891f4f1c264
Neil Banman <neil.banman@gmail.com>
2023-02-26 23:20:40 -0800

    added BooleanArray.toInt()

--- src/main/kotlin/org/gristle/adventOfCode/utilities/shortcuts.kt
+++ src/main/kotlin/org/gristle/adventOfCode/utilities/shortcuts.kt
@@ -16,6 +16,17 @@ fun Boolean.print(s: String) {
 }
 
 /**
+ * Returns an Int from a BooleanArray
+ */
+fun BooleanArray.toInt() = foldIndexed(0) { idx, acc, b ->
+    if (b) {
+        acc + (1 shl (size - idx - 1))
+    } else {
+        acc
+    }
+}
+
+/**
  * Returns the actual Int digit (not the ASCII code) of the numeric Char; -1 if not numeric.
  */
 fun Char.toDigit() = Character.getNumericValue(this)
