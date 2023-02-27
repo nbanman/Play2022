f1ab9699da87670d6d507d9a9daad9600783515e
Neil Banman <neil.banman@gmail.com>
2023-02-27 09:56:38 -0800

    implement Day, tweak parsing

--- src/main/kotlin/org/gristle/adventOfCode/y2021/d6/Y2021D6.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2021/d6/Y2021D6.kt
@@ -1,46 +1,42 @@
 package org.gristle.adventOfCode.y2021.d6
 
-import org.gristle.adventOfCode.utilities.elapsedTime
-import org.gristle.adventOfCode.utilities.readRawInput
+import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.utilities.getInts
 
-class Y2021D6(input: String) {
+class Y2021D6(input: String) : Day {
 
     private val fish = input
-        .split(',')
-        .map(String::toInt)
-        .groupBy { it }
+        .getInts()
+        .groupingBy { it }
+        .eachCount()
 
     // initialize count of fish by day
     private val fishByDays = List(9) { i ->
-        fish[i]?.size?.toLong() ?: 0L
+        fish[i]?.toLong() ?: 0L
     }
 
-    fun solve(days: Int): Long {
-
-        fun List<Long>.propagate() = List(9) { i ->
-            when (i) {
-                6 -> get(0) + get(7)
-                8 -> get(0)
-                else -> get(i + 1)
-            }
+    // iterate one day
+    private fun List<Long>.propagate() = List(9) { i ->
+        when (i) {
+            6 -> get(0) + get(7)
+            8 -> get(0)
+            else -> get(i + 1)
         }
-
-        return (1..days)
-            .fold(fishByDays) { acc, _ -> acc.propagate() }
-            .sum()
     }
-    
-    fun part1() = solve(80)
 
-    fun part2() = solve(256)
+    fun solve(days: Int): Long = generateSequence(fishByDays) { it.propagate() }
+        .take(days + 1)
+        .last()
+        .sum()
+
+    override fun part1() = solve(80)
+
+    override fun part2() = solve(256)
 }
 
-fun main() {
-    var time = System.nanoTime()
-    val c = Y2021D6(readRawInput("y2021/d6"))
-    println("Class creation: ${elapsedTime(time)}ms")
-    time = System.nanoTime()
-    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // 361169
-    time = System.nanoTime()
-    println("Part 2: ${c.part2()} (${elapsedTime(time)}ms)") // 1634946868992
-}
+fun main() = Day.runDay(6, 2021, Y2021D6::class)
+
+//    Class creation: 16ms
+//    Part 1: 361169 (0ms)
+//    Part 2: 1634946868992 (0ms)
+//    Total time: 17ms
