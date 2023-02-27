098f76ac763360ccb631ffeb8c4053984ad9c6da
Neil Banman <neil.banman@gmail.com>
2023-02-26 22:34:39 -0800

    implemented Day

--- src/main/kotlin/org/gristle/adventOfCode/y2021/d1/Y2021D1.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2021/d1/Y2021D1.kt
@@ -1,24 +1,22 @@
 package org.gristle.adventOfCode.y2021.d1
 
-import org.gristle.adventOfCode.utilities.Stopwatch
-import org.gristle.adventOfCode.utilities.readRawInput
+import org.gristle.adventOfCode.Day
 
-class Y2021D1(input: String) {
+class Y2021D1(input: String) : Day {
     private val measurements = input.lines().map(String::toInt)
 
     private fun List<Int>.countIncreased() = zipWithNext().count { (a, b) -> a < b }
 
-    fun part1() = measurements.countIncreased()
+    override fun part1() = measurements.countIncreased()
 
-    fun part2() = measurements
+    override fun part2() = measurements
         .windowed(3) { it.sum() }
         .countIncreased()
 }
 
-fun main() {
-    val timer = Stopwatch(true)
-    val c = Y2021D1(readRawInput("y2021/d1"))
-    println("Class creation: ${timer.lap()}ms")
-    println("Part 1: ${c.part1()} (${timer.lap()}ms)") // 1342
-    println("Part 2: ${c.part2()} (${timer.lap()}ms)") // 1378
-}
+fun main() = Day.runDay(1, 2021, Y2021D1::class)
+
+//    Class creation: 30ms
+//    Part 1: 1342 (0ms)
+//    Part 2: 1378 (5ms)
+//    Total time: 37ms
