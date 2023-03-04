ba2661a4bd5ccfbcd9fb006271076a0a10f0a29c
Neil Banman <neil.banman@gmail.com>
2023-03-03 16:01:04 -0800

    finished comments

--- src/main/kotlin/org/gristle/adventOfCode/y2021/d14/Y2021D14.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2021/d14/Y2021D14.kt
@@ -1,56 +1,47 @@
 package org.gristle.adventOfCode.y2021.d14
 
 import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.utilities.blankSplit
 import org.gristle.adventOfCode.utilities.minMax
 
+private typealias PropagationRules = Map<String, Pair<String, String>>
 private typealias ProteinPairs = Map<String, Long>
 
 class Y2021D14(input: String) : Day {
-
-    // propagation rules. Defined in init block because there is an intermediate parsing step for both rules and 
-    // allProteins that does not need to be saved. The solving algorithm counts number of neighboring proteins.
-    // Thus, the insertion rule is represented by a pair of strings. "AB => C" becomes "AC" to "CB"
-    private val rules: Map<String, Pair<String, String>>
-
-    // List of all the proteins for the algorithm to iterate through
-    private val allProteins: List<Char>
-
-    init {
-        // intermediate parsing step drops the first line and only considers letters
-        val letters = input.dropWhile { it != '\n' }.filter { it.isLetter() }.toList()
-
-        rules = buildMap {
-            letters
-                .chunked(3)
-                .forEach { (a, b, c) -> put("$a$b", "$a$c" to "$c$b") }
-        }
-
-        allProteins = letters.distinct()
-    }
-
-    // Initial polymer state represented as a map of pairs of proteins along with the number of times the pair
-    // exists in the String. Defined in init block because there is an intermediate parsing step for both proteinPairs
-    // and firstAndLast that does not need to be saved.
     private val proteinPairs: ProteinPairs
-
-    // The main algorithm does not properly count proteins that are at the ends of the polymer, so those proteins
-    // need to be retained.
     private val firstAndLast: String
-
+    private val rules: PropagationRules
     init {
-        // intermediate parsing step, the first line of the input
-        val polymerTemplate = input.takeWhile { it != '\n' }
 
+        // Intermediate parsing step for defining above values, splitting the template input from the rules input
+        val (polymerTemplate, rulesInput) = input.blankSplit()
+
+        // Initial polymer state represented as a map of pairs of proteins along with the number of times the pair
+        // exists in the String.    
         proteinPairs = buildMap {
             polymerTemplate.windowed(2).forEach { key ->
                 this[key] = (this[key] ?: 0L) + 1 // bumps count
             }
         }
 
+        // The main algorithm does not properly count proteins that are at the ends of the polymer, so those proteins
+        // need to be retained.
         firstAndLast = "${polymerTemplate.first()}${polymerTemplate.last()}"
+
+        // Propagation rules. The solving algorithm counts number of neighboring proteins.
+        // Thus, the insertion rule is represented by a pair of strings. "AB => C" becomes "AC" to "CB"
+        rules = buildMap {
+            rulesInput // start with input string
+                .filter { it.isLetter() } // discard formatting and only look at "proteins"
+                .toList() // convert to List<Char> for nifty destructuring
+                .chunked(3) // groups of 3 proteins
+                .forEach { (a, b, c) -> put("$a$b", "$a$c" to "$c$b") } // assign values to map
+        }
     }
 
-    // runs a single step, returning a map with updated pair counts
+    /**
+     * Runs a single step, returning a map with updated pair counts.
+     */
     private fun ProteinPairs.step(): Map<String, Long> {
         val newPairs = mutableMapOf<String, Long>()
         forEach { (polyPair, amt) ->
@@ -70,18 +61,28 @@ class Y2021D14(input: String) : Day {
      * total proteins. Then the answer is the most common protein minus the least common protein.
      */
     private fun solve(steps: Int): Long {
+
+        // Runs the stepping process n number of times.
         val steppedPairs = (1..steps).fold(proteinPairs) { pairs, _ -> pairs.step() }
 
-        val proteinCount = allProteins.map { protein ->
-            val count = steppedPairs
-                .filter { it.key.contains(protein) }
-                .map { if (it.key[0] == it.key[1]) it.value * 2 else it.value }
-                .sum()
-            val modifier = firstAndLast.count { it == protein }
-            (count + modifier) / 2
+        // Count number of proteins by iterating through the pairs and adding the pair amount for each time the 
+        // protein appears. Note that the value stored is a double count because the same protein is counted in the
+        // first position and then in the last position!
+        val proteinCounts = buildMap {
+            for ((proteins, amt) in steppedPairs) {
+                for (protein in proteins) {
+                    this[protein] = (this[protein] ?: 0L) + amt
+                }
+            }
+
+            // Proteins at the very end are not double-counted, so bump the count for these to make it consistent. 
+            for (protein in firstAndLast) {
+                this[protein] = (this[protein] ?: 0L) + 1L
+            }
         }
 
-        return proteinCount.minMax().let { (min, max) -> max - min }
+        // Grab the highest and lowest values, return the difference (divided by 2 to adjust the double-count).
+        return proteinCounts.values.minMax().let { (min, max) -> (max - min) / 2 }
     }
 
     override fun part1() = solve(10)
@@ -91,7 +92,7 @@ class Y2021D14(input: String) : Day {
 
 fun main() = Day.runDay(14, 2021, Y2021D14::class)
 
-//    Class creation: 15ms
-//    Part 1: 3555 (6ms)
-//    Part 2: 4439442043739 (2ms)
-//    Total time: 24ms
+//    Class creation: 10ms
+//    Part 1: 3555 (4ms)
+//    Part 2: 4439442043739 (3ms)
+//    Total time: 18ms
