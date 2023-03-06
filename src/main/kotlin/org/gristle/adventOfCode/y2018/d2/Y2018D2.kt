418aa6503161760ba0bffea2543c415b662f141d
Neil Banman <neil.banman@gmail.com>
2023-03-06 11:01:39 -0800

    implemented Day

--- src/main/kotlin/org/gristle/adventOfCode/y2018/d2/Y2018D2.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2018/d2/Y2018D2.kt
@@ -1,15 +1,17 @@
 package org.gristle.adventOfCode.y2018.d2
 
-import org.gristle.adventOfCode.utilities.*
+import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.utilities.eachCount
+import org.gristle.adventOfCode.utilities.getPairs
 
-class Y2018D2(input: String) {
+class Y2018D2(input: String) : Day {
     private val boxIds = input.lines()
-    fun part1(): Int {
+    override fun part1(): Int {
         val frequencies = boxIds.map { it.eachCount().values }
         return frequencies.count { it.contains(2) } * frequencies.count { it.contains(3) }
     }
 
-    fun part2(): String {
+    override fun part2(): String {
         fun List<String>.countDifferences() = first()
             .zip(last())
             .count { (first, last) -> first != last }
@@ -24,12 +26,9 @@ class Y2018D2(input: String) {
     }
 }
 
-fun main() {
-    var time = System.nanoTime()
-    val c = Y2018D2(readRawInput("y2018/d2"))
-    println("Class creation: ${elapsedTime(time)}ms")
-    time = System.nanoTime()
-    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // 7688
-    time = System.nanoTime()
-    println("Part 2: ${c.part2()} (${elapsedTime(time)}ms)") // lsrivmotzbdxpkxnaqmuwcchj
-}
+fun main() = Day.runDay(2, 2018, Y2018D2::class)
+
+//    Class creation: 20ms
+//    Part 1: 7688 (7ms)
+//    Part 2: lsrivmotzbdxpkxnaqmuwcchj (40ms)
+//    Total time: 68ms
