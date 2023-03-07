64ca4b12d04eb9b92b82910b4be570b40a488b1c
Neil Banman <neil.banman@gmail.com>
2023-03-06 17:46:31 -0800

    10x speedup by switching Register to LongArray and mutating in place. Still too slow!

--- src/main/kotlin/org/gristle/adventOfCode/y2018/d21/Y2018D21.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2018/d21/Y2018D21.kt
@@ -2,7 +2,51 @@ package org.gristle.adventOfCode.y2018.d21
 
 import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.groupValues
-import org.gristle.adventOfCode.y2018.shared.Ops
+
+enum class Ops(val fn: (reg: LongArray, p: Int, a: Int, b: Int, c: Int) -> Unit) {
+    ADDR({ reg, p, a, b, c -> reg.store(p, c, reg[a] + reg[b]) }),
+    ADDI({ reg, p, a, b, c -> reg.store(p, c, reg[a] + b) }),
+    MULR({ reg, p, a, b, c -> reg.store(p, c, reg[a] * reg[b]) }),
+    MULI({ reg, p, a, b, c -> reg.store(p, c, reg[a] * b) }),
+    BANR({ reg, p, a, b, c -> reg.store(p, c, reg[a] and reg[b]) }),
+    BANI({ reg, p, a, b, c -> reg.store(p, c, reg[a] and b.toLong()) }),
+    BORR({ reg, p, a, b, c -> reg.store(p, c, reg[a] or reg[b]) }),
+    BORI({ reg, p, a, b, c -> reg.store(p, c, reg[a] or b.toLong()) }),
+    SETR({ reg, p, a, _, c -> reg.store(p, c, reg[a]) }),
+    SETI({ reg, p, a, _, c -> reg.store(p, c, a.toLong()) }),
+    GTIR({ reg, p, a, b, c -> reg.store(p, c, if (a > reg[b]) 1 else 0) }),
+    GTRI({ reg, p, a, b, c -> reg.store(p, c, if (reg[a] > b) 1 else 0) }),
+    GTRR({ reg, p, a, b, c -> reg.store(p, c, if (reg[a] > reg[b]) 1 else 0) }),
+    EQIR({ reg, p, a, b, c -> reg.store(p, c, if (a.toLong() == reg[b]) 1 else 0) }),
+    EQRI({ reg, p, a, b, c -> reg.store(p, c, if (reg[a] == b.toLong()) 1 else 0) }),
+    EQRR({ reg, p, a, b, c -> reg.store(p, c, if (reg[a] == reg[b]) 1 else 0) });
+
+    companion object {
+        fun from(s: String) = when (s) {
+            "addr" -> ADDR
+            "addi" -> ADDI
+            "mulr" -> MULR
+            "muli" -> MULI
+            "banr" -> BANR
+            "bani" -> BANI
+            "borr" -> BORR
+            "bori" -> BORI
+            "setr" -> SETR
+            "seti" -> SETI
+            "gtir" -> GTIR
+            "gtri" -> GTRI
+            "gtrr" -> GTRR
+            "eqir" -> EQIR
+            "eqri" -> EQRI
+            else -> EQRR
+        }
+    }
+}
+
+private fun LongArray.store(pointer: Int, location: Int, result: Long) {
+    this[location] = result
+    this[pointer]++
+}
 
 class Y2018D21(private val input: String) : Day {
     private val pattern = """([a-z]{4}) (\d+) (\d+) (\d+)""".toRegex()
@@ -15,57 +59,36 @@ class Y2018D21(private val input: String) : Day {
         val c: Int,
         val lineNo: Int,
     ) {
-        fun execute(reg: List<Long>) = op.fn(reg, p, a, b, c)
+        fun execute(reg: LongArray) = op.fn(reg, p, a, b, c)
     }
 
-    fun solve(): Pair<Long, Long> {
-        val data = input
-        val p = data.takeWhile { it !in "\r\n" }.takeLast(1).toInt()
-        val commands = data
+    fun solve(part2: Boolean = false): Long {
+        val p = input.takeWhile { it !in "\r\n" }.takeLast(1).toInt()
+        val commands = input
             .groupValues(pattern)
             .mapIndexed { index, gv ->
-                val ops = when (gv[0]) {
-                    "addr" -> Ops.ADDR
-                    "addi" -> Ops.ADDI
-                    "mulr" -> Ops.MULR
-                    "muli" -> Ops.MULI
-                    "banr" -> Ops.BANR
-                    "bani" -> Ops.BANI
-                    "borr" -> Ops.BORR
-                    "bori" -> Ops.BORI
-                    "setr" -> Ops.SETR
-                    "seti" -> Ops.SETI
-                    "gtir" -> Ops.GTIR
-                    "gtri" -> Ops.GTRI
-                    "gtrr" -> Ops.GTRR
-                    "eqir" -> Ops.EQIR
-                    "eqri" -> Ops.EQRI
-                    else -> Ops.EQRR
-                }
-                Command(ops, p, gv[1].toInt(), gv[2].toInt(), gv[3].toInt(), index)
+                Command(Ops.from(gv[0]), p, gv[1].toInt(), gv[2].toInt(), gv[3].toInt(), index)
             }
 
-        var register = listOf(0L, 0L, 0L, 0L, 0L, 0L)
+        val register = longArrayOf(0L, 0L, 0L, 0L, 0L, 0L)
         val r1Set = mutableSetOf<Long>()
-        var p1 = 0L
         while (true) {
             val command = commands[register[p].toInt()]
-            register = command
-                .execute(register)
+            command.execute(register)
             if (command.lineNo == 28) {
-                if (r1Set.isEmpty()) p1 = register[1]
-                if (r1Set.contains(register[1])) {
-                    return p1 to r1Set.last()
-                } else r1Set.add(register[1])
+                if (part2) {
+                    if (r1Set.contains(register[1])) return r1Set.last()
+                } else {
+                    return register[1]
+                }
+                r1Set.add(register[1])
             }
         }
     }
 
-    val solution = solve()
+    override fun part1() = solve()
 
-    override fun part1() = solution.first
-
-    override fun part2() = solution.second
+    override fun part2() = solve(true)
 }
 
 fun main() = Day.runDay(Y2018D21::class)
