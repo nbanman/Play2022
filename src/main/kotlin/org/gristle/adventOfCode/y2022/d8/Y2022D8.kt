dc2e3e7243ad35fcdc6e97f5563c63073f722f39
Neil Banman <neil.banman@gmail.com>
2023-03-15 10:00:56 -0700

    tweaks

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d8/Y2022D8.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d8/Y2022D8.kt
@@ -7,13 +7,14 @@ import org.gristle.adventOfCode.utilities.toGrid
 
 class Y2022D8(input: String) : Day {
 
-    // Representations of the positions and heights of all the trees in the forest. 
-    // Don't mistake these two variables for each other!
-    private val forest = input.toGrid()
-    private val treeHeights = forest.coords().associateWith { forest[it] }
+    // Representations of the positions and heights of all the trees in the forest.
+    private val treeHeights = let {
+        val forest = input.toGrid()
+        forest.coords().associateWith { forest[it] }
+    }
 
     // utility function that makes the coordinates aware of whether they are in the forest
-    private val Coord.outOfForest: Boolean get() = !treeHeights.containsKey(this)
+    private fun Coord.outOfForest(): Boolean = !treeHeights.containsKey(this)
 
     // for a given position, provide a list of sequences that generate coordinates radiating away from the position
     // in each of the four directions
@@ -24,31 +25,31 @@ class Y2022D8(input: String) : Day {
     // determines whether a sequence should be terminated, returning true if the position is out of the forest 
     // or if the tree at the position blocks the starting tree's line of sight (LOS).
     private fun terminating(pos: Coord, tree: Coord): Boolean {
-        val posHeight = treeHeights[pos] ?: return true
+        val posHeight = treeHeights[pos] ?: return true // null means out of forest, so return true
         return posHeight >= treeHeights.getValue(tree)
     }
 
     // for a given position, checks all directions and returns true if *any* allow LOS out of the forest
     private fun Coord.isVisible(): Boolean {
         val tree = this // unnecessary but provides semantic value
-        return rays(tree).any { ray ->
-            ray
+        return rays(tree).any {
+            it
                 // keep delivering coordinates until out of forest or LOS blocked
                 .first { pos -> terminating(pos, tree) }
-                .outOfForest // true if made it out of forest before LOS blocked
+                .outOfForest() // true if made it out of forest before LOS blocked
         }
     }
 
     // for a given position, counts number of visible trees in each direction, then multiplies them together
     private fun scenicScore(treehouse: Coord) = rays(treehouse)
-        .map { ray ->
-            ray
+        .map {
+            it
                 // need to track both the position and the index because the index will be used to count the trees
                 // and the position will be used to add 1 if LOS is blocked (since a blocking tree 
                 // should be counted).    
                 .withIndex()
                 .first { (_, pos) -> terminating(pos, treehouse) }
-                .let { (index, pos) -> index + if (pos.outOfForest) 0 else 1 }
+                .let { (index, pos) -> index + if (pos.outOfForest()) 0 else 1 }
         }.reduce(Int::times)
 
     override fun part1(): Int = treeHeights.keys.count { tree -> tree.isVisible() }
@@ -57,3 +58,8 @@ class Y2022D8(input: String) : Day {
 }
 
 fun main() = Day.runDay(Y2022D8::class) // 1708, 504000
+
+//    Class creation: 141ms
+//    Part 1: 1708 (106ms)
+//    Part 2: 504000 (59ms)
+//    Total time: 307ms
