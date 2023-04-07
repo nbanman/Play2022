e54fa908c809b2f6e82aaa7be42065daa705f796
Neil Banman <neil.banman@gmail.com>
2023-04-07 13:08:17 -0700

    added Int.isPrime function

--- src/main/kotlin/org/gristle/adventOfCode/utilities/math.kt
+++ src/main/kotlin/org/gristle/adventOfCode/utilities/math.kt
@@ -1,5 +1,7 @@
 package org.gristle.adventOfCode.utilities
 
+import kotlin.math.sqrt
+
 fun Sequence<Pair<Long, Long>>.crt(): Long {
     val n = fold(1L) { acc, (mod, _) -> acc * mod }
     val bigPhase = sumOf { (mod, remainder) ->
@@ -7,4 +9,16 @@ fun Sequence<Pair<Long, Long>>.crt(): Long {
         remainder * ni * (ni.toBigInteger().modInverse(remainder.toBigInteger())).toLong()
     }
     return bigPhase % n
+}
+
+fun Int.isPrime(): Boolean = when {
+    this <= 1 -> false
+    this <= 3 -> true
+    this % 2 == 0 || this % 3 == 0 -> false
+    else -> {
+        val limit = sqrt(this.toFloat()).toInt()
+        generateSequence(5) { it + 6 }
+            .takeWhile { it <= limit }
+            .none { this % it == 0 || this % (it + 2) == 0 }
+    }
 }
