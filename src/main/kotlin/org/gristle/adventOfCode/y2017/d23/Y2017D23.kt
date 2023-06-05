b59f39f62971840d8fd6953f6c97573924e64b00
Neil Banman <neil.banman@gmail.com>
2023-06-05 14:38:14 -0700

    streamlined parsing, valueOf fun

--- src/main/kotlin/org/gristle/adventOfCode/y2017/d23/Y2017D23.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2017/d23/Y2017D23.kt
@@ -1,26 +1,23 @@
 package org.gristle.adventOfCode.y2017.d23
 
 import org.gristle.adventOfCode.Day
-import org.gristle.adventOfCode.utilities.groupValues
 import org.gristle.adventOfCode.utilities.isPrime
-import org.gristle.adventOfCode.y2017.d18.Y2017D18
+import org.gristle.adventOfCode.y2017.shared.Command
 
 // not refactored! ugly!
 class Y2017D23(input: String) : Day {
 
-    private val pattern = """(\w{3}) (-?\w+) (-?\w+)""".toRegex()
-
     val commands = input
-        .groupValues(pattern)
-        .map { Y2017D18.Command(it[0], it[1], it[2]) }
+        .split(' ', '\n')
+        .chunked(3)
+        .map { (name, arg1, arg2) -> Command(name, arg1, arg2) }
 
     private var registers = mutableMapOf<String, Long>()
 
-    private fun valueOf(arg: String) = if (arg.last().isDigit()) {
-        arg.toLong()
-    } else {
-        registers[arg] ?: 0L
-    }
+    private fun valueOf(arg: String) = arg
+        .toLongOrNull()
+        ?: registers[arg]
+        ?: 0L
 
     override fun part1(): Int {
         var index = 0
@@ -52,7 +49,6 @@ class Y2017D23(input: String) : Day {
     }
 
     override fun part2(): Int {
-        // Part 2
         val b = commands.first().arg2.toInt() * 100 + 100_000
         return (b..b + 17_000 step 17).count {
             !it.isPrime()
