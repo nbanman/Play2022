e9296d53204b53376e0f69120d158ae8d926bfbb
Neil Banman <neil.banman@gmail.com>
2023-06-06 17:11:08 -0700

    10x speedup with simpler code, using primitive arrays instead of MutableGrid

--- src/main/kotlin/org/gristle/adventOfCode/y2015/d6/Y2015D6.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2015/d6/Y2015D6.kt
@@ -1,119 +1,69 @@
 package org.gristle.adventOfCode.y2015.d6
 
 import org.gristle.adventOfCode.Day
-import org.gristle.adventOfCode.utilities.Coord
-import org.gristle.adventOfCode.utilities.MutableGrid
-import org.gristle.adventOfCode.utilities.groupValues
+import org.gristle.adventOfCode.utilities.getIntList
 import kotlin.math.max
 
 class Y2015D6(input: String) : Day {
 
-    /**
-     * Defines the suite of  three functions that alter the lights. Defined as an interface because the functions
-     * for part 1 and part 2 work differently, so will plug in different implementations for each part. They work
-     * on a MutableList so all return Unit.
-     */
-    interface LightAdjustment {
-        fun on(lights: MutableGrid<Int>, location: Coord)
-        fun off(lights: MutableGrid<Int>, location: Coord)
-        fun toggle(lights: MutableGrid<Int>, location: Coord)
+    data class Instruction(val command: Int, val x1: Int, val y1: Int, val x2: Int, val y2: Int) {
+        companion object {
+            fun of(line: String): Instruction {
+                val command = when {
+                    line.startsWith("turn on") -> 1
+                    line.startsWith("turn off") -> -1
+                    line.startsWith("toggle") -> 0
+                    else -> throw IllegalArgumentException("Error parsing input: $line")
+                }
+                val (x1, y1, x2, y2) = line.getIntList()
+                return Instruction(command, x1, y1, x2, y2)
+            }
+        }
     }
 
-    /**
-     * Stores the range to execute the particular instruction and executes the instruction. Comes in three 
-     * different flavors that execute different functions: On, Off, and Toggle. 
-     */
-    class Instruction(
-        private val topLeft: Coord,
-        private val bottomRight: Coord,
-        private val getAdjuster: (LightAdjustment) -> (MutableGrid<Int>, Coord) -> Unit
-    ) {
+    val instructions = input
+        .lineSequence()
+        .map(Instruction::of)
+        .toList()
 
-        /**
-         * Adjusts the light field in accordance with the instruction and the given range.
-         */
-        fun execute(lights: MutableGrid<Int>, lightAdjustment: LightAdjustment) {
-            val adjust = getAdjuster(lightAdjustment)
-            Coord.forRectangle(topLeft, bottomRight) { location -> adjust(lights, location) }
-        }
-        
-        companion object {
-            /**
-             * Provides the appropriate instruction object.
-             */
-            fun of(groupValues: List<String>): Instruction {
-                val coords = groupValues.drop(1).map { it.toInt() }
-                val topLeft = Coord(coords[0], coords[1])
-                val bottomRight = Coord(coords[2], coords[3])
-                
-                return when (groupValues[0]) {
-                    "turn on" -> Instruction(topLeft, bottomRight) { it::on }
-                    "turn off" -> Instruction(topLeft, bottomRight) { it::off }
-                    else -> Instruction(topLeft, bottomRight) { it::toggle }
+    val length = 1_000
+
+    override fun part1(): Int {
+        val lights = BooleanArray(length * length)
+        fun BooleanArray.execute(instruction: Instruction) {
+            for (y in instruction.y1..instruction.y2) for (x in instruction.x1..instruction.x2) {
+                val index = y * length + x
+                this[index] = when (instruction.command) {
+                    1 -> true
+                    -1 -> false
+                    else -> !this[index]
                 }
             }
         }
+        instructions.forEach { lights.execute(it) }
+        return lights.count { it }
     }
 
-    /**
-     * Parses input into List of Instructions
-     */
-    private val instructions = input
-        .groupValues("""(turn (?:on|off)|toggle) (\d+),(\d+) through (\d+),(\d+)""")
-        .map { Instruction.of(it) }
-
-    /**
-     * Algo for solving both parts. Creates a light field as a mutable list. Executes each instruction on the
-     * light field. Returns the sum of the brightness.
-     */
-    fun solve(lightAdjustment: LightAdjustment): Int {
-        val lights = MutableGrid(1000, 1000) { 0 }
-        instructions.forEach { it.execute(lights, lightAdjustment) }
+    override fun part2(): Int {
+        val lights = IntArray(length * length)
+        fun IntArray.execute(instruction: Instruction) {
+            for (y in instruction.y1..instruction.y2) for (x in instruction.x1..instruction.x2) {
+                val index = y * length + x
+                this[index] = when (instruction.command) {
+                    1 -> this[index] + 1
+                    -1 -> max(0, this[index] - 1)
+                    else -> this[index] + 2
+                }
+            }
+        }
+        instructions.forEach { lights.execute(it) }
         return lights.sum()
     }
-
-    /**
-     * Runs solve algo with part 1-specific adjustment functions.
-     */
-    override fun part1() = solve(
-        object : LightAdjustment {
-            override fun on(lights: MutableGrid<Int>, location: Coord) {
-                lights[location] = 1
-            }
-
-            override fun off(lights: MutableGrid<Int>, location: Coord) {
-                lights[location] = 0
-            }
-
-            override fun toggle(lights: MutableGrid<Int>, location: Coord) {
-                lights[location] = if (lights[location] == 1) 0 else 1
-            }
-        }
-    )
-
-    /**
-     * Runs solve algo with part 2-specific adjustment functions.
-     */
-    override fun part2() = solve(
-        object : LightAdjustment {
-            override fun on(lights: MutableGrid<Int>, location: Coord) {
-                lights[location]++
-            }
-
-            override fun off(lights: MutableGrid<Int>, location: Coord) {
-                lights[location] = max(0, lights[location] - 1)
-            }
-
-            override fun toggle(lights: MutableGrid<Int>, location: Coord) {
-                lights[location] += 2
-            }
-        }
-    )
 }
 
 fun main() = Day.runDay(Y2015D6::class)
 
-//    Class creation: 29ms
-//    Part 1: 569999 (520ms)
-//    Part 2: 17836115 (465ms)
-//    Total time: 1015ms
+//    Class creation: 28ms
+//    Part 1: 569999 (34ms)
+//    Part 2: 17836115 (37ms)
+//    Total time: 100ms
