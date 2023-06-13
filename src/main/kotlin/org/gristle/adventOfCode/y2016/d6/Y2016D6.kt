b8b2606dca2611ff47cfcd3cd9c4f866b502bb92
Neil Banman <neil.banman@gmail.com>
2023-06-13 10:38:32 -0700

    added time comments

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d6/Y2016D6.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d6/Y2016D6.kt
@@ -11,4 +11,9 @@ class Y2016D6(input: String) : Day {
     override fun part2() = columns.map { column -> column.minBy { it.value }.key }.joinToString("")
 }
 
-fun main() = Day.runDay(Y2016D6::class) // asvcbhvg, odqnikqv
+fun main() = Day.runDay(Y2016D6::class)
+
+//    Class creation: 25ms
+//    Part 1: asvcbhvg (0ms)
+//    Part 2: odqnikqv (0ms)
+//    Total time: 25ms
