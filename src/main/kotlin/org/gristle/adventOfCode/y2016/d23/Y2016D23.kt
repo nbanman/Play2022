fcf43d29b7a7839e9ee1bd901d2eda9c488479ea
Neil Banman <neil.banman@gmail.com>
2023-06-17 13:56:05 -0700

    uses new Assembunny code

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d23/Y2016D23.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d23/Y2016D23.kt
@@ -1,29 +1,22 @@
 package org.gristle.adventOfCode.y2016.d23
 
 import org.gristle.adventOfCode.Day
-import org.gristle.adventOfCode.utilities.groupValues
 import org.gristle.adventOfCode.y2016.shared.Assembunny
-import org.gristle.adventOfCode.y2016.shared.Registers
-import org.gristle.adventOfCode.y2016.shared.runInstructions
 
-class Y2016D23(input: String) : Day {
+class Y2016D23(private val input: String) : Day {
 
-    private val pattern = """(\w+) (-?\w+)(?: (-?\w+))?""".toRegex()
+    override fun part1(): Int {
+        val registers = IntArray(4) { idx -> if (idx == 0) 7 else 0 }
+        return Assembunny(registers)
+            .runInstructions(input)['a']
+    }
 
-    private val instructions = input
-        .groupValues(pattern)
-        .map { (type, arg1, arg2) -> Assembunny(type, arg1, arg2) }
-
-    private val p1Registers = Registers().apply { updateValue("a", 7) }
-
-    override fun part1() = runInstructions(instructions, p1Registers).a
-
-    override fun part2() = (1..12).reduce { acc, i -> acc * i } + 94 * 82
+    override fun part2(): Int = (1..12).reduce(Int::times) + 94 * 82
 }
 
 fun main() = Day.runDay(Y2016D23::class)
 
-//Class creation: 19ms
-//Part 1: 12748 (19ms)
-//Part 2: 479009308 (0ms)
-//Total time: 38ms
+//    Class creation: 2ms
+//    Part 1: 12748 (21ms)
+//    Part 2: 479009308 (0ms)
+//    Total time: 23ms
