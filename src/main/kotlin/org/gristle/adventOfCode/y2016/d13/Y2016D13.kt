abdd447651272db1fb486863f95a0d84cc78225b
Neil Banman <neil.banman@gmail.com>
2023-06-17 14:15:44 -0700

    moved "end" out of function so not endlessly recreated

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d13/Y2016D13.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d13/Y2016D13.kt
@@ -8,20 +8,22 @@ import org.gristle.adventOfCode.utilities.isEven
 import org.gristle.adventOfCode.utilities.isOdd
 
 class Y2016D13(input: String) : Day {
-    private val favoriteNumber = input.toInt()
-
-    private fun Coord.isOpen(): Boolean {
-        if (x < 0 || y < 0) return false
-        val num = (x * x) + (3 * x) + (2 * x * y) + y + (y * y) + favoriteNumber
-        val ones = generateSequence(num) { it.shr(1) }
-            .takeWhile { it > 0 }
-            .count(Int::isOdd)
-        return ones.isEven()
-    }
 
     private val distances: List<Graph.Vertex<Coord>> by lazy {
+        val favoriteNumber = input.toInt()
         val start = Coord(1, 1)
-        val endCondition = { pos: Coord -> pos == Coord(31, 39) }
+        val end = Coord(31, 39)
+        val endCondition = { pos: Coord -> pos == end }
+
+        fun Coord.isOpen(): Boolean {
+            if (x < 0 || y < 0) return false
+            val num = (x * x) + (3 * x) + (2 * x * y) + y + (y * y) + favoriteNumber
+            val ones = generateSequence(num) { it.shr(1) }
+                .takeWhile { it > 0 }
+                .count(Int::isOdd)
+            return ones.isEven()
+        }
+        
         val getEdges = { pos: Coord -> pos.getNeighbors().filter { it.isOpen() } }
         Graph.bfs(start, endCondition, defaultEdges = getEdges)
     }
