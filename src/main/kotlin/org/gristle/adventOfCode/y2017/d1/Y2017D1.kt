6afe906df67c7c1c7040e50dd51d777ffabcbc90
Neil Banman <neil.banman@gmail.com>
2023-06-19 16:49:13 -0700

    refactored redundancy from comparisonIndex

--- src/main/kotlin/org/gristle/adventOfCode/y2017/d1/Y2017D1.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2017/d1/Y2017D1.kt
@@ -6,12 +6,12 @@ class Y2017D1(input: String) : Day {
     private val numbers = input.map(Char::digitToInt)
 
     private inline fun solve(comparisonIndex: List<Int>.(index: Int) -> Int): Int = numbers
-        .filterIndexed { index, i -> numbers[numbers.comparisonIndex(index)] == i }
+        .filterIndexed { index, i -> numbers[(numbers.comparisonIndex(index)) % numbers.size] == i }
         .sum()
 
-    override fun part1() = solve { (it + 1) % size }
+    override fun part1() = solve { it + 1 }
 
-    override fun part2() = solve { (it + size / 2) % size }
+    override fun part2() = solve { it + size / 2 }
 }
 
 fun main() = Day.runDay(Y2017D1::class)
