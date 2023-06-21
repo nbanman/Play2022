61cf713d1f238a4c702188eca1b81bdfb06fd72d
Neil Banman <neil.banman@gmail.com>
2023-06-21 08:47:20 -0700

    2x speedup

--- src/main/kotlin/org/gristle/adventOfCode/y2017/d11/Y2017D11.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2017/d11/Y2017D11.kt
@@ -4,25 +4,14 @@ import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.Hexagon
 
 class Y2017D11(input: String) : Day {
-    private val dirs = input.split(',')
-
-    val solution: Pair<Int, Int> = let {
-        val home = Hexagon()
-        dirs
-            .fold(home to 0) { (hex, furthest), step ->
-                val intermediate = hex.hexAt(step)
-                intermediate to maxOf(furthest, intermediate.distance(home))
-            }.let { (hex, furthest) -> hex.distance(home) to furthest }
-    }
-
-    override fun part1() = solution.first
-
-    override fun part2() = solution.second
+    private val path: List<Hexagon> by lazy { input.split(',').runningFold(Hexagon(), Hexagon::hexAt) }
+    override fun part1() = path.last().distance()
+    override fun part2() = path.maxOf(Hexagon::distance)
 }
 
 fun main() = Day.runDay(Y2017D11::class)
 
-//    Class creation: 19ms
-//    Part 1: 747 (0ms)
-//    Part 2: 1544 (0ms)
-//    Total time: 19ms
+//    Class creation: 2ms
+//    Part 1: 747 (4ms)
+//    Part 2: 1544 (2ms)
+//    Total time: 9ms
