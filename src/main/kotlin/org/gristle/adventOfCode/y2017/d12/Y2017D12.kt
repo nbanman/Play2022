b1025810ef57d37dbeb55f640c956a90636d7527
Neil Banman <neil.banman@gmail.com>
2023-06-21 11:55:15 -0700

    refactored allLinks (faster)

--- src/main/kotlin/org/gristle/adventOfCode/y2017/d12/Y2017D12.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2017/d12/Y2017D12.kt
@@ -5,39 +5,39 @@ import org.gristle.adventOfCode.utilities.getIntList
 
 class Y2017D12(input: String) : Day {
 
-    companion object {
-        fun allLinks(links: Map<Int, List<Int>>, seed: Int): List<Int> {
-
-            tailrec fun aL(found: Set<Int>, evaluate: Set<Int>): Set<Int> {
-                return if (evaluate.isEmpty()) {
-                    found
-                } else {
-                    val newFound = found + evaluate
-                    val newRegisters = evaluate.fold(listOf<Int>()) { acc, i ->
-                        acc + links.getValue(i).filter { it !in newFound }
-                    }.toSet()
-                    aL(found + evaluate, newRegisters)
-                }
-            }
-            return aL(emptySet(), setOf(seed)).toList()
-        }
+    private val programs: Map<Int, List<Int>> by lazy {
+        input
+            .lineSequence()
+            .map { it.getIntList() }
+            .associate { it.first() to it.drop(1) }
     }
 
-    private val links: Map<Int, List<Int>> = input
-        .lineSequence()
-        .map { it.getIntList() }
-        .associate { it.first() to it.drop(1) }
+    // simple BFS
+    private fun allLinks(start: Int, programs: Map<Int, List<Int>>): Set<Int> {
+        val visited = mutableSetOf<Int>()
+        val queue = ArrayDeque<Int>().apply { add(start) }
+        while (queue.isNotEmpty()) {
+            val current = queue.removeFirst()
+            visited.add(current)
+            queue.addAll(programs.getValue(current).subtract(visited))
+        }
+        return visited
+    }
 
-    override fun part1() = allLinks(links, 0).size
+    override fun part1() = allLinks(0, programs).size
 
-    override fun part2() = generateSequence((0..1999).toMutableSet()) {
-        it.apply { removeAll(allLinks(links, it.first()).toSet()) }
-    }.indexOfFirst { it.isEmpty() }
+    override fun part2(): Int {
+        fun removeGroup(programSet: MutableSet<Int>): MutableSet<Int> =
+            programSet.apply { removeAll(allLinks(programSet.first(), programs)) }
+
+        return generateSequence(programs.keys.toMutableSet(), ::removeGroup)
+            .indexOfFirst { it.isEmpty() }
+    }
 }
 
 fun main() = Day.runDay(Y2017D12::class)
 
-//    Class creation: 66ms
-//    Part 1: 115 (2ms)
-//    Part 2: 221 (15ms)
-//    Total time: 83ms
+//    Class creation: 2ms
+//    Part 1: 115 (26ms)
+//    Part 2: 221 (10ms)
+//    Total time: 40ms
