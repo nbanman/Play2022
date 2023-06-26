eb8f03473f15743ff1510161496ca24eddd6cdf0
Neil Banman <neil.banman@gmail.com>
2023-06-26 13:52:25 -0700

    used Nsew instead of String for direction

--- src/main/kotlin/org/gristle/adventOfCode/y2017/d19/Y2017D19.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2017/d19/Y2017D19.kt
@@ -2,6 +2,7 @@ package org.gristle.adventOfCode.y2017.d19
 
 import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.Grid
+import org.gristle.adventOfCode.utilities.Nsew
 import org.gristle.adventOfCode.utilities.toGrid
 
 class Y2017D19(private val input: String) : Day {
@@ -9,7 +10,7 @@ class Y2017D19(private val input: String) : Day {
 
         tailrec fun runMaze(
             index: Int = startIndex,
-            direction: String = "down",
+            direction: Nsew = Nsew.SOUTH,
             report: String = "",
             steps: Int = 0
         ): Pair<String, Int> {
@@ -22,34 +23,33 @@ class Y2017D19(private val input: String) : Day {
             if (neighborIndices.size == 1 && index != startIndex) return newReport to steps + 1
 
             val newIndex = if (spot == '+') {
-                if (direction == "up" || direction == "down") {
+                if (direction == Nsew.NORTH || direction == Nsew.SOUTH) {
                     if ((index - 1) in neighborIndices) index - 1 else index + 1
                 } else { // location is left or right
                     if ((index - maze.width) in neighborIndices) index - maze.width else index + maze.width
                 }
             } else {
                 when (direction) {
-                    "up" -> index - maze.width
-                    "down" -> index + maze.width
-                    "left" -> index - 1
-                    else -> index + 1
+                    Nsew.NORTH -> index - maze.width
+                    Nsew.SOUTH -> index + maze.width
+                    Nsew.EAST -> index + 1
+                    Nsew.WEST -> index - 1
                 }
             }
             val newDirection = when (newIndex) {
-                index - 1 -> "left"
-                index + 1 -> "right"
-                index + maze.width -> "down"
-                else -> "up"
+                index - 1 -> Nsew.WEST
+                index + 1 -> Nsew.EAST
+                index + maze.width -> Nsew.SOUTH
+                else -> Nsew.NORTH
             }
             return runMaze(newIndex, newDirection, newReport, steps + 1)
         }
     }
 
-    val solution: Pair<String, Int> = let {
+    val solution: Pair<String, Int> by lazy {
         val maze = "$input ".toGrid()
         val startIndex = maze.indexOfFirst { it != ' ' }
-        val (p1, p2) = Mouse(maze, startIndex).runMaze()
-        p1 to p2
+        Mouse(maze, startIndex).runMaze()
     }
 
     override fun part1() = solution.first
@@ -59,7 +59,7 @@ class Y2017D19(private val input: String) : Day {
 
 fun main() = Day.runDay(Y2017D19::class)
 
-//    Class creation: 58ms
-//    Part 1: EOCZQMURF (0ms)
+//    Class creation: 2ms
+//    Part 1: EOCZQMURF (56ms)
 //    Part 2: 16312 (0ms)
 //    Total time: 58ms
