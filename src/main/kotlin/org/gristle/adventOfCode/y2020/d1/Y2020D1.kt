dae53dbfaa4371ffad37c54d99dd7c113f131815
Neil Banman <neil.banman@gmail.com>
2023-07-01 11:57:56 -0700

    changed getPairs() to getPairSequence()

--- src/main/kotlin/org/gristle/adventOfCode/y2020/d1/Y2020D1.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2020/d1/Y2020D1.kt
@@ -1,7 +1,7 @@
 package org.gristle.adventOfCode.y2020.d1
 
 import org.gristle.adventOfCode.Day
-import org.gristle.adventOfCode.utilities.getPairs
+import org.gristle.adventOfCode.utilities.getPairSequence
 
 class Y2020D1(input: String) : Day {
 
@@ -13,7 +13,7 @@ class Y2020D1(input: String) : Day {
         .let { it * (2020 - it) } // multiply the two together
 
     override fun part2() = entries
-        .getPairs() // convert entries into a list of paired combinations of entries
+        .getPairSequence() // convert entries into a list of paired combinations of entries
         // find first pair where there is another entry that adds to 2020
         .first { (first, second) -> entrySet.contains(2020 - first - second) }
         .let { (first, second) -> first * second * (2020 - first - second) } // multiply the three together
@@ -21,7 +21,7 @@ class Y2020D1(input: String) : Day {
 
 fun main() = Day.runDay(Y2020D1::class)
 
-//    Class creation: 22ms
+//    Class creation: 6ms
 //    Part 1: 1015476 (0ms)
-//    Part 2: 200878544 (6ms)
-//    Total time: 28ms
+//    Part 2: 200878544 (4ms)
+//    Total time: 11ms
