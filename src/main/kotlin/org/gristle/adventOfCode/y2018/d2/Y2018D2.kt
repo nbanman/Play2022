58283cd52eae619a59a5f3779db22da71fcf49ef
Neil Banman <neil.banman@gmail.com>
2023-07-01 11:47:29 -0700

    2x speedup by using functions that quit early when answer found

--- src/main/kotlin/org/gristle/adventOfCode/y2018/d2/Y2018D2.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2018/d2/Y2018D2.kt
@@ -2,33 +2,48 @@ package org.gristle.adventOfCode.y2018.d2
 
 import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.eachCount
-import org.gristle.adventOfCode.utilities.getPairs
+import org.gristle.adventOfCode.utilities.getPairSequence
 
 class Y2018D2(input: String) : Day {
+
     private val boxIds = input.lines()
+
     override fun part1(): Int {
         val frequencies = boxIds.map { it.eachCount().values }
         return frequencies.count { it.contains(2) } * frequencies.count { it.contains(3) }
     }
 
     override fun part2(): String {
-        fun List<String>.countDifferences() = first()
-            .zip(last())
-            .count { (first, last) -> first != last }
+        fun Pair<String, String>.differsByOne(): Boolean {
+            var diffs = false
+            for (idx in first.indices) {
+                if (first[idx] != second[idx]) {
+                    if (!diffs) {
+                        diffs = true
+                    } else {
+                        return false
+                    }
+                }
+            }
+            return diffs
+        }
 
-        fun List<String>.shared() = buildString {
-            this@shared.first().forEachIndexed { index, a ->
-                if (a == this@shared.last()[index]) append(a)
+        fun Pair<String, String>.shared() = buildString {
+            this@shared.first.forEachIndexed { index, a ->
+                if (a == this@shared.second[index]) append(a)
             }
         }
 
-        return boxIds.getPairs().first { it.countDifferences() == 1 }.shared()
+        return boxIds
+            .getPairSequence()
+            .first { it.differsByOne() }
+            .shared()
     }
 }
 
 fun main() = Day.runDay(Y2018D2::class)
 
-//    Class creation: 20ms
-//    Part 1: 7688 (7ms)
-//    Part 2: lsrivmotzbdxpkxnaqmuwcchj (40ms)
-//    Total time: 68ms
+//    Class creation: 9ms
+//    Part 1: 7688 (8ms)
+//    Part 2: lsrivmotzbdxpkxnaqmuwcchj (14ms)
+//    Total time: 31ms
