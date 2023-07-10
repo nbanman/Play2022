5e069b8179c104212ca4b0d1d0a1957806becc22
Neil Banman <neil.banman@gmail.com>
2023-07-10 10:43:07 -0700

    6x speedup by using IntArray instead of Grid and counting rather than tracking individual claims

--- src/main/kotlin/org/gristle/adventOfCode/y2018/d3/Y2018D3.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2018/d3/Y2018D3.kt
@@ -2,49 +2,39 @@ package org.gristle.adventOfCode.y2018.d3
 
 import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.Coord
-import org.gristle.adventOfCode.utilities.Grid
 import org.gristle.adventOfCode.utilities.getInts
 
 class Y2018D3(input: String) : Day {
-
-    data class Claim(val id: Int, val tl: Coord, val size: Coord) {
-        val br = Coord(tl.x + size.x - 1, tl.y + size.y - 1)
-        override fun toString(): String {
-            return "Claim(id=$id, tl=$tl, size=$size, br=$br)"
-        }
-    }
-
     private val claims = input
         .getInts()
-        .chunked(5)
-        .map { Claim(it[0], Coord(it[1], it[2]), Coord(it[3], it[4])) }
+        .chunked(5) { (_, x, y, w, h) -> Coord(x, y) to Coord(x + w - 1, y + h - 1) }
         .toList()
 
-    private val width = claims.maxOf { it.tl.x + it.size.x }
-    private val height = claims.maxOf { it.tl.y + it.size.y }
+    private val width = claims.maxOf { (_, br) -> br.x } + 1
+    private val height = claims.maxOf { (_, br) -> br.y } + 1
 
-    private val skein = Grid<MutableSet<Int>>(width, height) { mutableSetOf() }
-
-    init {
-        claims.forEach { claim ->
-            Coord.forRectangle(claim.tl, claim.br) { skein[it].add(claim.id) }
+    private val skein = IntArray(width * height).apply {
+        claims.forEach { (tl, br) ->
+            Coord.forRectangle(tl, br) {
+                this[it.asIndex(width)]++
+            }
         }
     }
 
+    override fun part1(): Int = skein.count { it > 1 }
 
-    override fun part1() = skein.count { it.size > 1 }
+    override fun part2(): Int = 1 + claims
+        .indexOfFirst { (tl, br) ->
+            Coord.rectangleFrom(tl, br).all { pos ->
+                skein[pos.asIndex(width)] == 1
+            }
+        }
 
-    override fun part2(): Int {
-        val noOverlaps = skein.filter { it.size == 1 }.groupingBy { it.first() }.eachCount()
-        return claims.first { claim ->
-            claim.size.area() == noOverlaps.entries.find { it.key == claim.id }?.value
-        }.id
-    }
 }
 
 fun main() = Day.runDay(Y2018D3::class)
 
-//    Class creation: 148ms
-//    Part 1: 110891 (15ms)
-//    Part 2: 297 (164ms)
-//    Total time: 327ms
+//    Class creation: 32ms
+//    Part 1: 110891 (4ms)
+//    Part 2: 297 (13ms)
+//    Total time: 49ms
