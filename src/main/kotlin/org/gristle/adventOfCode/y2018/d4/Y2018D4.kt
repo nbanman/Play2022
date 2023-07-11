cc3b75cbd4ad5625bb2d285dc236386b6c065678
Neil Banman <neil.banman@gmail.com>
2023-07-11 09:04:26 -0700

    refactored to be shorter and more explicable, and added comments

--- src/main/kotlin/org/gristle/adventOfCode/y2018/d4/Y2018D4.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2018/d4/Y2018D4.kt
@@ -3,86 +3,77 @@ package org.gristle.adventOfCode.y2018.d4
 import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.groupValues
 import org.gristle.adventOfCode.utilities.transpose
+import java.time.LocalDateTime
 
 class Y2018D4(input: String) : Day {
 
-    data class LogEntry(
-        val month: Int,
-        val day: Int,
-        val hour: Int,
-        val minute: Int,
-        val event: String
-    ) : Comparable<LogEntry> {
-        override fun compareTo(other: LogEntry) =
-            compareBy<LogEntry> { it.month }
-                .thenBy { it.day }
-                .thenBy { it.hour }
-                .thenBy { it.minute }
-                .compare(this, other)
-    }
+    // Parses to map using the guard ID as the key and a list of specific sleep schedules for days as the value
+    private val guards: Map<Int, List<List<Boolean>>> = buildMap<Int, MutableList<List<Boolean>>> {
 
-    private val pattern = """\[1518-(\d\d)-(\d\d) (\d\d):(\d\d)] (?:Guard )?(#\d+|falls asleep|wakes up)""".toRegex()
+        // Parse input to list of log entries sorted by dateTime
+        val log: List<Pair<LocalDateTime, Int>> = input
+            .groupValues("""\[([^]]+)\] (?:Guard #)?(\d+|\w)""")
+            .map { (dateTime, action) ->
+                val id = action.toIntOrNull() ?: -1
+                LocalDateTime.parse(dateTime.replace(' ', 'T')) to id
+            }.sortedBy { (dateTime, _) -> dateTime }
 
-    private val logs = input
-        .groupValues(pattern)
-        .map { gv ->
-            val ints = gv.dropLast(1).map { it.toInt() }
-            LogEntry(ints[0], ints[1], ints[2], ints[3], gv.last())
-        }.sorted()
+        // index of log listings, as there is a nested while loop requiring more fine-tuned control 
+        var index = 0
 
-    private fun MutableList<Boolean>.sleep(minute: Int) {
-        for (i in minute..lastIndex) this[i] = true
-    }
+        // run loop until all log listings have been processed
+        do {
 
-    private fun MutableList<Boolean>.wake(minute: Int) {
-        for (i in minute..lastIndex) this[i] = false
-    }
+            // get id of guard on duty and set various variables to track the sleep log of the particular day
+            val (_, id) = log[index]
+            val hour = BooleanArray(60)
+            var minute = 0
+            var asleep = false
+            index++
 
-    private val guards = buildMap<String, MutableList<MutableList<Boolean>>> {
-        var minutes = MutableList(60) { false }
-        logs.forEach { log ->
-            when (log.event) {
-                "wakes up" -> minutes.wake(log.minute)
-                "falls asleep" -> minutes.sleep(log.minute)
-                else -> {
-                    val guard = getOrPut(log.event) { mutableListOf() }
-                    guard.add(MutableList(60) { false })
-                    minutes = guard.last()
-                }
+            // nested while loop populates the hour array with sleep info
+            while (index < log.size && log[index].second == -1) {
+                val nextMinute = log[index].first.minute
+                for (min in minute until nextMinute) hour[min] = asleep
+                minute = nextMinute
+                asleep = !asleep
+                index++
             }
+
+            // fills out the rest of the hour array
+            for (min in minute..59) hour[min] = asleep
+
+            // adds the hour to the map
+            getOrPut(id) { mutableListOf() }.add(hour.toList())
+        } while (index < log.size)
+    }
+
+    override fun part1() = guards
+        .maxBy { (_, days) -> days.sumOf { hour -> hour.count { it } } } // gets guard with most hours asleep
+        .let { (id, days) ->
+
+            // finds the minute where the guard is most often asleep
+            val mostAsleep = (0..59).maxBy { minute -> days.count { day -> day[minute] } }
+
+            // returns the id * the minute
+            id * mostAsleep
         }
-    } as Map<String, List<List<Boolean>>>
 
-    val getTimeList: (Map.Entry<String, List<List<Boolean>>>) -> IndexedValue<List<Boolean>> = { guard ->
-        guard
-            .value
-            .transpose()
-            .withIndex()
-            .maxByOrNull { (_, minute) -> minute.count { it } }
-            ?: throw Exception("Map entry has no value")
-    }
-
-    override fun part1(): Int {
-        val sleepiest = guards.entries
-            .maxByOrNull { guard -> guard.value.sumOf { day -> day.count { it } } }
-            ?: throw Exception("Map entry has no value")
-        val guardId = sleepiest.key.drop(1).toInt()
-        val sleepiestTimes = getTimeList(sleepiest)
-        return guardId * sleepiestTimes.index
-    }
-
-    override fun part2() = guards
-        .entries
-        .map { guard ->
-            val minuteMap = getTimeList(guard)
-            guard.key to minuteMap
-        }.maxByOrNull { (_, minMax) -> minMax.value.count { it } }
-        ?.let { it.first.drop(1).toInt() * it.second.index }
+    override fun part2(): Int = guards
+        .map { (id, days) -> // for each guard...
+            days
+                .transpose()
+                .mapIndexed { minute, sleepRecord -> // ...make a table of each minute and how many times asleep...
+                    minute to sleepRecord.count { it }
+                }.maxBy { (_, asleep) -> asleep } // ...then grab the minute that guard was most often asleep...
+                .let { (minute, asleep) -> Triple(id, minute, asleep) } // ...and package it all together
+        }.maxBy { (_, _, asleep) -> asleep } // find guard who had the minute that was most asleep
+        .let { (id, minute) -> id * minute } // multiply that minute by the guard's ID
 }
 
 fun main() = Day.runDay(Y2018D4::class)
 
-//    Class creation: 43ms
-//    Part 1: 19025 (7ms)
-//    Part 2: 23776 (4ms)
-//    Total time: 55ms
+//    Class creation: 50ms
+//    Part 1: 19025 (4ms)
+//    Part 2: 23776 (5ms)
+//    Total time: 59ms
