3dba9d7cc262305a9ab623567e8a505ee71e2322
Neil Banman <neil.banman@gmail.com>
2023-07-11 09:59:38 -0700

    refactored to use ArrayList instead ArrayDeque (no add/insert at beginning) and Strings. 25% speedup. Added comments

--- src/main/kotlin/org/gristle/adventOfCode/y2018/d5/Y2018D5.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2018/d5/Y2018D5.kt
@@ -1,27 +1,39 @@
 package org.gristle.adventOfCode.y2018.d5
 
 import org.gristle.adventOfCode.Day
-import java.util.*
 import kotlin.math.abs
 
 class Y2018D5(input: String) : Day {
 
-    private fun String.react() = ArrayDeque<Char>()
-        .also { forEach { c -> if (it.isEmpty() || abs(it.peek() - c) != 32) it.push(c) else it.pop() } }
-        .joinToString("")
+    // takes chars and creates a new list of chars only adding the char if the character is not removed
+    // and the char is not preceded by the opposite polarity char. If it is preceded by the opposite polarity char,
+    // that char is removed from the list.
+    private fun Iterable<Char>.react(removed: Char? = null): List<Char> = mutableListOf<Char>()
+        .also { newPolymer ->
+            forEach { unit ->
+                when {
+                    removed != null && (unit == removed || unit == removed.uppercaseChar()) -> {}
+                    newPolymer.isEmpty() || abs(newPolymer.last() - unit) != 32 -> newPolymer.add(unit)
+                    else -> newPolymer.removeLast()
+                }
+            }
+        }
 
-    private val reacted = input.react()
+    // The initial reaction, the size of which is the pt 1 answer. However, the pt 2 answer can use this value to 
+    // bootstrap its calculations.
+    private val reactedPolymer: List<Char> by lazy { input.asIterable().react() }
 
-    override fun part1() = reacted.length
+    // The size of the fully reacted polymer.
+    override fun part1() = reactedPolymer.size
 
-    override fun part2() = ('a'..'z')
-        .map { reacted.replace(it.toString(), "", true).react().length }
-        .minByOrNull { it }
+    // Further react the polymer 26 times, each time selectively removing a particular unit. Return the smallest
+    // size.
+    override fun part2() = ('a'..'z').minOf { removed -> reactedPolymer.react(removed).size }
 }
 
 fun main() = Day.runDay(Y2018D5::class)
 
-//    Class creation: 30ms
-//    Part 1: 10972 (0ms)
-//    Part 2: 5278 (34ms)
-//    Total time: 64ms
+//    Class creation: 2ms
+//    Part 1: 10972 (15ms)
+//    Part 2: 5278 (27ms)
+//    Total time: 45ms
