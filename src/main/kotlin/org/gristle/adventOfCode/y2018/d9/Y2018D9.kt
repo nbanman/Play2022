0a5daecc9b3634d5e74d74673025e93b6b8f785e
Neil Banman <neil.banman@gmail.com>
2023-07-12 12:09:07 -0700

    tweaked parsing

--- src/main/kotlin/org/gristle/adventOfCode/y2018/d9/Y2018D9.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2018/d9/Y2018D9.kt
@@ -1,10 +1,19 @@
 package org.gristle.adventOfCode.y2018.d9
 
 import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.utilities.getIntList
 
-class Y2018D9(private val input: String) : Day {
+class Y2018D9(input: String) : Day {
 
-    private val pattern = """(\d+) players; last marble is worth (\d+) points""".toRegex()
+    private val players: Int
+    private val highestValue: Int
+
+    init {
+        input.getIntList().let { (a, b) ->
+            players = a
+            highestValue = b
+        }
+    }
 
     class Dll<E>(val value: E, leftNode: Dll<E>? = null, rightNode: Dll<E>? = null) {
         var left = leftNode ?: this
@@ -34,21 +43,19 @@ class Y2018D9(private val input: String) : Day {
     data class Score(var amt: Long)
 
     fun solve(multiplier: Int = 1): Long {
-        val gv = pattern.find(input)?.groupValues ?: throw IllegalArgumentException()
-        val players = gv[1].toInt()
-        val lastMarble = gv[2].toLong() * multiplier
+        val lastMarble = highestValue * multiplier
         val scores = List(players) { Score(0) }
         var currentMarble = Dll(0)
         for (x in 1..lastMarble) {
-            if (x % 23 == 0L) {
+            if (x % 23 == 0) {
                 currentMarble = currentMarble.goLeft(7)
-                scores[((x - 1) % players).toInt()].amt += x + currentMarble.value
+                scores[((x - 1) % players)].amt += x + currentMarble.value
                 currentMarble = currentMarble.remove()
             } else {
-                currentMarble = currentMarble.right.addRight(Dll(x.toInt()))
+                currentMarble = currentMarble.right.addRight(Dll(x))
             }
         }
-        return scores.maxOf { it.amt }
+        return scores.maxOf(Score::amt)
     }
 
     override fun part1() = solve()
