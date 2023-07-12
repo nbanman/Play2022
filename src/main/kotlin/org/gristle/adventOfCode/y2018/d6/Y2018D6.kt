b33440173fc70dd3b7be1dca00816ad7f40a9588
Neil Banman <neil.banman@gmail.com>
2023-07-12 00:48:47 -0700

    refactored 2x speed (30 more lines though!). Speedup mostly from more efficient assignment of closest coordinates to spaces. Added comments

--- src/main/kotlin/org/gristle/adventOfCode/y2018/d6/Y2018D6.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2018/d6/Y2018D6.kt
@@ -1,65 +1,115 @@
 package org.gristle.adventOfCode.y2018.d6
 
 import org.gristle.adventOfCode.Day
-import org.gristle.adventOfCode.utilities.Coord
-import org.gristle.adventOfCode.utilities.MutableGrid
-import org.gristle.adventOfCode.utilities.minMaxRanges
+import org.gristle.adventOfCode.utilities.*
+import kotlin.math.max
 
 class Y2018D6(input: String) : Day {
 
-    private val offsetCoordinates = input.lines().map { line ->
-        val (x, y) = line.split(", ").map(String::toInt)
-        Coord(x, y)
-    }
+    // coordinates from input, but offset so top left coordinate is normalized to (0,0) 
+    private val coordinates: List<Coord>
 
-    private val ranges = offsetCoordinates.minMaxRanges()
-    private val xRange = ranges.first
-    private val yRange = ranges.second
-
-    private val offset = Coord(xRange.first, yRange.first)
-    private val coordinates = offsetCoordinates.map { it - offset }
-
-    private val width = xRange.last - xRange.first + 1
-    private val height = yRange.last - yRange.first + 1
-    private val space = MutableGrid(width, height) { -2 }
+    // width and height of starfield
+    private val width: Int
+    private val height: Int
 
     init {
-        for (index in space.indices) {
-            if (space[index] >= 0) continue
-            val coord = space.coordOf(index)
-            val (closest, next) = coordinates
-                .map { it.manhattanDistance(coord) }
-                .withIndex()
-                .sortedBy { it.value }
-                .take(2)
-            if (closest.value == next.value) {
-                space[index] = -1
-            } else {
-                space[index] = closest.index
-            }
-        }
+
+        // "raw" coordinates before offset parsed from input
+        val offsetCoordinates = input
+            .getInts()
+            .chunked(2) { (x, y) -> Coord(x, y) }
+            .toList()
+
+        val bounds = offsetCoordinates.getBounds()
+
+        // calculate the offset
+        val offset = Coord(-bounds.first.first, -bounds.second.first)
+
+        // return offset coordinates
+        coordinates = offsetCoordinates.map { it + offset }
+
+        // return the width and height of the starfield
+        width = bounds.first.last - bounds.first.first + 1
+        height = bounds.second.last - bounds.second.first + 1
     }
 
     override fun part1(): Int {
-        val borderingIds = (space.row(0) + space.row(space.height - 1) +
-                space.column(0) + space.column(space.width - 1)).toSet()
 
-        return space
-            .filter { it >= 0 && it !in borderingIds }
-            .groupingBy { it }
-            .eachCount()
-            .entries
-            .maxOf { it.value }
+        // array to track the closest coordinate to every space within the starfield. '-1' means unexplored.
+        // '-2' means equally distant to at least two coordinates. Any other value is the index of the closest
+        // coordinate.
+        val space = MutableGrid(width, height) { -1 }
+
+        // seed the space with the original coordinates
+        coordinates.forEachIndexed { index, pos -> space[pos] = index }
+
+        // helper for below ring function
+        val sides = listOf(
+            Coord(0, -1) to Coord(1, 1),
+            Coord(1, 0) to Coord(-1, 1),
+            Coord(0, 1) to Coord(-1, -1),
+            Coord(-1, 0) to Coord(1, -1),
+        )
+
+        // function returns all coordinates that are distance t away from the coordinate
+        fun Coord.ring(t: Int) = sides
+            .flatMap { (initial, direction) ->
+                (2..t).runningFold(this + Coord(initial.x * t, initial.y * t)) { pos, _ -> pos + direction }
+            }
+
+        // for each distance unit assign unassigned spaces to the closest coordinate 
+        timeLoop@ for (distance in 1 until max(width, height)) {
+
+            // track any assignments because if another coordinate also reaches the space at the same distance then
+            // the assignment must be undone
+            val distanceLog = mutableSetOf<Coord>()
+
+            coordinates.forEachIndexed { index, pos -> // for each coordinate...
+                pos
+                    .ring(distance) // ...get the spaces reached at that distance
+                    .filter { space.validCoord(it) } // remove spaces not in starfield (infinite space)
+                    .forEach { ringPos ->
+                        when (space[ringPos]) {
+                            -1 -> { // when that space is unoccupied...
+                                space[ringPos] = index // ...assign the index to it...
+                                distanceLog.add(ringPos) // ...and add the assignment to the log
+                            }
+
+                            -2 -> {} // when a conflict has already been found, do nothing
+
+                            // when an assignment has already been made at that space, undo it and put '-2' in its
+                            // place to denote a conflict
+                            else -> if (ringPos in distanceLog) space[ringPos] = -2
+                        }
+                    }
+            }
+
+            // break early if no new assignments were made
+            if (distanceLog.isEmpty()) break@timeLoop
+        }
+
+        // 'infinite' stores those values that reach the edge of known space, and thus should not be counted
+        val infinite = space.row(0).toSet() +
+                space.row(space.height - 1) +
+                space.column(0) +
+                space.column(space.width - 1)
+
+        return space // look at each space in the starfield
+            .filter { it !in infinite } // remove those spaces assigned to 'infinite' coordinates
+            .eachCount() // count occurrences of the remainder
+            .maxOf { (_, count) -> count } // return the largest 
     }
 
-    override fun part2() = space.indices.count { index ->
-        coordinates.sumOf { it.manhattanDistance(space.coordOf(index)) } < 10_000
-    }
+    // recreate indices of the starfield and count if the sum of all coordinate distances to that spot is less than
+    // 10,000
+    override fun part2() = (0 until width * height)
+        .count { index -> coordinates.sumOf { it.manhattanDistance(Coord.fromIndex(index, width)) } < 10_000 }
 }
 
 fun main() = Day.runDay(Y2018D6::class)
 
-//    Class creation: 432ms
-//    Part 1: 5365 (35ms)
-//    Part 2: 42513 (47ms)
-//    Total time: 515ms
+//    Class creation: 6ms
+//    Part 1: 5365 (173ms)
+//    Part 2: 42513 (50ms)
+//    Total time: 230ms
