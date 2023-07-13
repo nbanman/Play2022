4f615a4a4feb0c064a54928a030c77749d0e5591
Neil Banman <neil.banman@gmail.com>
2023-07-13 14:02:35 -0700

    refactored, added comments. slightly faster despite calculating pts 1 and 2 separately, as opposed to both all at once.

--- src/main/kotlin/org/gristle/adventOfCode/y2018/d13/Y2018D13.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2018/d13/Y2018D13.kt
@@ -2,120 +2,142 @@ package org.gristle.adventOfCode.y2018.d13
 
 import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.Coord
+import org.gristle.adventOfCode.utilities.Grid
 import org.gristle.adventOfCode.utilities.Nsew
 import org.gristle.adventOfCode.utilities.toGrid
 
-class Y2018D13(private val input: String) : Day {
+class Y2018D13(input: String) : Day {
 
-    data class Car(
-        val c: Coord,
-        val direction: Nsew,
-        val turnState: TurnState = TurnState.LEFT,
-        val crashed: Boolean = false
-    )
-
+    // Tracks the different turning behaviors of cars at intersections
     enum class TurnState {
         LEFT {
             override fun advance() = STRAIGHT
+            override fun dir(dir: Nsew): Nsew = dir.left()
         },
-
         STRAIGHT {
             override fun advance() = RIGHT
+            override fun dir(dir: Nsew): Nsew = dir
         },
-
         RIGHT {
             override fun advance() = LEFT
+            override fun dir(dir: Nsew): Nsew = dir.right()
         };
 
+        // toggles the TurnState to the next state after being used 
         abstract fun advance(): TurnState
+
+        // returns the new direction after being used
+        abstract fun dir(dir: Nsew): Nsew
     }
 
-    val solution: Pair<Coord, Coord> = let {
-        val maze = "$input       ".toGrid()
-        var firstCrash = Coord.ORIGIN
-        var cars = maze
-            .mapIndexedNotNull { index, c ->
-                if (c in "<^>v") {
-                    Car(
-                        maze.coordOf(index),
-                        when (c) {
-                            '^' -> Nsew.NORTH
-                            'v' -> Nsew.SOUTH
-                            '>' -> Nsew.EAST
-                            else -> Nsew.WEST
-                        }
-                    )
-                } else null
-            }.sortedBy { maze.indexOf(it.c) }
-        while (cars.size > 1) {
-            val newLocations = mutableListOf<Coord>()
-            val crashLocations = mutableListOf<Coord>()
-            val oldLocations = cars.map { it.c }.toMutableList()
-            cars = cars
-                .map { car ->
-                    if (car.c in newLocations) {
-                        Car(car.c, car.direction, car.turnState, true)
-                    } else {
-                        oldLocations.remove(car.c)
-                        val spot = maze[car.c]
-                        val newDirection = when (spot) {
+    // Stores a car's direction and TurnState information
+    data class Car(
+        val dir: Nsew,
+        val turnState: TurnState,
+    )
+
+    // Parse the input to a racetrack
+    private val racetrack: Grid<Char> = input.toGrid()
+
+    // Create initial map of cars by position    
+    private val cars: Map<Coord, Car> = racetrack
+        .mapIndexedNotNull { pos, c -> // look at all spaces on racetrack
+
+            // set direction if the space holds a car; otherwise, null
+            val dir = when (c) {
+                '^' -> Nsew.NORTH
+                '>' -> Nsew.EAST
+                'v' -> Nsew.SOUTH
+                '<' -> Nsew.WEST
+                else -> null
+            }
+
+            // if the space holds a car, add it to map
+            dir?.let { nsew -> racetrack.coordOf(pos) to Car(nsew, TurnState.LEFT) }
+        }.toMap()
+
+    // Advances state in the sequence below.
+    private fun Pair<Map<Coord, Car>, Coord?>.move(racetrack: Grid<Char>): Pair<Map<Coord, Car>, Coord?> {
+
+        // Makes mutable copy of the input state
+        val stillRacing = first.toMutableMap()
+
+        // Tracks the coordinates of any crashes
+        var crashCoord: Coord? = null
+
+        // Moves cars along track. If there's a collision, remove collided cars from map and note coordinates
+        stillRacing
+            .entries
+            .sortedBy { (pos) -> racetrack.indexOf(pos) } // go from top-left to bottom-right
+            .forEach { (pos, car) ->
+
+                // entry could already be deleted from map if there was a collision with it, so only run if 
+                // the map still contains the entry
+                if (stillRacing.contains(pos)) {
+
+                    // find the spot the car will move to
+                    val newPos = pos.move(car.dir)
+
+                    // remove spot the car previously occupied
+                    stillRacing.remove(pos)
+
+                    if (stillRacing[newPos] == null) { // if no crash...
+
+                        // place car in new position, adjusting dir and TurnState as appropriate
+                        when (racetrack[newPos]) {
+                            '+' -> stillRacing[newPos] = Car(
+                                car.turnState.dir(car.dir),
+                                car.turnState.advance(),
+                            )
+
                             '\\' -> {
-                                when (car.direction) {
-                                    Nsew.NORTH -> car.direction.left()
-                                    Nsew.SOUTH -> car.direction.left()
-                                    Nsew.EAST -> car.direction.right()
-                                    Nsew.WEST -> car.direction.right()
+                                val newDir = when (car.dir) {
+                                    Nsew.NORTH -> Nsew.WEST
+                                    Nsew.SOUTH -> Nsew.EAST
+                                    Nsew.EAST -> Nsew.SOUTH
+                                    Nsew.WEST -> Nsew.NORTH
                                 }
+                                stillRacing[newPos] = car.copy(dir = newDir)
                             }
+
                             '/' -> {
-                                when (car.direction) {
-                                    Nsew.NORTH -> car.direction.right()
-                                    Nsew.SOUTH -> car.direction.right()
-                                    Nsew.EAST -> car.direction.left()
-                                    Nsew.WEST -> car.direction.left()                    }
-                            }
-                            '+' -> {
-                                when (car.turnState) {
-                                    TurnState.LEFT -> car.direction.left()
-                                    TurnState.STRAIGHT -> car.direction
-                                    TurnState.RIGHT -> car.direction.right()
+                                val newDir = when (car.dir) {
+                                    Nsew.NORTH -> Nsew.EAST
+                                    Nsew.SOUTH -> Nsew.WEST
+                                    Nsew.EAST -> Nsew.NORTH
+                                    Nsew.WEST -> Nsew.SOUTH
                                 }
+                                stillRacing[newPos] = car.copy(dir = newDir)
                             }
-                            else -> {
-                                car.direction
-                            }
+
+                            else -> stillRacing[newPos] = car
                         }
-
-                        val newState = if (spot == '+') car.turnState.advance() else car.turnState
-
-                        val newLocation = newDirection.forward(car.c)
-
-                        val gotCrashed = if (newLocation in newLocations || newLocation in oldLocations) {
-                            if (firstCrash == Coord.ORIGIN) firstCrash = newLocation
-                            crashLocations.add(newLocation)
-                            true
-                        } else false
-
-                        newLocations.add(newLocation)
-
-                        Car(newLocation, newDirection, newState, gotCrashed)
+                    } else { // crash...
+                        if (crashCoord == null) crashCoord = newPos // mark the crash coordinates
+                        stillRacing.remove(newPos) // remove the car that was crashed into
                     }
                 }
-            cars = cars
-                .filter { it.c !in crashLocations && !it.crashed }
-                .sortedBy { maze.indexOf(it.c) }
-        }
-        firstCrash to cars.first().c
+            }
+        return stillRacing to crashCoord
     }
 
-    override fun part1() = solution.first
+    private val initialState: Pair<Map<Coord, Car>, Coord?> = cars to null
 
-    override fun part2() = solution.second
+    // sequence advances state indefinitely
+    private val race = generateSequence(initialState) { it.move(racetrack) }
+
+    override fun part1() = race
+        .first { (_, crash) -> crash != null } // stop sequence when a crash is recorded
+        .let { (_, crash) -> crash?.let { "${it.x},${it.y}" } ?: "" } // return crash coordinate
+
+    override fun part2() = race
+        .first { (cars) -> cars.size == 1 } // stop sequence when only one car remains
+        .let { (cars) -> cars.keys.first().let { "${it.x},${it.y}" } } // return car coordinate}
 }
 
 fun main() = Day.runDay(Y2018D13::class)
-//    
-//    Class creation: 80ms
-//    Part 1: (86, 118) (0ms)
-//    Part 2: (2, 81) (0ms)
-//    Total time: 80ms
+
+//    Class creation: 20ms
+//    Part 1: 86,118 (8ms)
+//    Part 2: 2,81 (39ms)
+//    Total time: 69ms
