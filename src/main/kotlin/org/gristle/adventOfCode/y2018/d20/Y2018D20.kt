d0f1a7648ff33df9c37aceb8898b8c2fc3ed35ee
Neil Banman <neil.banman@gmail.com>
2023-07-24 14:49:40 -0700

    Refactored, 33x speedup. Added comments.

--- src/main/kotlin/org/gristle/adventOfCode/y2018/d20/Y2018D20.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2018/d20/Y2018D20.kt
@@ -1,81 +1,64 @@
 package org.gristle.adventOfCode.y2018.d20
 
 import org.gristle.adventOfCode.Day
-import org.gristle.adventOfCode.utilities.*
+import org.gristle.adventOfCode.utilities.Coord
+import org.gristle.adventOfCode.utilities.Graph
 import org.gristle.adventOfCode.utilities.Graph.steps
-import java.util.*
 
 class Y2018D20(private val input: String) : Day {
 
-    private fun makeMap(): Grid<Char> {
-        val length = input.length / 2 + 20
-        val map = MutableGrid(length, length) { '^' }
+    // Build edge map for BFS function by parsing the input
+    private val map: Map<Coord, List<Coord>> = buildMap<Coord, MutableList<Coord>> {
 
-        fun move(coord: Coord, c: Char): Coord {
-            if (c !in "NSEW") return coord
-            val (doorCoord, doorSymbol, newCoord) = when (c) {
-                'N' -> Triple(coord.north(), '-', coord.north(2))
-                'W' -> Triple(coord.west(), '|', coord.west(2))
-                'E' -> Triple(coord.east(), '|', coord.east(2))
-                'S' -> Triple(coord.south(), '-', coord.south(2))
-                else -> throw IllegalArgumentException()
-            }
-            map[doorCoord] = doorSymbol
-            return newCoord
-        }
+        // The input has multiple paths using a regex-style notation. Keep track of the paths in a stack.
+        val returnCoords = mutableListOf<Coord>()
 
-        fun decorate(coord: Coord) {
-            if (map[coord] != 'X') map[coord] = '.'
-            val neighbors = map.getNeighborIndices(coord, false)
-            val diagonals = map.getNeighborIndices(coord, true) - neighbors.toSet()
-            diagonals.forEach { map[it] = '#' }
-            neighbors.filter { map[it] !in "#-|.X" }.forEach { map[it] = '?' }
-        }
-
-        fun exploreMap(regex: String, start: Coord) {
-            val returnCoords = ArrayDeque<Coord>()
-            var i = 0
-            var coord = start
-            map[start] = 'X'
-            while (i < regex.length - 1) {
-                when (val c = regex[i]) {
-                    '(' -> returnCoords.add(coord)
-                    '|' -> coord = returnCoords.last
-                    ')' -> returnCoords.removeLast()
-                    else -> {
-                        coord = move(coord, c)
-                        decorate(coord)
-                    }
+        // For a given coordinate, makes a move in accordance with the next char in the input. Processes the (|) 
+        // by using the returnCoords stack. Builds out the edge map.
+        fun move(pos: Coord, c: Char): Coord {
+            val next = when (c) {
+                'N' -> pos.north()
+                'W' -> pos.west()
+                'E' -> pos.east()
+                'S' -> pos.south()
+                '(' -> {
+                    returnCoords.add(pos)
+                    return pos
                 }
-                i++
+
+                '|' -> return returnCoords.last()
+                ')' -> {
+                    returnCoords.removeLast()
+                    return pos
+                }
+
+                else -> return pos
             }
+
+            // Builds the edge map
+            val to = getOrPut(pos) { mutableListOf() }
+            if (next !in to) to.add(next)
+
+            val from = getOrPut(next) { mutableListOf() }
+            if (pos !in from) from.add(pos)
+            return next
         }
 
-        exploreMap(input, Coord(length / 2, length / 2))
-
-        val topLeft = map.coordOfElement('#')
-        val bottomRight = map.lastCoordOfElement('#')
-        val gridSizes = bottomRight - topLeft
-
-        return map
-            .subGrid(topLeft, gridSizes.x + 1, gridSizes.y + 1)
-            .let { m -> m.mapToGrid { if (it == '?') '#' else it } }
+        // Explores the map.
+        input.asSequence().runningFold(Coord.ORIGIN, ::move).last()
     }
 
-    private val area = makeMap()
-    private val distances = Graph.bfs(area.coordOfElement('X')) { coord ->
-        area.getNeighborIndices(coord)
-            .filter { area[it] != '#' }
-            .map { area.coordOf(it) }
-    }
+    // Standard BFS search of the space. Returns list of all vertices reached and their distance from origin.
+    private val paths: List<Graph.Vertex<Coord>> by lazy { Graph.bfs(Coord.ORIGIN, edges = map) }
 
-    override fun part1() = distances.steps() / 2
-    override fun part2() = distances.count { area[it.id] == '.' && it.weight >= 2000 }
+    override fun part1() = paths.steps()
+
+    override fun part2() = paths.count { it.weight >= 1000 }
 }
 
 fun main() = Day.runDay(Y2018D20::class)
 
-//    Class creation: 1729ms
-//    Part 1: 3930 (0ms)
-//    Part 2: 8240 (8ms)
-//    Total time: 1738ms
+//    Class creation: 19ms
+//    Part 1: 3930 (30ms)
+//    Part 2: 8240 (1ms)
+//    Total time: 52ms
