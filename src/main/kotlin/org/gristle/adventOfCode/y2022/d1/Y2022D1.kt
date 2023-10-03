ade274be960d09ec4bae9b0606e44e87c649378e
Neil Banman <neil.banman@gmail.com>
2023-10-03 13:57:04 -0700

    tweaks

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d1/Y2022D1.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d1/Y2022D1.kt
@@ -1,18 +1,20 @@
 package org.gristle.adventOfCode.y2022.d1
 
 import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.utilities.blankSplit
+import org.gristle.adventOfCode.utilities.getInts
 import org.gristle.adventOfCode.utilities.poll
 import org.gristle.adventOfCode.utilities.toPriorityQueueDescending
 
 class Y2022D1(input: String) : Day {
     private val calories: List<Int> = input
-        .split("\n\n") // split on blank lines, so each elf's snacks are grouped
+        .blankSplit() // group each elf's snacks
         // sum all the ints found in each elf's string, then put them all in a descending priority queue
-        .toPriorityQueueDescending { it.split('\n').map(String::toInt).sum() }
+        .toPriorityQueueDescending { it.getInts().sum() }
         .poll(3) // grab the top three contenders
 
     override fun part1(): Int = calories.first() // peek at the top of the priority queue, which holds the highest sum
-    override fun part2(): Int = calories.sum() // sum those three
+    override fun part2(): Int = calories.sum() // sum the top three contenders
 }
 
 fun main() = Day.runDay(Y2022D1::class) // 71300, 209691
