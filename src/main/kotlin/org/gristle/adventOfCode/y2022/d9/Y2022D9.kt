044dc419b4a3b4ac7b0a22226c56bb08785f32b9
Neil Banman <neil.banman@gmail.com>
2023-10-04 10:44:35 -0700

    tweaks, added comments

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d9/Y2022D9.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d9/Y2022D9.kt
@@ -2,27 +2,19 @@ package org.gristle.adventOfCode.y2022.d9
 
 import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.Coord
-import org.gristle.adventOfCode.utilities.Nsew
+import org.gristle.adventOfCode.utilities.getIntList
+import org.gristle.adventOfCode.utilities.toNsew
 import kotlin.math.sign
 
 // Sequence-palooza!!
 class Y2022D9(input: String) : Day {
 
-    // parse directional Char to a direction object that can move a Coord in that direction
-    private fun Char.toDirection() = when (this) {
-        'R' -> Nsew.EAST
-        'U' -> Nsew.NORTH
-        'L' -> Nsew.WEST
-        'D' -> Nsew.SOUTH
-        else -> throw IllegalArgumentException("Invalid input")
-    }
-
     // Sequence of positions that the head occupies, including any repeats
     private val headPositions: Sequence<Coord> = input
         .lineSequence()
         .flatMap { line -> // parse into a Sequence of directions, expanded so that "U 4" becomes 4 Nsew.NORTH entries
-            val direction = line[0].toDirection()
-            val times = line.takeLastWhile(Char::isDigit).toInt()
+            val direction = line[0].toNsew()
+            val (times) = line.getIntList()
             generateSequence { direction }.take(times)
         }
         // take directions and turn them into a Sequence of positions that the head visits
@@ -34,7 +26,7 @@ class Y2022D9(input: String) : Day {
         yield(pos)
         frontPositions.forEach { frontPos ->
             // only move if the link in front is at least 2 away on either the x- or the y-axis
-            if (pos.chebyshevDistance(frontPos) > 1) {
+            if (frontPos.chebyshevDistance(pos) > 1) {
                 // move one toward the link in front, on both axes
                 val diff = frontPos - pos
                 pos = Coord(pos.x + diff.x.sign, pos.y + diff.y.sign)
@@ -43,10 +35,11 @@ class Y2022D9(input: String) : Day {
         }
     }
 
+    // takes the initial sequence of positions, and adds subsequent links that follow the preceding sequence
     fun solve(links: Int): Int = generateSequence(headPositions, ::followPath)
         .take(links)
-        .last()
-        .toSet()
+        .last() // only interested in the last sequence representing the final link
+        .toSet() // convert to set to remove duplicated positions
         .size
 
     override fun part1() = solve(2)
