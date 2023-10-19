42501ca3ee097abbd0efe612a3608769d490fada
Neil Banman <neil.banman@gmail.com>
2023-10-19 12:45:53 -0700

    added comments

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d13/Y2022D13.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d13/Y2022D13.kt
@@ -4,11 +4,19 @@ import org.gristle.adventOfCode.Day
 import kotlin.math.sign
 
 class Y2022D13(input: String) : Day {
-    sealed class PData : Comparable<PData> {
-        data class Value(val value: Int) : PData() {
 
-            fun toPacket() = Packet(listOf(this))
-            override fun compareTo(other: PData): Int {
+    /**
+     * Packets consist of Values (an Int value) and Lists (a list of sub-packets). They are comparable using
+     * rules spelled out in the puzzle.
+     */
+    sealed class Packet : Comparable<Packet> {
+        class Value(val value: Int) : Packet() {
+
+            // it's easiest to use the PacketList comparison logic when comparing values to PacketLists, so this 
+            // utility function turns a Value into a PacketList with one sub-Value.
+            fun toPacket() = PacketList(listOf(this))
+
+            override fun compareTo(other: Packet): Int {
                 return when (other) {
                     is Value -> (this.value - other.value).sign
                     else -> this.toPacket().compareTo(other)
@@ -16,10 +24,10 @@ class Y2022D13(input: String) : Day {
             }
         }
 
-        data class Packet(val list: List<PData>) : PData() {
-            override fun compareTo(other: PData): Int {
+        class PacketList(val list: List<Packet>) : Packet() {
+            override fun compareTo(other: Packet): Int {
                 when (other) {
-                    is Packet -> {
+                    is PacketList -> {
                         (this.list zip other.list).forEach { (leftItem, rightItem) ->
                             val answer = leftItem.compareTo(rightItem)
                             if (answer != 0) return answer
@@ -37,42 +45,50 @@ class Y2022D13(input: String) : Day {
         companion object {
             private val regex = Regex("""\[|]|\d+""")
 
-            fun of(line: String): Packet {
-                val parser: Iterator<String> = regex.findAll(line).map(MatchResult::value).iterator()
-                fun makePacket(): Packet {
-                    val list = mutableListOf<PData>()
-                    while (parser.hasNext()) {
-                        when (val next = parser.next()) {
-                            "[" -> list.add(makePacket())
-                            "]" -> break
-                            else -> list.add(Value(next.toInt()))
+            /**
+             * Factory method. Finds all relevant tokens, uses a consumable iterator so that makePacket() can go
+             * recursive without losing its place.
+             */
+            fun of(line: String): PacketList {
+
+                // Consumable iterator of '[', ']', and '#'.
+                val iterator: Iterator<String> = regex.findAll(line).map(MatchResult::value).iterator()
+
+                // Recursive function makes PacketLists, filling out any sub-packets as necessary.
+                fun makePacket(): PacketList {
+                    val list: List<Packet> = buildList {
+                        while (iterator.hasNext()) { // while loop consumes entire iterator unless break encountered
+                            when (val next = iterator.next()) {
+                                "[" -> add(makePacket()) // '[' means a PacketList found, so go recursive
+                                "]" -> break // ']' means packet finished, so break, finishing that list
+                                else -> add(Value(next.toInt())) // a number means add a Value as a sub-packet
+                            }
                         }
                     }
-                    return Packet(list)
+                    return PacketList(list)
                 }
+
                 return makePacket()
             }
         }
     }
 
     private val packets = input
-        .lines()
+        .lineSequence()
         .filter(String::isNotBlank)
-        .map(PData::of)
+        .map(Packet::of)
+        .toList()
+
+    private val dividerPackets = listOf(Packet.of("[[2]]"), Packet.of("[[6]]"))
 
     override fun part1(): Int = packets
-        .chunked(2)
-        .mapIndexed { index, value ->
-            val (left, right) = value
-            if (left < right) (index + 1) else 0
-        }.sum()
+        .chunked(2) // pairs the packets
+        .withIndex()
+        .sumOf { (index, value) -> if (value.first() < value.last()) index + 1 else 0 }
 
-    override fun part2(): Int {
-        val dividerPackets = listOf(PData.of("[[2]]"), PData.of("[[6]]")).sorted()
-        return dividerPackets
-            .mapIndexed { index, packet -> packets.count { packet > it } + 1 + index }
-            .reduce(Int::times)
-    }
+    override fun part2(): Int = dividerPackets
+        .mapIndexed { index, packet -> packets.count { packet > it } + index + 1 }
+        .reduce(Int::times)
 }
 
 fun main() = Day.runDay(Y2022D13::class)
