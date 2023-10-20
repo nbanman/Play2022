c65fbb09b58c3f87d5eefec32e738907afb301a6
Neil Banman <neil.banman@gmail.com>
2023-10-20 11:30:34 -0700

    improved performance of constructing numbers list

--- src/main/kotlin/org/gristle/adventOfCode/y2022/d20/Y2022D20.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2022/d20/Y2022D20.kt
@@ -1,22 +1,28 @@
 package org.gristle.adventOfCode.y2022.d20
 
 import org.gristle.adventOfCode.Day
-import org.gristle.adventOfCode.utilities.getLongs
+import org.gristle.adventOfCode.utilities.getLongList
 
-class Y2022D20(private val input: String) : Day {
+class Y2022D20(input: String) : Day {
 
-    fun solve(factor: Int, times: Int): Long {
+    private val values = input.getLongList()
+
+    private fun solve(factor: Int, times: Int): Long {
+
         // Pair numbers with their original index to ensure numbers are unique. I don't know a better way of getting
         // the numbers in order other than doing an indexOf function.
-        val numbers = input.getLongs().map { it * factor }.withIndex().toMutableList()
+        val numbers = MutableList(values.size) { index -> IndexedValue(index, values[index] * factor) }
 
         repeat(times) { // repeat swapping process n times
+
             numbers.indices.forEach { currentIndex -> // swap for each number in the original order
                 val nextIndex = numbers.indexOfFirst { it.index == currentIndex }
                 val number = numbers.removeAt(nextIndex)
-                numbers.add((nextIndex + number.value).mod(numbers.size), number)
+                val addIndex = (nextIndex + number.value).mod(numbers.size)
+                numbers.add(addIndex, number)
             }
         }
+
         val zeroIndex = numbers.indexOfFirst { it.value == 0L }
         return (1..3).fold(0L) { acc, thousand ->
             acc + numbers[(zeroIndex + thousand * 1000) % numbers.size].value
@@ -28,6 +34,9 @@ class Y2022D20(private val input: String) : Day {
     override fun part2() = solve(811589153, 10)
 }
 
-// pt 1: 4151 (87ms) (original 216ms) 
-// pt 2: 7848878698663 (488ms) (original 1006ms)
 fun main() = Day.runDay(Y2022D20::class)
+
+//    Class creation: 19ms
+//    Part 1: 4151 (48ms) (original 216ms)
+//    Part 2: 7848878698663 (435ms) (original 1006ms)
+//    Total time: 503ms
