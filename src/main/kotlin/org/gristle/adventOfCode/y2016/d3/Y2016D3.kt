6a3994f8fa5df38aa39d13d9a497cc4c017df4b6
Neil Banman <neil.banman@gmail.com>
2023-10-24 13:50:31 -0700

    use collate function instead of transpose

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d3/Y2016D3.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d3/Y2016D3.kt
@@ -1,14 +1,21 @@
 package org.gristle.adventOfCode.y2016.d3
 
 import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.utilities.collate
 import org.gristle.adventOfCode.utilities.getInts
-import org.gristle.adventOfCode.utilities.transpose
 
-class Y2016D3(input: String) : Day {
-    private val triangles = input.getInts().chunked(3).toList()
+class Y2016D3(private val input: String) : Day {
+
     private fun isValid(triangle: List<Int>) = triangle.sorted().let { (a, b, c) -> a + b > c }
-    override fun part1() = triangles.count(::isValid)
-    override fun part2() = triangles.transpose().flatMap { it.chunked(3) }.count(::isValid)
+
+    private inline fun solve(rearrange: Sequence<Int>.() -> Sequence<Int> = { this }) = input
+        .getInts()
+        .rearrange()
+        .chunked(3)
+        .count(::isValid)
+
+    override fun part1() = solve()
+    override fun part2() = solve { collate(3).flatten() }
 }
 
 fun main() = Day.runDay(Y2016D3::class)
