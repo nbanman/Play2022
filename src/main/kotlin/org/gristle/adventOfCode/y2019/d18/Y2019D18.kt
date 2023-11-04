22778b87904cb8482a3da27511c0cb68348ef511
Neil Banman <neil.banman@gmail.com>
2023-11-04 11:10:55 -0700

    Refactor, 8x speedup. Added more comments.
    Changed the main Dijkstra algorithm so that previous key locations were not revisited. This required a broader edge map that had weight and metadata linking all keys to each other, but drastically reduced the search space for the main traversal.

--- src/main/kotlin/org/gristle/adventOfCode/y2019/d18/Y2019D18.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2019/d18/Y2019D18.kt
@@ -7,30 +7,48 @@ import org.gristle.adventOfCode.utilities.Grid
 import org.gristle.adventOfCode.utilities.toGrid
 import org.gristle.adventOfCode.utilities.toMutableGrid
 
-// Refactored: much slower but much cleaner code. Not as fast because the previous version relied on 
-// a custom Dijkstra algorithm that had a more effective cache to eliminate possible paths.
-
 class Y2019D18(input: String) : Day {
 
-    data class Key(val currentKey: Char, val precedingKeys: Set<Char>)
+    /**
+     * Used for the edgemap, contains enhanced edge data. This includes the location of the edge, but also all
+     * keys needed to be collected to make the move (precedingKeys) as wall as all keys that are encountered while
+     * traveling to the key (interveningKeys). precedingKeys is used to filter out moves that are not yet possible
+     * due to not having the right key to pass through a door. interveningKeys is used to update the state so that
+     * all keys are added in one swoop.
+     */
+    data class Key(val currentKey: Char, val precedingKeys: Set<Char>, val interveningKeys: Set<Char>)
 
+    /**
+     * Used for the main Dijkstra search. Contains the current position of each robot (location) and all keys that
+     * have been collected up to that point (keys).
+     */
     data class State(val location: List<Char>, val keys: Set<Char>)
 
     private val tunnels = input.toGrid()
 
     fun solve(tunnels: Grid<Char>, starts: List<Char>): Int {
-        // Intermediate edge map used to later make the keyEdges edge map used for the final solution.
-        val edges: Map<Char, List<Graph.Edge<Char>>> = buildMap {
+
+        // Intermediate edge map used to later make the keyEdges edge map used for the final solution. Just gives
+        // the distance to the nearest keys and doors. Once a key or door is reached, the BFS does not explore 
+        // beyond that key or door. But it will still look for other keys or doors that do not require going through
+        // that key or door.
+        val basicEdges: Map<Char, List<Graph.Edge<Char>>> = buildMap {
             tunnels // take input...
                 .withIndex() // associate each value with its index...
                 .filter { (_, value) -> value.isLetter() || value in starts } // only run for important positions
                 .forEach { (index, start) ->
 
                     // lambda for getting edges from the grid
-                    val getEdges: (IndexedValue<Char>) -> List<IndexedValue<Char>> = { (index, _) ->
-                        tunnels
-                            .getNeighborsIndexedValue(index) // get adjacent values
-                            .filter { it.value != '#' } // remove walls
+                    val getEdges: (IndexedValue<Char>) -> List<IndexedValue<Char>> = { (index, current) ->
+
+                        // If a key or door is reached, end this particular path.
+                        if (current.isLetter() && current != start) {
+                            emptyList()
+                        } else { // ...otherwise keep exploring
+                            tunnels
+                                .getNeighborsIndexedValue(index) // get adjacent values
+                                .filter { it.value != '#' } // remove walls
+                        }
                     }
 
                     // populate edge map with all key and door locations that do not require passing through a 
@@ -38,40 +56,42 @@ class Y2019D18(input: String) : Day {
                     this[start] = Graph.bfs(IndexedValue(index, start), defaultEdges = getEdges)
                         .drop(1)
                         .filter { it.id.value.isLetter() } // only look at keys or doors
-                        .filter { destination -> // this checks that there are no other keys or doors in between
-                            destination
-                                .path()
-                                .let { path -> path.subList(1, path.lastIndex) }
-                                .all { it.id.value == '.' }
-                        }
                         .map { Graph.Edge(it.id.value, it.weight) } // convert to an Edge
                 }
         }
 
-        // Transform the map of edges to a form suitable for searching, with only keys being valid places to move
-        // and each edge storing the keys needed to have been collected before.
-        val keyEdges: Map<Char, List<Graph.Edge<Key>>> = edges
+        // Edge map used for the main Dijkstra search. Itself uses a Dijkstra search relying on the basic edge map.
+        val keyEdges: Map<Char, List<Graph.Edge<Key>>> = basicEdges
             .keys
             .filter { it.isLowerCase() || it in starts }
-            .associateWith { key ->
+            .associateWith { key -> // make edge map
                 Graph
-                    .dijkstra(key, edges = edges)
-                    .filter { it.id.isLowerCase() }
-                    .mapNotNull { distance ->
-                        val path = distance.path().drop(1).filter { it.id.isLetter() }
-                        if (path.count { it.id.isLowerCase() } == 1) {
-                            val keys = path
-                                .filter { it.id.isUpperCase() }
-                                .map { it.id.lowercaseChar() }
-                                .toSet()
-                            Graph.Edge(Key(path.last().id, keys), path.last().weight)
-                        } else {
-                            null
-                        }
-                    }
+                    .dijkstraSequence(key, edges = basicEdges)
+                    .drop(1) // first edge returned is the start, so drop that 
+                    .filter { it.id.isLowerCase() } // only look at key locations
+                    .map { distance ->
+
+                        // get the full path to key, dropping the start
+                        val path = distance.path().drop(1)
+
+                        // separate doors from keys
+                        val (keyList, interveningKeyList) = path.partition { it.id.isUpperCase() }
+                        val keys = keyList
+                            .map { it.id.lowercaseChar() }
+                            .toSet()
+                        val interveningKeys = interveningKeyList
+                            .map { it.id }
+                            .toSet()
+
+                        // create edge with metadata
+                        Graph.Edge(Key(path.last().id, keys, interveningKeys), path.last().weight)
+                    }.toList()
             }
 
+        // initial state for the main Dijkstra algorithm
         val startState = State(starts, emptySet())
+
+        // continue Dijkstra until all keys have been collected
         val endCondition = { state: State -> state.keys.size == 26 }
 
         // lambda finds edges by looking at each robot position, looking where each could move, and generating
@@ -79,16 +99,24 @@ class Y2019D18(input: String) : Day {
         val findEdges = { state: State ->
             state.location.flatMapIndexed { robot, location ->
                 keyEdges
-                    .getValue(location)
-                    .filter { state.keys.containsAll(it.vertexId.precedingKeys) }
-                    .map { edge ->
+                    .getValue(location) // gets all edges for location
+                    .filter { keyEdge -> // keeps if key is new to state and all prerequisite keys collected 
+                        keyEdge.vertexId.currentKey !in state.keys
+                                && state.keys.containsAll(keyEdge.vertexId.precedingKeys)
+                    }.map { edge -> // converts valid edge to state
+
+                        // updates robot locations
                         val newLocations: List<Char> =
                             state.location.toMutableList().apply { this[robot] = edge.vertexId.currentKey }
-                        val newState = State(newLocations, state.keys + edge.vertexId.currentKey)
+
+                        // updates keys collected
+                        val newKeys = state.keys union edge.vertexId.interveningKeys
+                        val newState = State(newLocations, newKeys)
                         Graph.Edge(newState, edge.weight)
                     }
             }
         }
+
         return Graph.dijkstra(startState, endCondition, defaultEdges = findEdges).steps()
     }
 
@@ -112,8 +140,7 @@ class Y2019D18(input: String) : Day {
 
 fun main() = Day.runDay(Y2019D18::class)
 
-//    [2019 Day 18]
-//    Class creation: 18ms
-//    Part 1: 3918 (940ms)
-//    Part 2: 2004 (3453ms)
-//    Total time: 4412ms
+//    Class creation: 7ms
+//    Part 1: 3918 (249ms)
+//    Part 2: 2004 (275ms)
+//    Total time: 532ms
