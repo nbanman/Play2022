30a1460985104aec57abca1224a10ff95097a369
Neil Banman <neil.banman@gmail.com>
2023-11-14 12:52:22 -0800

    removed complicated runningFold acting on a pair by running a partition on input and going from there

--- src/main/kotlin/org/gristle/adventOfCode/y2015/d3/Y2015D3.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2015/d3/Y2015D3.kt
@@ -3,23 +3,21 @@ package org.gristle.adventOfCode.y2015.d3
 import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.Coord
 import org.gristle.adventOfCode.utilities.isEven
+import org.gristle.adventOfCode.utilities.partitionIndexed
 
 class Y2015D3(private val input: String) : Day {
 
-    override fun part1() = input
+    private fun deliver(s: String) = s
         .runningFold(Coord.ORIGIN) { santa, dir -> santa.move(dir) }
         .toSet()
-        .size
 
-    override fun part2() = input
-        .runningFoldIndexed(Coord.ORIGIN to Coord.ORIGIN) { index, (santa, robot), dir ->
-            if (index.isEven()) {
-                santa.move(dir) to robot
-            } else {
-                santa to robot.move(dir)
-            }
-        }.flatMap { (santa, robot) -> listOf(santa, robot) }
-        .toSet()
+    override fun part1() = deliver(input).size
+
+    override fun part2(): Int = input
+        .partitionIndexed { index, _ -> index.isEven() }
+        .toList()
+        .map(::deliver)
+        .reduce(Set<Coord>::union)
         .size
 }
 
