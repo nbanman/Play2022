c3d67f387a3fc5e278f243206d44ddcc01fdc2cc
Neil Banman <neil.banman@gmail.com>
2023-11-30 22:58:35 -0800

    initial

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d1/Y2023D1.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d1/Y2023D1.kt
@@ -1,18 +1,37 @@
 package org.gristle.adventOfCode.y2023.d1
 
 import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.utilities.toDigit
 
 class Y2023D1(input: String) : Day {
 
-    private val parsed = input.lines()
+    private val lines = input.lines()
 
-    override fun part1() = "To be implemented"
+    override fun part1() = lines.sumOf { line ->
+        line.first { it.isDigit() }.toDigit() * 10 + line.last { it.isDigit() }.toDigit()
+    }
 
-    override fun part2() = "To be implemented"
+    private val pattern = """one|two|three|four|five|six|seven|eight|nine|[0-9]""".toRegex()
+    private val reversePattern = """eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|[0-9]""".toRegex()
+
+    private fun String.toDigit() = when (this) {
+        "one", "eno" -> 1
+        "two", "owt" -> 2
+        "three", "eerht" -> 3
+        "four", "ruof" -> 4
+        "five", "evif" -> 5
+        "six", "xis" -> 6
+        "seven", "neves" -> 7
+        "eight", "thgie" -> 8
+        "nine", "enin" -> 9
+        else -> this.toInt()
+    }
+
+    override fun part2() = lines.sumOf { line ->
+        val firstDigit = pattern.findAll(line).first().value.toDigit()
+        val secondDigit = reversePattern.findAll(line.reversed()).first().value.toDigit()
+        firstDigit * 10 + secondDigit
+    }
 }
 
-fun main() = Day.runDay(Y2023D1::class, sampleInput)
-
-private val sampleInput = listOf(
-    """""",
-)
+fun main() = Day.runDay(Y2023D1::class)
