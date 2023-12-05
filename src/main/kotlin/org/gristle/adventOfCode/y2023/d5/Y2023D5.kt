f44fd6057899e5aa2105d461635986ad481e4ee2
Neil Banman <neil.banman@gmail.com>
2023-12-05 11:13:03 -0800

    refactor, 10s -> 26ms

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d5/Y2023D5.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d5/Y2023D5.kt
@@ -3,12 +3,38 @@ package org.gristle.adventOfCode.y2023.d5
 import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.blankSplit
 import org.gristle.adventOfCode.utilities.getLongList
+import kotlin.math.min
 
 class Y2023D5(input: String) : Day {
 
     data class Listing(val sourceStart: Long, val destinationStart: Long, val length: Long) {
-        val sourceRange = sourceStart until sourceStart + length
-        val destinationRange = destinationStart until destinationStart + length
+        private val sourceRange = sourceStart until sourceStart + length
+
+        // returns up to 3 sub-ranges
+        fun splitAndSend(source: LongRange): List<LongRange> = buildList {
+
+            // first is anything before the Listing, no offset
+            if (source.first < sourceRange.first) {
+                add(source.first..min(source.last, sourceRange.first))
+            }
+
+            // second is any overlap with the Listing, offset
+            val offset = destinationStart - sourceStart
+            if (source.last >= sourceRange.first && source.first <= sourceRange.last) {
+                val newFirst = if (isEmpty()) source.first else sourceStart
+                add(newFirst + offset..min(source.last, sourceRange.last) + offset)
+            }
+
+            // third is anything after the Listing, no offset
+            when {
+                isEmpty() -> add(source)
+                last().last == source.last -> {}
+                last().last == source.last + offset -> {}
+                else -> {
+                    add(last().last + 1 - offset..source.last)
+                }
+            }
+        }
     }
 
     private val seeds: List<Long>
@@ -29,51 +55,39 @@ class Y2023D5(input: String) : Day {
             }
     }
 
-    private fun List<Listing>.convert(
-        source: Long,
-        from: Listing.() -> LongRange,
-        to: Listing.() -> LongRange
-    ): Long =
-        firstOrNull { listing -> source in listing.from() }
-            ?.let { listing -> listing.to().first + source - listing.from().first }
-            ?: source
+    fun solve(seedRanges: List<LongRange>): Long = seedRanges
+        .minOf { seedRange -> // for each seedRange, find the smallest end number
 
-    private fun List<List<Listing>>.convertAll(
-        source: Long,
-        from: Listing.() -> LongRange,
-        to: Listing.() -> LongRange
-    ): Long = fold(source) { acc, listings -> listings.convert(acc, from, to) }
-
-    override fun part1() =
-        seeds.minOf { conversions.convertAll(it, Listing::sourceRange, Listing::destinationRange) }
-
-    override fun part2(): Any {
-        val seedRanges: List<LongRange> = seeds
-            .chunked(2) { (start, length) -> start until start + length }
-            .sortedBy { it.first }
-        val testRange: LongRange = conversions
-            .last()
-            .minBy { it.destinationStart }
-            .destinationRange
-        return testRange
-            .asSequence()
-            .map { it to conversions.reversed().convertAll(it, Listing::destinationRange, Listing::sourceRange) }
-            .filter { (_, source) ->
-                seedRanges.any {
-                    source in it
+            // feed a list of ranges through a gauntlet of conversion stages. the list of ranges will grow each step
+            // as each conversion handles different parts of the range differently.
+            val subRanges = conversions.fold(listOf(seedRange)) { ranges, listings ->
+                ranges.flatMap { range ->
+                    // fold constantly breaks down the last range in the list,
+                    listings.fold(mutableListOf(range)) { subRanges, listing ->
+                        if (subRanges.last().last == range.last) {
+                            subRanges.addAll(listing.splitAndSend(subRanges.removeLast()))
+                        }
+                        subRanges
+                    }
                 }
-            }.first()
-            .first
-    }
+            }
+            subRanges.minOf { it.first }
+        }
+
+
+    override fun part1() = solve(seeds.map { it..it })
+
+    override fun part2() = solve(seeds.chunked(2) { (start, length) -> start until start + length })
 }
 
 fun main() = Day.runDay(Y2023D5::class)
 
 //    Class creation: 11ms
-//    Part 1: 379811651 (1ms)
-//    Part 2: 27992443 (9549ms)
-//    Total time: 9562ms
+//    Part 1: 379811651 (8ms)
+//    Part 2: 27992443 (7ms)
+//    Total time: 26ms
 
+@Suppress("unused")
 private val sampleInput = listOf(
     """seeds: 79 14 55 13
 
