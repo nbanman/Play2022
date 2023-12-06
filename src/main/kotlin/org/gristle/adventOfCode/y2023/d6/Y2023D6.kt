8225c2a310a46f5be70fad424906bb7bec6f0e3e
Neil Banman <neil.banman@gmail.com>
2023-12-06 11:10:28 -0800

    initial

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d6/Y2023D6.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d6/Y2023D6.kt
@@ -1,19 +1,46 @@
 package org.gristle.adventOfCode.y2023.d6
 
 import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.utilities.getLongList
+import org.gristle.adventOfCode.utilities.lines
+import org.gristle.adventOfCode.utilities.transpose
 
-class Y2023D6(input: String) : Day {
+class Y2023D6(private val input: String) : Day {
 
-    private val parsed = input.lines()
+    private fun waysToWin(race: Pair<Long, Long>): Int {
+        val (time, distance) = race
+        val halfTime = time / 2
+        var backHalf = 0
+        for (operand in halfTime..distance) {
+            if (operand * (time - operand) > distance) backHalf++ else break
+        }
+        return (backHalf - 1) * 2 - if (time and 1L == 0L) 1 else 0
+    }
 
-    override fun part1() = "To be implemented"
+    private inline fun solve(parse: (String) -> List<Long>): Int {
+        val races: List<Pair<Long, Long>> = input
+            .lines(parse)
+            .transpose()
+            .map { (time, distance) -> time to distance }
 
-    override fun part2() = "To be implemented"
+        return races.map(::waysToWin).reduce(Int::times)
+    }
+
+    override fun part1() = solve { it.getLongList() }
+
+    override fun part2() = solve { it.filter(Char::isDigit).getLongList() }
 }
 
 fun main() = Day.runDay(Y2023D6::class)
 
+//    Class creation: 2ms
+//    Part 1: 2374848 (2ms)
+//    Part 2: 39132886 (14ms)
+//    Total time: 20ms
+
 @Suppress("unused")
 private val sampleInput = listOf(
-    """""",
+    """Time:      7  15   30
+Distance:  9  40  200
+""" to "288",
 )
