b5f0fb55aa6df7e65949658e0f9910a6e29064ce
Neil Banman <neil.banman@gmail.com>
2023-12-06 11:27:06 -0800

    added comments

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d6/Y2023D6.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d6/Y2023D6.kt
@@ -2,33 +2,42 @@ package org.gristle.adventOfCode.y2023.d6
 
 import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.getLongList
-import org.gristle.adventOfCode.utilities.lines
 import org.gristle.adventOfCode.utilities.transpose
 
 class Y2023D6(private val input: String) : Day {
 
-    private fun waysToWin(race: Pair<Long, Long>): Int {
-        val (time, distance) = race
-        val halfTime = time / 2
-        var backHalf = 0
-        for (operand in halfTime..distance) {
-            if (operand * (time - operand) > distance) backHalf++ else break
-        }
-        return (backHalf - 1) * 2 - if (time and 1L == 0L) 1 else 0
-    }
-
-    private inline fun solve(parse: (String) -> List<Long>): Int {
-        val races: List<Pair<Long, Long>> = input
-            .lines(parse)
-            .transpose()
-            .map { (time, distance) -> time to distance }
-
-        return races.map(::waysToWin).reduce(Int::times)
-    }
-
     override fun part1() = solve { it.getLongList() }
 
     override fun part2() = solve { it.filter(Char::isDigit).getLongList() }
+
+    private inline fun solve(parseLine: (String) -> List<Long>): Int {
+        
+        // parses input by parsing the digits in each input line according to the rules for each part, then
+        // transposing the list of lists so that the vertical columns are grouped together.
+        val races: List<List<Long>> = input
+            .lines()
+            .map(parseLine)
+            .transpose()
+        
+        return races.map(::waysToWin).reduce(Int::times)
+    }
+
+    private fun waysToWin(race: List<Long>): Int {
+        val (time, distance) = race
+        var backHalfCount = 0
+        
+        // the largest product is when the two operands are the same (or as close as can be for odd times),
+        // so start there. If product > distance, increment the count and the first operand. Do that until
+        // every combination in the back half has been checked or product <= distance
+        for (operand in (time / 2) until distance) {
+            if (operand * (time - operand) > distance) backHalfCount++ else break
+        }
+        
+        // only the back half was counted, so double it, since the first half is a mirror of the second.
+        // even-numbered times lose one count because there is a singular "peak" that isn't mirrored on the other
+        // side. e.g. t=8 -> 7, 12, 15, *16*, 15, 12, 7
+        return (backHalfCount - 1) * 2 - if (time and 1L == 0L) 1 else 0
+    }
 }
 
 fun main() = Day.runDay(Y2023D6::class)
