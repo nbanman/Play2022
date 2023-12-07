3767da4919ef506d29d251737277d9059c7fcb96
Neil Banman <neil.banman@gmail.com>
2023-12-07 09:21:45 -0800

    added comments

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d7/Y2023D7.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d7/Y2023D7.kt
@@ -3,7 +3,7 @@ package org.gristle.adventOfCode.y2023.d7
 import org.gristle.adventOfCode.Day
 
 class Y2023D7(input: String) : Day {
-
+    
     private val hands = input
         .lines()
         .map { line -> 
@@ -11,44 +11,57 @@ class Y2023D7(input: String) : Day {
             Hand(cards, bid.toInt())
         }
     
+    // takes the hands, sorts by the hand strength as defined by each puzzle part, assigns points using rank and
+    // bid amount, then returns sum of all points
     private fun solve(strength: (Hand) -> Int) = hands
         .sortedWith(compareBy(strength))
         .mapIndexed { index, hand -> (index + 1) * hand.bid }
         .sum()
-
+    
     override fun part1() = solve(Hand::strength)
 
     override fun part2() = solve(Hand::strengthWild)
     
     data class Hand(val cards: String, val bid: Int) {
+
+        // groups cards together, for use in determining handStrength. Sorted because the relative size of the groups
+        // is used to determine what kind of hand we have 
         private val handCount: List<Pair<Char, Int>> = cards
             .groupingBy { it }
             .eachCount()
             .map { (card, amt) -> card to amt }
             .sortedWith(compareByDescending { (_, amt) -> amt })
 
-        private val handStrength: Int = handCount
+        // take the two most populous groups in the hand, perform arcane function to deliver ordered ranking of each
+        // hand type. Arcane function: [biggest group size] * 2 + [2nd biggest group size]. Then normalized to 0..6.
+        private val handType: Int = handCount
             .take(2)
             .foldIndexed(0) { index, acc, (_, amt) -> acc + (2 - index) * amt }
             .let { raw -> (raw - 4).coerceAtLeast(0) }
         
-        val strength: Int = cards.fold(handStrength) { acc, card -> (acc shl 4) + cardValues.getValue(card) }
+        // strength is an Int that we use to sort with. The most important thing is handType, after that, the value
+        // of the cards, in order. E.g., 98 > 8A, because 9 is greater than 8. We can represent this all as an Int
+        // concatenating them all, giving 4 bits for each value.
+        val strength: Int = cards.fold(handType) { acc, card -> (acc shl 4) + cardValues.getValue(card) }
+        
+        // strengthWild does the same thing as strength, but first it adjusts the handType to accommodate Jokers.
+        // And it also uses a different card value ranking.
         val strengthWild: Int = let {
-            val handStrengthWild: Int =
+            
+            // changes the hand type if a joker is found
+            val handTypeWild: Int =
                 if (cards.contains('J')) {
-                    when (handStrength) {
-                        4, 5, 6 -> 6
-                        3 -> 5
-                        2 -> if (handCount.last().first == 'J') 4 else 5
-                        1 -> 3
-                        else -> 1
+                    when (handType) {
+                        4, 5, 6 -> 6 // full house to 5-kind all become 5-kind
+                        3 -> 5 // 3-kind becomes 4-kind
+                        2 -> if (handCount.last().first == 'J') 4 else 5 // if JJ found, full house, else 3-kind
+                        1 -> 3 // pair becomes 3-kind
+                        else -> 1 // high card becomes pair
                     }
                 } else {
-                    handStrength
+                    handType
                 }
-            cards.fold(handStrengthWild) { acc, card ->
-                (acc shl 4) + cardValuesWild.getValue(card)
-            }
+            cards.fold(handTypeWild) { acc, card -> (acc shl 4) + cardValuesWild.getValue(card) }
         }
         
         companion object {
@@ -67,6 +80,11 @@ class Y2023D7(input: String) : Day {
 }
 
 fun main() = Day.runDay(Y2023D7::class)
+
+//    Class creation: 48ms
+//    Part 1: 253866470 (3ms)
+//    Part 2: 254494947 (3ms)
+//    Total time: 55ms
 
 @Suppress("unused")
 private val sampleInput = listOf(
