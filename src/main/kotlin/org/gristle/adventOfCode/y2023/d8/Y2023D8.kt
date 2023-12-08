fc830d5e85f24159d2f30cff9a57882359b293cc
Neil Banman <neil.banman@gmail.com>
2023-12-07 23:40:42 -0800

    added comments

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d8/Y2023D8.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d8/Y2023D8.kt
@@ -1,23 +1,21 @@
 package org.gristle.adventOfCode.y2023.d8
 
 import org.gristle.adventOfCode.Day
-import org.gristle.adventOfCode.utilities.Nsew
 import org.gristle.adventOfCode.utilities.blankSplit
 import org.gristle.adventOfCode.utilities.lcm
 
 class Y2023D8(input: String) : Day {
 
-    private val directions: Sequence<Nsew>
+    private val directions: Sequence<Char>
     private val network: Map<String, Pair<String, String>>
     
     init {
         val (dirStr, netStr) = input.blankSplit()
-        directions = sequence { 
-            val dirList = dirStr.map(Nsew::of)
-            while (true) {
-                for (dir in dirList) yield(dir)
-            }
-        }
+        
+        // infinite sequence of directions (repeats after string ends)
+        directions = sequence { while (true) for (dir in dirStr) yield(dir) }
+        
+        // representing a puzzle map with a Kotlin map
         network = buildMap {
             Regex("""\w{3}""")
                 .findAll(netStr)
@@ -27,20 +25,28 @@ class Y2023D8(input: String) : Day {
         }
     }
     
+    // the 'sequence' builder lets you build sequences based on other sequences, in a way that you have access
+    // to the state of both at the same time. In this case, it takes the sequence of directions, and relies on
+    // that to deliver a sequence of Strings from the network map.
     private fun traverseSequence(startNode: String): Sequence<String> = sequence { 
         var node = startNode
         yield(node)
         for (dir in directions) {
-            node = network.getValue(node).let { (left, right) -> if (dir == Nsew.WEST) left else right }
+            node = network.getValue(node).let { (left, right) -> if (dir == 'L') left else right }
             yield(node)
         }
     }
 
+    // deliver steps needed for traveler to go from startNode to an end condition,
     private inline fun traverse(startNode: String, endCondition: (String) -> Boolean): Int = 
         traverseSequence(startNode).indexOfFirst(endCondition)
     
+    // basic
     override fun part1() = traverse("AAA") { it == "ZZZ" }
 
+    // find how long it takes for each ghost to make it from a to z, making the *massive* assumption that
+    // they immediately cycle (they do). These are all different (though coprime). So lcm or just multiplying them
+    // all together gives you the answer.
     override fun part2(): Long {
         val startNodes = network.keys
             .filter { it.last() == 'A' }
