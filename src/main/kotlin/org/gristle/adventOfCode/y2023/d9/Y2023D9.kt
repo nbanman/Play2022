80d123e317a7e111f4e9401dc2bc447b9b9d186d
Neil Banman <neil.banman@gmail.com>
2023-12-08 23:41:32 -0800

    initial

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d9/Y2023D9.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d9/Y2023D9.kt
@@ -1,19 +1,38 @@
 package org.gristle.adventOfCode.y2023.d9
 
 import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.utilities.getIntList
 
 class Y2023D9(input: String) : Day {
 
-    private val parsed = input.lines()
+    private val patterns = input
+        .lines()
+        .map(String::getIntList)
 
-    override fun part1() = "To be implemented"
+    private fun findNext(pattern: List<Int>) = generateSequence(pattern) { it.zipWithNext { a, b -> b - a } }
+        .takeWhile { next -> next.any { it != 0 } }
+    
+    override fun part1() = patterns.sumOf { pattern -> findNext(pattern).sumOf { it.last() } }
 
-    override fun part2() = "To be implemented"
+    override fun part2() = patterns.sumOf { pattern ->
+        val initial = 0L.toInt() // lousy hack to make the compiler/linter shut up
+        findNext(pattern)
+            .map { it.first() }
+            .toList()
+            .foldRight(initial) { i, acc -> i - acc }
+    }
 }
 
 fun main() = Day.runDay(Y2023D9::class)
 
+//    Class creation: 20ms
+//    Part 1: 1974913025 (6ms)
+//    Part 2: 884 (4ms)
+//    Total time: 32ms
+
 @Suppress("unused")
 private val sampleInput = listOf(
-    """""",
+    """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45""",
 )
