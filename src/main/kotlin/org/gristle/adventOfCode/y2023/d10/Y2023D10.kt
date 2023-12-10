d6a9bf3878232df2ce1500475bb54d1e1c2bc5b8
Neil Banman <neil.banman@gmail.com>
2023-12-10 15:55:13 -0800

    10x speedup using math

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d10/Y2023D10.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d10/Y2023D10.kt
@@ -2,79 +2,64 @@ package org.gristle.adventOfCode.y2023.d10
 
 import org.gristle.adventOfCode.Day
 import org.gristle.adventOfCode.utilities.*
-import java.lang.IllegalStateException
+import kotlin.math.absoluteValue
 
 class Y2023D10(input: String) : Day {
 
-    private val pipes: Grid<Char> = input.toGrid()
+    // convert String to Grid for easy traversal/lookup
+    private val field = input.toGrid()
 
-    val edges: (Coord) -> List<Coord> = { pos ->
-        validDirections.getValue(pipes[pos])
-            .map { direction -> direction to pos.move(direction) }
-            .filter { (direction, neighborPos) ->
-                pipes.validCoord(neighborPos) &&
-                        direction in receivingDirections.getValue(pipes[neighborPos])
-            }.map { (_, neighborPos) -> neighborPos }
-    }
-    
-    private val start = pipes.coordOfElement('S')
-    
-    private val loop: List<Graph.Vertex<Coord>> by lazy {
-        Graph.bfs(start, defaultEdges = edges)
-    } 
-
-    override fun part1() = loop.maxOf { it.weight }.toInt()
-    
-    override fun part2(): Int {
-
-        val startMimic: Char = let {
-            val neighbors: Set<Nsew> = validDirections.getValue(pipes[start])
-                .map { direction -> direction to start.move(direction) }
-                .filter { (direction, neighborPos) ->
-                    pipes.validCoord(neighborPos) &&
-                            direction in receivingDirections.getValue(pipes[neighborPos])
-                }.map { (direction, _) -> direction }
-                .toSet()
-            validDirections.entries.find { it.value == neighbors }
-                ?.key
-                ?: throw IllegalStateException("Cannot find 'S' connectors.")
-        }
-
-        val loopSet = loop.map { it.id }.toSet()
-
-        val expandedPipe: Set<Coord> = loopSet.flatMap { pos ->
-            val fitting: Char = pipes[pos].let { if (it == 'S') startMimic else it }
-            val x = pos.x * 3
-            val y = pos.y * 3
-            fitting.expand().map { Coord(x + it % 3, y + it / 3) }
-        }.toSet()
-
-        val expandedEdges: (Coord) -> List<Coord> = { pos ->
-            pos.getNeighbors()
-                .filter { neighbor ->
-                    neighbor.x in 0 until pipes.width * 3
-                            && neighbor.y in 0 until pipes.height * 3 
-                            && neighbor !in expandedPipe
-                }
-        } 
+    // list of directions taken by the loop
+    private val loop: List<Nsew> by lazy {
         
-        val outsideOfLoop: Set<Coord> = Graph.bfs(Coord.ORIGIN, defaultEdges = expandedEdges)
-            .map { it.id }
-            .toSet()
+        // start at 'S'
+        val startPos = field.coordOfElement('S')
         
-        val pipePlusFlood = expandedPipe + outsideOfLoop
-
-        val shortSequence = (0..9).asSequence()
-        
-        return pipes.coords().count { pos ->
-            val x = pos.x * 3
-            val y = pos.y * 3
-            shortSequence.none { 
-                Coord(x + it % 3, y + it / 3) in pipePlusFlood
+        // find the initial direction by looking in each direction, and pick the first one that has a pipe
+        // fitting that connects back to the start
+        val startDir = validDirections.getValue('S') // get directions
+            .first { direction -> // pick the first direction pointing to a fitting pointing back to startPos
+                val neighborPos = startPos.move(direction)
+                field.validCoord(neighborPos) &&
+                        direction in validDirections.getValue(field[neighborPos]).map { it.flip() }
             }
+        
+        // lambda from moving along the pipe, taking in the position and direction and returning the next
+        // position and direction
+        val move: (Pair<Coord, Nsew>) -> Pair<Coord, Nsew> = { (pos, dir) ->
+            val newPos = pos.move(dir)
+            val newDir = (validDirections.getValue(field[newPos]) - dir.flip()).first()
+            newPos to newDir
         }
+        
+        // start at the position and direction *after* 'S', then continue until we hit 'S'. This gives a list
+        // of all the fittings in the loop.
+        generateSequence(move(startPos to startDir), move)
+            .takeUntil { (pos, _) -> field[pos] == 'S' }
+            .map { (_, dir) -> dir }
+            .toList()
     }
-    
+
+    // Furthest direction is half-way along the loop.
+    override fun part1() = loop.size / 2
+
+    // Get the area of the loop, then get the inner points using Pick's theorem.
+    // Area algorithm based on Stokes' theorem. h/t Jakub Gwozdz
+    override fun part2(): Int {
+        val area = loop
+            .fold(0 to 0) { (sum, d), dir ->
+                when (dir) {
+                    Nsew.NORTH -> sum to d + 1
+                    Nsew.SOUTH -> sum to d - 1
+                    Nsew.WEST -> sum - d to d
+                    Nsew.EAST -> sum + d to d
+                }
+            }.first.absoluteValue
+        // Pick's theorem: area = [interior points] + ([boundary points / 2]) - 1
+        // Solve for i -> i = - (b / 2) - 1
+        return area - (loop.size / 2) + 1
+    }
+
     companion object {
         private val validDirections: Map<Char, Set<Nsew>> = listOf(
             'S' to setOf(Nsew.NORTH, Nsew.EAST, Nsew.SOUTH, Nsew.WEST),
@@ -86,30 +71,15 @@ class Y2023D10(input: String) : Day {
             'F' to setOf(Nsew.SOUTH, Nsew.EAST),
             '.' to emptySet()
         ).toMap()
-
-        private val receivingDirections: Map<Char, Set<Nsew>> =
-            validDirections.entries.associate { (section, directions) ->
-                section to directions.map { it.flip() }.toSet()
-            }
-
-        private fun Char.expand() = when (this) {
-            '|' -> listOf(1, 4, 7)
-            '-' -> listOf(3, 4, 5)
-            'L' -> listOf(1, 4, 5)
-            'J' -> listOf(1, 3, 4)
-            '7' -> listOf(3, 4, 7)
-            'F' -> listOf(4, 5, 7)
-            else -> emptyList()
-        }
     }
 }
 
 fun main() = Day.runDay(Y2023D10::class)
 
-//    Class creation: 15ms
-//    Part 1: 7086 (52ms)
-//    Part 2: 317 (465ms)
-//    Total time: 532ms
+//    Class creation: 13ms
+//    Part 1: 7086 (27ms)
+//    Part 2: 317 (4ms)
+//    Total time: 45ms
 
 @Suppress("unused")
 private val sampleInput = listOf(
