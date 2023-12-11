d474f845124df55f3740006c6f6754b2f655d8bb
Neil Banman <neil.banman@gmail.com>
2023-12-11 12:20:53 -0800

    refactor, 2x speedup (where ephemient goes, I follow)

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d11/Y2023D11.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d11/Y2023D11.kt
@@ -1,49 +1,58 @@
 package org.gristle.adventOfCode.y2023.d11
 
 import org.gristle.adventOfCode.Day
-import org.gristle.adventOfCode.utilities.minMax
+import org.gristle.adventOfCode.utilities.Grid
+import org.gristle.adventOfCode.utilities.toGrid
+import kotlin.math.abs
 
-class Y2023D11(private val image: String) : Day {
-    
-    // working with string directly, so need width and height of grid in order to traverse up/down/left/right
-    private val width: Int = image.indexOf('\n') + 1
-    
-    // note that it is image.length '+ 1' because my input has trim() called on it before getting to me, getting
-    // rid of the last '\n'
-    private val height: Int = (image.length + 1) / width
-    
-    // goes row by row (then column by column), keeping only those where there are no galaxies
-    private val xExpansion: List<Int> = (0 until width - 1).filter { x -> 
-        (0 until height).all { y -> image[x + y * width] == '.' } 
-    }
-    private val yExpansion: List<Int> = (0 until height).filter { y ->
-        (0 until width - 1).all { x -> image[x + y * width] == '.' }
-    }
-    
-    // list o'galaxies
-    private val galaxies: List<Int> = buildList { 
-        image.forEachIndexed { index, c -> if (c == '#') add(index) }
-    }
+class Y2023D11(input: String) : Day {
+    private val image: Grid<Boolean> = input.toGrid { it == '#' }
 
-    private fun shortestPath(a: Int, b: Int, expansionFactor: Int): Long {
-        // negative ranges don't work, so we need to know which is largest and smallest on each axis
-        val (minX, maxX) = minMax(a % width, b % width)
-        val (minY, maxY) = minMax(a / width, b / width)
-        
-        // get distance w/o taking expansion into account
-        val manhattanDistance = (maxX - minX + maxY - minY).toLong()
-        
-        // for every expansion row and column, add additional distance
-        val xExpansion = xExpansion.count { it in (minX + 1)..<maxX } * (expansionFactor - 1)
-        val yExpansion = yExpansion.count { it in (minY + 1)..<maxY } * (expansionFactor - 1)
-        return manhattanDistance + xExpansion + yExpansion
-    }
-    
-    fun solve(expansionFactor: Int): Long = galaxies 
+    // for each axis, get indexed iterables of pairs. The first being the index where galaxies reside, the 
+    // second being the number of galaxies. We separate the axes to avoid repeat calculation.
+    private val xGal = image.xIndices
+        .map { x -> x to image.column(x).count { it } }
+        .filter { (_, count) -> count > 0 }
         .withIndex()
-        .sumOf { (index, a) ->
-            galaxies.drop(index + 1).sumOf { b -> shortestPath(a, b, expansionFactor) }
+    private val yGal = image.yIndices
+        .map { y -> y to image.row(y).count { it } }
+        .filter { (_, count) -> count > 0 }
+        .withIndex()
+
+    // for each axis, track the indices representing expansion fields
+    private val xExpansions: List<Int> = image.xIndices.filter { x -> image.column(x).none { it } }
+    private val yExpansions: List<Int> = image.yIndices.filter { y -> image.row(y).none { it } }
+
+    // run the distance function twice (once for each axis), return the sum 
+    fun solve(expansionFactor: Int): Long = distance(expansionFactor, xGal, xExpansions) +
+            distance(expansionFactor, yGal, yExpansions)
+
+    // Get the distance between two indices where galaxies reside.
+    // This involves calculating the unexpanded difference multiplied by the number of expansions passed times
+    // the expansion factor
+    // Lastly, the distance is multiplied by #galaxies in the first index multiplied by #galaxies in the second index.
+    fun distance(
+        expansionFactor: Int,
+        galaxies: Iterable<IndexedValue<Pair<Int, Int>>>,
+        expansions: List<Int>
+    ): Long {
+        return galaxies.sumOf { (i, a) ->
+            val (aPos, aCount) = a
+
+            // calculate which expansions are to the left of the source galaxies
+            // this returns a negative number due to how binarySearch returns values but this will be rectified
+            // later.
+            val alreadyPassed = expansions.binarySearch(aPos)
+            galaxies.drop(i + 1).sumOf { (_, b) ->
+                val (bPos, bCount) = b
+
+                // calculates which expansions are to the left of the destination galaxies, and subtracts the
+                // ones to the left of the source galaxies. The abs function handles the negative value.
+                val expansionsPassed = abs(expansions.binarySearch(bPos) - alreadyPassed)
+                ((bPos - aPos) + expansionsPassed.toLong() * (expansionFactor - 1)) * aCount * bCount
+            }
         }
+    }
 
     override fun part1() = solve(2)
 
@@ -52,10 +61,10 @@ class Y2023D11(private val image: String) : Day {
 
 fun main() = Day.runDay(Y2023D11::class)
 
-//    Class creation: 7ms
-//    Part 1: 9545480 (49ms)
-//    Part 2: 406725732046 (17ms)
-//    Total time: 74ms
+//    Class creation: 18ms
+//    Part 1: 9545480 (12ms)
+//    Part 2: 406725732046 (6ms)
+//    Total time: 37ms
 
 @Suppress("unused")
 private val sampleInput = listOf(
