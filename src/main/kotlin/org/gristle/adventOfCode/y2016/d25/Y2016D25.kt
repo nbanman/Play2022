6a0c6e09261c02d34a1ee10cc5d864d6540c0426
Neil Banman <neil.banman@gmail.com>
2023-12-11 11:26:45 -0800

    derive offset from the assembunny instructions

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d25/Y2016D25.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d25/Y2016D25.kt
@@ -1,13 +1,11 @@
 package org.gristle.adventOfCode.y2016.d25
 
 import org.gristle.adventOfCode.Day
-import org.gristle.adventOfCode.utilities.getInts
+import org.gristle.adventOfCode.y2016.shared.Assembunny
 
-class Y2016D25(input: String) : Day {
+class Y2016D25(private val input: String) : Day {
 
-    private val offset = input.getInts().first()
-
-    private fun Int.isClockSignal() = generateSequence(this) { it / 2 }
+    private fun Int.isClockSignal() = generateSequence(this) { it shr 1 }
         .takeWhile { it != 0 }
         .withIndex()
         .all { (idx, value) -> idx and 1 == value and 1 }
@@ -15,8 +13,17 @@ class Y2016D25(input: String) : Day {
     // Reverse engineering the code ends with infinite loop that prints reverse binary representation of d over and
     // over again, and defines d = a + 2555. So a = d - 2555. Starting from 2555, the first number that is
     // 10101010...10 is 2730. So that's d. A is 2555 less than that, or 175.    
-    override fun part1() = generateSequence(1) { it + 1 }
-        .first { (it + offset).isClockSignal() }
+    override fun part1(): Int {
+        
+        val instructions = Assembunny.parseInstructions(input)
+        
+        val offset = Assembunny().runAsSequence(instructions)
+            .take(10_000)
+            .last()[3]
+        
+        return generateSequence(1) { it + 1 }
+            .first { (it + offset).isClockSignal() }
+    } 
 
     override fun part2() = null
 }
