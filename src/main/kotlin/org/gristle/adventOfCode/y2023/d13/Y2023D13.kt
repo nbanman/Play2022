4d4a957c3f033dc6c02de487d062f93e8cf28e1e
Neil Banman <neil.banman@gmail.com>
2023-12-13 10:34:09 -0800

    added comments

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d13/Y2023D13.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d13/Y2023D13.kt
@@ -7,6 +7,9 @@ import org.gristle.adventOfCode.utilities.toGrid
 
 class Y2023D13(input: String) : Day {
 
+    // The value for part 1 is used in part 2, so make it a class value. We want representative times for part 1,
+    // so make the value lazy. This parses by splitting on blank lines, turning each split into a Grid, then
+    // running the findSeam function using Grid components. The Grid and the answer are returned in a Pair.
     private val patterns: List<Pair<Grid<Boolean>, Int>> by lazy {
         input.blankSplit()
             .map { s ->
@@ -20,8 +23,12 @@ class Y2023D13(input: String) : Day {
             }
     } 
     
-
-    private fun Grid<Boolean>.findSeam(length: Int, lines: List<List<Boolean>>, factor: Int): Int? {
+    // Finds the initial seam. It uses a stack that starts at the left/top and keeps adding lines until
+    // a duplicate is found. That is a potential seam. It verifies the seam by popping lines off the stack and
+    // comparing them to the next lines. If the stack value and the next line value don't add up, it's not a valid
+    // seam, and the values are reset and the outer loop is resumed. If it reaches the end of the stack or the 
+    // end of the lines, the seam is valid and that value is returned.  
+    private fun findSeam(length: Int, lines: List<List<Boolean>>, factor: Int): Int? {
         val stack: MutableList<List<Boolean>> = ArrayList(length - 1)
         stack.add(lines.first())
         var potentialSeam = -1
@@ -36,8 +43,8 @@ class Y2023D13(input: String) : Day {
                 inner@while (true) {
                     stack.removeLast()
                     if (stack.isEmpty()) break@loop
-                    val next = lines.getOrNull(++location) ?: break@loop
-                    if (stack.last() != next) { // potential seam not real
+                    val innerNext = lines.getOrNull(++location) ?: break@loop
+                    if (stack.last() != innerNext) { // potential seam not real
                         location = potentialSeam - (location - potentialSeam)
                         while (location != potentialSeam + 1) {
                             stack.add(lines[location])
@@ -53,7 +60,13 @@ class Y2023D13(input: String) : Day {
         return if (potentialSeam == -1) null else potentialSeam * factor
     }
 
-    private fun Grid<Boolean>.findSmudgeSeam(length: Int, lines: List<List<Boolean>>, factor: Int, ignore: Int?): Int? {
+    // Finds the seam with a smudge. The logic is the same as findSeam, except for two things. First, it will
+    // ignore the seam if the index is passed in by the 'ignore' parameter. This prevents finding the old seam.
+    // Second, when making comparisons, it will allow a difference of 1, but only once for any potential seam.
+    // So if 'smudge' is used to identify the potential seam, then the popping/comparison phase must have 
+    // identical comparisons. If not, then the popping/comparison phase can have one comparison with a difference
+    // of one. This is tracked with a 'smudged' Boolean variable.
+    private fun findSmudgeSeam(length: Int, lines: List<List<Boolean>>, factor: Int, ignore: Int?): Int? {
         val stack: MutableList<List<Boolean>> = ArrayList(length - 1)
         stack.add(lines.first())
         var potentialSeam = -1
@@ -91,6 +104,7 @@ class Y2023D13(input: String) : Day {
         return if (potentialSeam == -1) null else potentialSeam * factor
     }
     
+    // Utility function to return the number of different elements between two lists.
     private fun List<Boolean>.difference(other: List<Boolean>): Int {
         return (this zip other).count { (a, b) -> a != b }
     }
@@ -98,20 +112,19 @@ class Y2023D13(input: String) : Day {
     override fun part1() = patterns.sumOf { (_, value) -> value }
 
     override fun part2() = patterns
-        .mapIndexed { index, (pattern, value) ->
+        .sumOf { (pattern, value) ->
+            
+            // The indices of previous seams to ignore. 
             val ignoreHorizontal = if (value < 100) value else null
             val ignoreVertical = if (value >= 100) value / 100 else null
+            
             with (pattern) {
                 val answer = findSmudgeSeam(width, columns(), 1, ignoreHorizontal)
                     ?: findSmudgeSeam(height, rows(), 100, ignoreVertical)
                     ?: throw IllegalStateException("No seam found in columns or rows.")
                 answer
             }
-        }.sum()
-    
-    companion object {
-        fun List<Boolean>.rep() = joinToString("") { if (it) "#" else "."}
-    }
+        }
 }
 
 fun main() = Day.runDay(Y2023D13::class)
