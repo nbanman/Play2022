03684a5646a68a4cc639a43bbd7f8010584ab6ac
Neil Banman <neil.banman@gmail.com>
2023-12-14 11:32:03 -0800

    20x speedup moving to Grids (still slow!)

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d14/Y2023D14.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d14/Y2023D14.kt
@@ -1,75 +1,58 @@
 package org.gristle.adventOfCode.y2023.d14
 
 import org.gristle.adventOfCode.Day
-import org.gristle.adventOfCode.utilities.Coord
-import org.gristle.adventOfCode.utilities.Nsew
-import org.gristle.adventOfCode.utilities.stabilized
+import org.gristle.adventOfCode.utilities.Grid
+import org.gristle.adventOfCode.utilities.MutableGrid
+import org.gristle.adventOfCode.utilities.toGrid
 
 class Y2023D14(input: String) : Day {
 
-    private val size: Coord
-    private val rocks: Set<Coord>
-    private val cubes: Set<Coord>
-    
-    init {
-        val width = input.indexOf('\n') + 1
-        val height = (input.length + 1) / width
-        size = Coord(width - 1, height)
-        rocks = buildSet {
-            val rocks = this
-            cubes = buildSet { 
-                for ((i, v) in input.withIndex()) {
-                    if (v in ".\n") continue
-                    val pos = Coord(i % width, i / width)
-                    if (v == 'O') rocks.add(pos) else add(pos)
+    private val initial = input.toGrid()
+
+    private fun tiltUp(rocks: Grid<Char>): Grid<Char> {
+        val tilted = MutableGrid(rocks.width, rocks.height) { '.' }
+        rocks.forEachIndexed { index, c ->
+            when (c) {
+                '#' -> tilted[index] = '#'
+                'O' -> {
+                    val tiltedIndex = generateSequence(index) { it - rocks.width }
+                        .takeWhile { it >= 0 && tilted[it] !in "#O" }
+                        .last()
+                    tilted[tiltedIndex] = 'O'
                 }
             }
         }
+        return tilted
     }
 
-    private fun Coord.canMove(rocks: Set<Coord>, dir: Nsew): Boolean = 
-        generateSequence(move(dir)) { it.move(dir) }
-            .first { it in cubes || it !in rocks }
-            .let { it !in cubes && it.x in 0 until size.x && it.y in 0 until size.y }
-    
-    private fun moveRocks(rocks: Set<Coord>, dir: Nsew): Set<Coord> = buildSet {
-        for (rock in rocks) {
-            if (rock.canMove(rocks, dir)) {
-                val moved = rock.move(dir, 1, this@Y2023D14.size)
-                if (moved in this || moved in cubes) {
-                    add(rock)
-                } else {
-                    add(moved)
-                }    
-            } else {
-                add(rock)
-            }
-        }
+    private fun Grid<Char>.load() = withIndex().sumOf { (index, c) ->
+        if (c == 'O') height - index / width else 0
     }
-    
-    private fun moveToSide(rocks: Set<Coord>, dir: Nsew) = generateSequence(rocks) { moveRocks(it, dir) }
-        .stabilized()
-    
-    private fun Coord.load() = size.y - this.y
-    
-    override fun part1() = moveToSide(rocks, Nsew.NORTH).sumOf { it.load() }
+
+    override fun part1(): Int = tiltUp(initial).load()
 
     override fun part2(): Int {
-        fun spinCycle(rocks: Set<Coord>) = listOf(Nsew.NORTH, Nsew.WEST, Nsew.SOUTH, Nsew.EAST)
-            .fold(rocks, ::moveToSide)
-        val rockFormations = mutableSetOf<Set<Coord>>()
-        val repeat = generateSequence(rocks, ::spinCycle)
+        fun spinCycle(rocks: Grid<Char>): Grid<Char> = (1..4).fold(rocks) { acc, i ->
+            tiltUp(acc).rotate90().toGrid(acc.height)
+        }
+        val rockFormations = mutableSetOf<Grid<Char>>()
+        val repeat = generateSequence(initial, ::spinCycle)
             .first { rocks -> !rockFormations.add(rocks) }
         val repeatFirst = rockFormations.indexOf(repeat)
         val cycles = 1_000_000_000 - repeatFirst
         val cycleLength = rockFormations.size - repeatFirst
         val answer = rockFormations
             .elementAt(repeatFirst + cycles % cycleLength)
-        return answer.sumOf { it.load() }
+        return answer.load()
     }
 }
 
 fun main() = Day.runDay(Y2023D14::class)
+
+//    Class creation: 9ms
+//    Part 1: 106990 (11ms)
+//    Part 2: 100531 (350ms)
+//    Total time: 371ms
 
 @Suppress("unused")
 private val sampleInput = listOf(
