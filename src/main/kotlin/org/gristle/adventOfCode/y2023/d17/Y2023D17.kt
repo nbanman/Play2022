72510cca03fd91fe1b5c5416ec77e947a4dbbb53
Neil Banman <neil.banman@gmail.com>
2023-12-17 14:11:00 -0800

    cleaned up and added comments

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d17/Y2023D17.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d17/Y2023D17.kt
@@ -9,94 +9,91 @@ import org.gristle.adventOfCode.utilities.toGrid
 
 class Y2023D17(input: String) : Day {
 
-    private val city = input.toGrid { it.digitToInt() }
+    // convert input string to Grid of heat loss values. I use Double because my standard A* library function only
+    // works in Double. 
+    private val city = input.toGrid { it.digitToInt().toDouble() }
 
+    // State tracking current position, direction, and how long crucible has been going straight.
     private data class State(val pos: Coord, val dir: Nsew, val straights: Int)
     
+    // Start, end, and simple manhattan distance heuristic used for both parts.
     private val start = State(Coord.ORIGIN, Nsew.EAST, 0)
     private val end = city.coordOf(city.lastIndex)
     private val heuristic = { state: State -> state.pos.manhattanDistance(end).toDouble() }
 
     override fun part1(): Int {
-        fun turnOrNull(state: State, turnDir: Nsew): Graph.Edge<State>? {
+        
+        // Utility function for getEdges.
+        // Generates next move in a particular direction, or null if the move goes out of range of the city Grid.
+        fun moveOrNull(state: State, turn: Nsew.() -> Nsew): Graph.Edge<State>? {
+            val turnDir = state.dir.turn()
             val pos = state.pos.move(turnDir)
-            val weight = city.getOrNull(pos)
-            return if (weight != null) {
-                val newState = State(pos, turnDir, 1)
-                Graph.Edge(newState, weight.toDouble())
-            } else {
-                null
-            }
+            val weight = city.getOrNull(pos) ?: return null
+            val straights = if (state.dir == turnDir) state.straights + 1 else 1
+            val newState = State(pos, turnDir, straights)
+            return Graph.Edge(newState, weight)
         }
         
+        // Get edges for any given state.
         val getEdges = { state: State -> 
             buildList {
                 // continue straight
                 if (state.straights < 3) {
-                    val pos = state.pos.move(state.dir)
-                    val weight = city.getOrNull(pos)
-                    if (weight != null) {
-                        val newState = State(pos, state.dir, state.straights + 1)
-                        add(Graph.Edge(newState, weight.toDouble()))
-                    }
+                    moveOrNull(state) { this }?.let { add(it) }
                 }
-                // go left
-                turnOrNull(state, state.dir.left())?.let { add(it) }
-
-                // go right
-                turnOrNull(state, state.dir.right())?.let { add(it) }
+                // go left and right
+                moveOrNull(state, Nsew::left)?.let { add(it) }
+                moveOrNull(state, Nsew::right)?.let { add(it) }
             }
         }
         
-        val path = Graph.aStar(start, heuristic, defaultEdges = getEdges)
-        return path.steps()
+        // Run A* with the edgefinder and return the shortest number of steps.
+        return Graph.aStar(start, heuristic, defaultEdges = getEdges).steps()
     }
     
     override fun part2(): Int {
-        fun move(pos: Coord, dir: Nsew, distance: Int): Pair<Coord, Double>? =
-            (1..distance).fold(pos to 0.0) { (currentPos, weight), _ ->
-                val nextPos = currentPos.move(dir)
-                if (!city.validCoord(nextPos)) return@move null
-                val newWeight = weight + city[nextPos]
-                nextPos to newWeight
-            }
 
-        val getEdges = { state: State ->
-            buildList { 
-                // continue straight
-                val distance = when (state.straights) {
-                    0 -> 4
-                    in 4..9 -> 1
-                    else -> 0
-                }
-                if (distance != 0) {
-                    move(state.pos, state.dir, distance)?.let { (pos, weight) ->
-                        val newState = State(pos, state.dir, state.straights + distance)
-                        add(Graph.Edge(newState, weight))
-                    }
-                }
-                
-                // go left
-                val leftDir = state.dir.left()
-                move(state.pos, leftDir, 4)?.let { (pos, weight) ->
-                    val newState = State(pos, leftDir, 4)
-                    add(Graph.Edge(newState, weight))
-                }
-
-                // go right
-                val rightDir = state.dir.right()
-                move(state.pos, rightDir, 4)?.let { (pos, weight) ->
-                    val newState = State(pos, rightDir, 4)
-                    add(Graph.Edge(newState, weight))
+        // Utility function for getEdges.
+        // Generates next move in a particular direction, or null if the move goes out of range of the city Grid.
+        fun moveOrNull(state: State, turn: Nsew.() -> Nsew, distance: Int): Graph.Edge<State>? {
+            val turnDir = state.dir.turn()
+            return if (distance == 0) null else {
+                (1..distance).fold(state.pos to 0.0) { (currentPos, weight), _ ->
+                    val nextPos = currentPos.move(turnDir)
+                    val newWeight = weight + (city.getOrNull(nextPos) ?: return null)
+                    nextPos to newWeight
+                }.let { (nextPos, newWeight) ->
+                    val straights = if (state.dir == turnDir) state.straights + distance else distance
+                    Graph.Edge(State(nextPos, turnDir, straights), newWeight)
                 }
             }
         }
-        val path = Graph.aStar(start, heuristic, defaultEdges = getEdges)
-        return path.steps()
-    }
 
+        // Get edges for any given state.
+        val getEdges = { state: State ->
+            buildList { 
+                
+                // go straight
+                // we can only continue straight if we haven't gone 10 times already. Also, when we start we 
+                // haven't moved at all so we are permitted to move 4 in that direction.
+                val distance = when (state.straights) {
+                    0 -> 4 // this only happens at start
+                    in 4..9 -> 1 // we have already turned and gone 4 in that direction, so add 1 up to 10
+                    else -> 0 // we have already moved 10 times; go no further
+                }
+                moveOrNull(state, { this }, distance)?.let { add(it) }
+                
+                // go left and right
+                moveOrNull(state, Nsew::left, 4)?.let { add(it) }
+                moveOrNull(state, Nsew::right, 4)?.let { add(it) }
+            }
+        }
+
+        // Run A* with the edgefinder and return the shortest number of steps.
+        return Graph.aStar(start, heuristic, defaultEdges = getEdges).steps()
+    }
 }
-//fun main() = Day.runDay(Y2023D17::class, sampleInput[1])
+
 fun main() = Day.runDay(Y2023D17::class)
 
 //    Class creation: 14ms
