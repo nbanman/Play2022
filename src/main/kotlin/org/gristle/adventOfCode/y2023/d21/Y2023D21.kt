89cef5eab497e44042c1003325e97dbdf743d6f6
Neil Banman <neil.banman@gmail.com>
2023-12-22 16:06:29 -0800

    changed bfs to work on raw string rather than Grid for that sweet 10ms boost

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d21/Y2023D21.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d21/Y2023D21.kt
@@ -1,29 +1,28 @@
 package org.gristle.adventOfCode.y2023.d21
 
 import org.gristle.adventOfCode.Day
-import org.gristle.adventOfCode.utilities.*
+import org.gristle.adventOfCode.utilities.isEven
+import org.gristle.adventOfCode.utilities.isOdd
+import org.gristle.adventOfCode.utilities.pow
 
 class Y2023D21(private val input: String) : Day {
 
-    private val gardenWidth = input.indexOf('\n')
-    
+    private val width = input.indexOf('\n')
+
     private val gardenPath = let {
-        val garden = input.toGrid()
-        val start = garden.coordOf(garden.indexOf('S'))
-        val q = ArrayDeque<Pair<Coord, Int>>()
+        val garden = input
+        val start = garden.indexOf('S')
+        val q = ArrayDeque<Pair<Int, Int>>()
             .apply { add(start to 0) }
-        val visited = BooleanArray(garden.size)
-            .apply { this[garden.indexOf(start)] = true }
+        val visited = BooleanArray(garden.length)
+            .apply { this[start] = true }
 
         generateSequence { q.removeFirstOrNull() }
             .onEach { (pos, steps) ->
-                val neighbors = listOf(pos.north(), pos.east(), pos.west(), pos.south())
+                val neighbors = listOf(pos - (width + 1), pos + 1, pos - 1, pos + width + 1)
                     .filter { neighbor ->
-                        (garden.getOrNull(neighbor)?.let { it != '#'} == true
-                                && !visited[garden.indexOf(neighbor)])
-                            .also {
-                                garden.indexOfOrNull(neighbor)?.let { visited[it] = true }
-                            }
+                        (garden.getOrNull(neighbor)?.let { it !in "#\n"} == true
+                                && !visited[neighbor].also { visited[neighbor] = true })
                     }.map { neighbor -> neighbor to steps + 1 }
                 q.addAll(neighbors)
             }.map { (_, steps) -> steps }
@@ -42,7 +41,7 @@ class Y2023D21(private val input: String) : Day {
         val evenCorners = evenPath.count { it > 65 }
         val oddCorners = oddPath.count { it > 65 && it.isOdd() }
 
-        val n = (26501365 - gardenWidth / 2) / gardenWidth // number of extra squares in a given direction
+        val n = (26501365 - width / 2) / width // number of extra squares in a given direction
 
         return (n + 1).pow(2) * oddPath.size + n.pow(2) * evenPath.size - (n + 1) * oddCorners + n * evenCorners
     }
@@ -50,10 +49,10 @@ class Y2023D21(private val input: String) : Day {
 
 fun main() = Day.runDay(Y2023D21::class)
 
-//    Class creation: 55ms
+//    Class creation: 27ms
 //    Part 1: 3782 (2ms)
-//    Part 2: 630661863455116 (6ms)
-//    Total time: 64ms
+//    Part 2: 630661863455116 (5ms)
+//    Total time: 35ms
 
 
 @Suppress("unused")
