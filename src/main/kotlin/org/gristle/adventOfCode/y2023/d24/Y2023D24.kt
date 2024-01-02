969217213716a4962b2b322b8db29319fd478376
Neil Banman <neil.banman@gmail.com>
2024-01-02 15:16:28 -0800

    initial. ripped off Roman Elizarov's solution at https://github.com/elizarov/AdventOfCode2023/blob/main/src/Day24_2_golf.kt
    Did manage 2x speedup by early return on time calculation.

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d24/Y2023D24.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d24/Y2023D24.kt
@@ -1,19 +1,102 @@
 package org.gristle.adventOfCode.y2023.d24
 
 import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.utilities.getLongs
+import org.gristle.adventOfCode.utilities.getNumbers
+import org.gristle.adventOfCode.utilities.getPairs
+import kotlin.math.abs
 
-class Y2023D24(input: String) : Day {
+class Y2023D24(private val input: String) : Day {
+    
+    data class Point3d(val x: Double, val y: Double, val z: Double)
 
-    private val parsed = input.lines()
+    data class Stone(val pos: Point3d, val vel: Point3d) {
+        fun intersects(other: Stone, testArea: ClosedFloatingPointRange<Double>): Boolean {
+            // get time for original stone at intersection point
+            val t = (other.pos.y + (pos.x - other.pos.x) * other.vel.y / other.vel.x - pos.y) / 
+                    (vel.y - vel.x * other.vel.y / other.vel.x)
+            if (t < 0.0) return false // intersected in past
+            
+            // get time for comparison stone at intersection point
+            val s = (t * vel.x + pos.x - other.pos.x) / other.vel.x
+            if (s < 0.0) return false // intersected in past
+            
+            // get x and y for intersection point and report whether they are both within the test area 
+            val x = t * vel.x + pos.x
+            val y = t * vel.y + pos.y
+            
+            return x in testArea && y in testArea
+        }
+    }
 
-    override fun part1() = "To be implemented"
+    private val testArea = 200000000000000.0..400000000000000.0
+    
+    // H/T Brian Norman for walking through the math. 
+    // https://github.com/bnorm/advent-of-code/blob/main/year2023/src/aoc/day24/main.kt#L33-L101
+    override fun part1(): Int {
+        val stones = input
+            .getNumbers { toDoubleOrNull() }
+            .chunked(3) { (x, y, z) -> Point3d(x, y, z) }
+            .chunked(2) { (pos, vel) -> Stone(pos, vel) }
+            .toList()
+        return stones.getPairs().count { (a, b) -> a.intersects(b, testArea) } 
+    } 
 
-    override fun part2() = "To be implemented"
+    // This was well beyond my abilities, so I had to look at others' solutions. Roman Elizarov's was the easiest for 
+    // me to understand, so I replicate it here in commented, non-golfed fashion.
+    // His version relies on the fact that on at least one axis the rock has the same position and velocity as one of
+    // the hailstones. This way the bruteforce aspect is kept to a minimum.
+    // https://github.com/elizarov/AdventOfCode2023/blob/main/src/Day24_2_golf.kt
+    override fun part2(): Long {
+        val stones: List<List<List<Long>>> = input.getLongs()
+            .chunked(3)
+            .chunked(2)
+            .toList()
+        
+        // Get the t
+        val times: List<Long> = (0..2).firstNotNullOf { axis ->
+            stones.firstNotNullOfOrNull { stone ->
+                stones.map { other ->
+                    // if the two stones have the same velocity on the tested axis, then further consider whether they
+                    // have the same position on the tested axis. If so, they meet immediately at time = 0. 
+                    // If not, they are parallel, and it's not a valid position.
+                    if (stone[1][axis] == other[1][axis]) {
+                        if (stone[0][axis] == other[0][axis]) 0L else return@firstNotNullOfOrNull null
+                    } else { // if the stones have a different velocity...
+                        // time is an integer (why?), so discard any that don't divide cleanly, which means it will
+                        // try the next stone.
+                        val diffPos = abs(other[0][axis] - stone[0][axis]) // compute difference in initial positions
+                        val diffVel = abs(other[1][axis] - stone[1][axis]) // compute difference in velocities
+                        if (diffPos % diffVel == 0L) diffPos / diffVel else return@firstNotNullOfOrNull null
+                    }
+                }
+            }
+        } 
+        
+        // get the indices of the first two hailstones that do not travel parallel  
+        val (i, j) = times.withIndex()
+            .filter { (_, time) -> time > 0 }
+            .map { it.index }
+        
+        // given a stone, an axis, and a time, calculate the position of the stone on that axis at that time
+        fun rockPos(i: Int, axis: Int, time: Long): Long = stones[i][0][axis] + stones[i][1][axis] * time
+        
+        // for each axis, calculate the position at time 0, and sum it.
+        return (0..2).sumOf { axis ->
+            val iPos = rockPos(i, axis, times[i])
+            iPos - (iPos - rockPos(j, axis, times[j])) / (times[i] - times[j]) * times[i]
+        }
+    }
 }
 
-fun main() = Day.runDay(Y2023D24::class)
+fun main() = Day.runDay(Y2023D24::class) // 808107741406756
 
 @Suppress("unused")
 private val sampleInput = listOf(
-    """""",
+    """19, 13, 30 @ -2,  1, -2
+18, 19, 22 @ -1, -1, -2
+20, 25, 34 @ -2, -2, -4
+12, 31, 28 @ -1, -2, -1
+20, 19, 15 @  1, -5, -3
+""",
 )
