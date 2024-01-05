8a2dcc58ec28c1a94afb4add33c7cdf3ac88a5f6
Neil Banman <neil.banman@gmail.com>
2024-01-04 23:28:39 -0800

    ~50% speedup by removing no-longer-needed DeepRecursiveFunction, plus using bitset for state

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d23/Y2023D23.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d23/Y2023D23.kt
@@ -9,7 +9,6 @@ class Y2023D23(private val trails: String) : Day {
     private val start = trails.indexOf('.')
     private val end = trails.lastIndexOf('.')
 
-    // get weighted vertices
     private val vertices: Set<Int> = buildSet {
         val branchPoints = trails
             .withIndex()
@@ -22,41 +21,10 @@ class Y2023D23(private val trails: String) : Day {
         add(end)
     }
 
-    private val edgeMap: Map<Int, List<Pair<Int, Int>>> = buildMap<Int, MutableList<Pair<Int, Int>>> {
-        // get weighted vertices
-        vertices.forEach { pos ->
-            connectVertex(pos, vertices, false).forEach { (neighbor, dist) ->
-                getOrPut(pos) { mutableListOf() }.add(neighbor to dist)
-            }
-        }
-    }
-    
     private val vertexMap = vertices.withIndex().associate { (index, pos) -> pos to index }
-    
-    private fun findLongestTrail(
-        edgeMap: Map<Int, List<Pair<Int, Int>>>,
-        start: Int,
-        end: Int,
-    ): Int {
-        val visited = mutableSetOf<Int>()
-        val longestTrail = DeepRecursiveFunction<Pair<Int, Int>, Int> { (pos, weight) ->
-            if (pos == end) {
-                weight
-            } else {
-                visited.add(pos)
-                val neighbors = edgeMap.getValue(pos) 
-                    .filter { (neighbor, _) -> neighbor !in visited }
-                    .map { (neighbor, newWeight) -> neighbor to weight + newWeight }
-                val maxDistance = neighbors
-                    .maxOfOrNull { state ->
-                        callRecursive(state)
-                    } ?: 0
-                visited.remove(pos)
-                maxDistance
-            }
-        }
-        return longestTrail(start to 0)
-    }
+
+    private val newStart = vertexMap.getValue(start)
+    private val newEnd = vertexMap.getValue(end)
 
     private fun connectVertex(pos: Int, vertices: Set<Int>, canGoUphill: Boolean): List<Pair<Int, Int>> {
         val visited = mutableSetOf<Int>()
@@ -64,7 +32,7 @@ class Y2023D23(private val trails: String) : Day {
         return generateSequence { q.removeLastOrNull() }
             .onEach { (current, dist) ->
                 if (current !in vertices || current == pos) {
-                    val neighbors = 
+                    val neighbors =
                         if (canGoUphill) {
                             listOf(current - (width + 1), current + 1, current - 1, current + (width + 1))
                         } else {
@@ -77,7 +45,7 @@ class Y2023D23(private val trails: String) : Day {
                                 else -> listOf(current - (width + 1), current + 1, current - 1, current + (width + 1))
                             }
                         }.filter { neighbor ->
-                            trails.getOrNull(neighbor)?.let { it !in "#\n" } == true && neighbor !in visited 
+                            trails.getOrNull(neighbor)?.let { it !in "#\n" } == true && neighbor !in visited
                         }
                     neighbors.forEach { visited.add(it) }
                     q.addAll(neighbors.map { it to dist + 1 })
@@ -86,10 +54,47 @@ class Y2023D23(private val trails: String) : Day {
             .toList()
     }
 
-    override fun part1(): Int {
-        return findLongestTrail(edgeMap, start, end)
+    private data class State(val pos: Int, val weight: Int, val visited: Long)
+
+    private fun findLongestTrail(
+        edgeMap: Map<Int, List<Pair<Int, Int>>>,
+        start: Int,
+        end: Int,
+    ): Int {
+        fun longestTrail(state: State): Int {
+            val (pos, weight, visit) = state
+            return if (pos == end) {
+                weight
+            } else {
+                val newVisit = visit + (1L shl pos)
+                val neighbors = edgeMap.getValue(pos)
+                    .filter { (neighbor, _) ->
+                        newVisit shr neighbor and 1L == 0L
+                    }.map { (neighbor, newWeight) -> State(neighbor, weight + newWeight, newVisit) }
+                val maxDistance = neighbors
+                    .maxOfOrNull { neighborState ->
+                        longestTrail(neighborState)
+                    } ?: 0
+                maxDistance
+            }
+        }
+        return longestTrail(State(start, 0, 0L))
     }
-    
+
+    override fun part1(): Int {
+        val edgeMap: Map<Int, List<Pair<Int, Int>>> = buildMap<Int, MutableList<Pair<Int, Int>>> {
+            // get weighted vertices
+            vertices.forEach { pos ->
+                val pp = vertexMap.getValue(pos)
+                connectVertex(pos, vertices, false).forEach { (neighbor, dist) ->
+                    getOrPut(pp) { mutableListOf() }.add(vertexMap.getValue(neighbor) to dist)
+                }
+            }
+        }
+
+        return findLongestTrail(edgeMap, newStart, newEnd)
+    }
+
     override fun part2(): Int {
         val initial: Map<Int, List<Pair<Int, Int>>> = buildMap<Int, MutableList<Pair<Int, Int>>> {
             // get weighted vertices
@@ -101,8 +106,7 @@ class Y2023D23(private val trails: String) : Day {
                     }
             }
         }
-        val newStart = vertexMap.getValue(start)
-        val newEnd = vertexMap.getValue(end)
+
         val verboten: Map<Int, Int> = Graph
             .bfsSequence(newStart) {
                 initial.getValue(it).filter { (neighbor, _) ->
@@ -111,17 +115,17 @@ class Y2023D23(private val trails: String) : Day {
             }.mapNotNull { v -> v.parent?.let { parent -> v.id to parent.id } }
             .toMap()
         val edgeMap = initial.entries.associate { (k, v) -> k to v.filter { (neighbor) -> verboten[k] != neighbor } }
-        
+
         return findLongestTrail(edgeMap, newStart, newEnd)
-    } 
+    }
 }
 
 fun main() = Day.runDay(Y2023D23::class)
 
-//    Class creation: 24ms
-//    Part 1: 2210 (34ms)
-//    Part 2: 6522 (5634ms)
-//    Total time: 5693ms
+//    Class creation: 35ms
+//    Part 1: 2210 (31ms)
+//    Part 2: 6522 (887ms)
+//    Total time: 954ms
 
 @Suppress("unused")
 private val sampleInput = listOf(
