922782d63fd2492bd23d12b381fd8e0f3a60f2f9
Neil Banman <neil.banman@gmail.com>
2024-01-05 10:12:20 -0800

    initial

---
+++ src/test/kotlin/org/gristle/adventOfCode/utilities/Test2023.kt
@@ -0,0 +1,151 @@
+package org.gristle.adventOfCode.utilities
+
+import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.y2023.d1.Y2023D1
+import org.gristle.adventOfCode.y2023.d10.Y2023D10
+import org.gristle.adventOfCode.y2023.d11.Y2023D11
+import org.gristle.adventOfCode.y2023.d12.Y2023D12
+import org.gristle.adventOfCode.y2023.d13.Y2023D13
+import org.gristle.adventOfCode.y2023.d14.Y2023D14
+import org.gristle.adventOfCode.y2023.d15.Y2023D15
+import org.gristle.adventOfCode.y2023.d17.Y2023D17
+import org.gristle.adventOfCode.y2023.d18.Y2023D18
+import org.gristle.adventOfCode.y2023.d19.Y2023D19
+import org.gristle.adventOfCode.y2023.d2.Y2023D2
+import org.gristle.adventOfCode.y2023.d20.Y2023D20
+import org.gristle.adventOfCode.y2023.d21.Y2023D21
+import org.gristle.adventOfCode.y2023.d22.Y2023D22
+import org.gristle.adventOfCode.y2023.d23.Y2023D23
+import org.gristle.adventOfCode.y2023.d24.Y2023D24
+import org.gristle.adventOfCode.y2023.d25.Y2023D25
+import org.gristle.adventOfCode.y2023.d3.Y2023D3
+import org.gristle.adventOfCode.y2023.d4.Y2023D4
+import org.gristle.adventOfCode.y2023.d5.Y2023D5
+import org.gristle.adventOfCode.y2023.d6.Y2023D6
+import org.gristle.adventOfCode.y2023.d7.Y2023D7
+import org.gristle.adventOfCode.y2023.d8.Y2023D8
+import org.gristle.adventOfCode.y2023.d9.Y2023D9
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Test2023 {
+    @Test
+    internal fun y2023d1() {
+        assertEquals("54388" to "53515", Day.testDay(Y2023D1::class))
+    }
+
+    @Test
+    internal fun y2023d2() {
+        assertEquals("2377" to "71220", Day.testDay(Y2023D2::class))
+    }
+
+    @Test
+    internal fun y2023d3() {
+        assertEquals("525911" to "75805607", Day.testDay(Y2023D3::class))
+    }
+
+    @Test
+    internal fun y2023d4() {
+        assertEquals("23750" to "13261850", Day.testDay(Y2023D4::class))
+    }
+
+    @Test
+    internal fun y2023d5() {
+        assertEquals("379811651" to "27992443", Day.testDay(Y2023D5::class))
+    }
+
+    @Test
+    internal fun y2023d6() {
+        assertEquals("2374848" to "39132886", Day.testDay(Y2023D6::class))
+    }
+
+    @Test
+    internal fun y2023d7() {
+        assertEquals("253866470" to "254494947", Day.testDay(Y2023D7::class))
+    }
+
+    @Test
+    internal fun y2023d8() {
+        assertEquals("19241" to "9606140307013", Day.testDay(Y2023D8::class))
+    }
+
+    @Test
+    internal fun y2023d9() {
+        assertEquals("1974913025" to "884", Day.testDay(Y2023D9::class))
+    }
+
+    @Test
+    internal fun y2023d10() {
+        assertEquals("7086" to "317", Day.testDay(Y2023D10::class))
+    }
+
+    @Test
+    internal fun y2023d11() {
+        assertEquals("9545480" to "406725732046", Day.testDay(Y2023D11::class))
+    }
+
+    @Test
+    internal fun y2023d12() {
+        assertEquals("7344" to "1088006519007", Day.testDay(Y2023D12::class))
+    }
+
+    @Test
+    internal fun y2023d13() {
+        assertEquals("27505" to "22906", Day.testDay(Y2023D13::class))
+    }
+
+    @Test
+    internal fun y2023d14() {
+        assertEquals("106990" to "100531", Day.testDay(Y2023D14::class))
+    }
+
+    @Test
+    internal fun y2023d15() {
+        assertEquals("505427" to "243747", Day.testDay(Y2023D15::class))
+    }
+
+    @Test
+    internal fun y2023d17() {
+        assertEquals("635" to "734", Day.testDay(Y2023D17::class))
+    }
+
+    @Test
+    internal fun y2023d18() {
+        assertEquals("50746" to "70086216556038", Day.testDay(Y2023D18::class))
+    }
+
+    @Test
+    internal fun y2023d19() {
+        assertEquals("449531" to "122756210763577", Day.testDay(Y2023D19::class))
+    }
+
+    @Test
+    internal fun y2023d20() {
+        assertEquals("938065580" to "250628960065793", Day.testDay(Y2023D20::class))
+    }
+
+    @Test
+    internal fun y2023d21() {
+        assertEquals("3782" to "630661863455116", Day.testDay(Y2023D21::class))
+    }
+
+    @Test
+    internal fun y2023d22() {
+        assertEquals("446" to "60287", Day.testDay(Y2023D22::class))
+    }
+
+    @Test
+    internal fun y2023d23() {
+        assertEquals("2210" to "6522", Day.testDay(Y2023D23::class))
+    }
+
+    @Test
+    internal fun y2023d24() {
+        assertEquals("14046" to "808107741406756", Day.testDay(Y2023D24::class))
+    }
+
+    @Test
+    internal fun y2023d25() {
+        assertEquals("569904" to "skipped", Day.testDay(Y2023D25::class))
+    }
+}
