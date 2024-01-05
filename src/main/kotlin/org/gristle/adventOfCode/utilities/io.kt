89da77f8641b0539d7a00a17643b2cda9fb6feb6
Neil Banman <neil.banman@gmail.com>
2024-01-05 11:12:43 -0800

    changed getInput to only trim line breaks at the end

--- src/main/kotlin/org/gristle/adventOfCode/utilities/io.kt
+++ src/main/kotlin/org/gristle/adventOfCode/utilities/io.kt
@@ -65,5 +65,33 @@ fun getInput(day: Int, year: Int = LocalDate.now().year, alternate: String = "")
             connection.disconnect()
         }
     }
-    return inputFile.readText().stripCarriageReturns().trimEnd()
+    return inputFile.readText().stripCarriageReturns().trimEnd { it == '\n' }
+}
+
+fun printColored(text: String, fg: AnsiColor? = null, bg: AnsiColor? = null) {
+    val fgCode = fg?.let { "${it.fg}" } ?: ""
+    val bgCode = bg?.let { "${it.bg}" } ?: ""
+    val sep = if (fgCode.isNotBlank() && bgCode.isNotBlank()) ";" else ""
+    print("\u001b[${fgCode}${sep}${bgCode}m$text\u001B[0m")
+}
+
+@Suppress("unused")
+enum class AnsiColor(val fg: Int, val bg: Int) {
+    BLACK(30, 40),
+    RED(31, 41),
+    GREEN(32, 42),
+    YELLOW(33, 43),
+    BLUE(34, 44),
+    MAGENTA(35, 45),
+    CYAN(36, 46),
+    WHITE(37, 47),
+
+    BRIGHT_BLACK(90, 100),
+    BRIGHT_RED(91, 101),
+    BRIGHT_GREEN(92, 102),
+    BRIGHT_YELLOW(93, 103),
+    BRIGHT_BLUE(94, 104),
+    BRIGHT_MAGENTA(95, 105),
+    BRIGHT_CYAN(96, 106),
+    BRIGHT_WHITE(97, 107),
 }
