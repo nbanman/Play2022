accfe936c8477fca43163feb681191d0b5d79b18
Neil Banman <neil.banman@gmail.com>
2024-01-18 14:44:15 -0800

    now recognizes a char as a joker rather than relying on a string replacement for a new character

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d7/Y2023D7.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d7/Y2023D7.kt
@@ -3,37 +3,35 @@ package org.gristle.adventOfCode.y2023.d7
 import org.gristle.adventOfCode.Day
 
 class Y2023D7(private val input: String) : Day {
-    
     // takes the hands, sorts by the hand strength as defined by each puzzle part, assigns points using rank and
     // bid amount, then returns sum of all points
-    private inline fun solve(crossinline swapJacksWithJokers: (String) -> String = { it }) = input
+    private fun solve(jokers: Char = '-') = input
         .lines()
         .map { line ->
             val (cards, bid) = line.split(' ')
-            Hand(swapJacksWithJokers(cards), bid.toInt())
+            Hand(cards, bid.toInt(), jokers)
         }.sorted()
         .mapIndexed { index, hand -> (index + 1) * hand.bid }
         .sum()
     
     override fun part1() = solve()
 
-    // Part 2 is the same thing, except 'J' cards are replaced by 'ĵ' cards, which are recognized as jokers and
-    // are dealt with appropriately in the Hand class logic.
-    override fun part2() = solve { it.replace('J', 'ĵ') }
+    // Part 2 is the same thing, except 'J' cards are identified as jokers and are dealt with appropriately in the 
+    // Hand class logic.
+    override fun part2() = solve(jokers = 'J')
     
-    data class Hand(val cards: String, val bid: Int): Comparable<Hand> {
-
+    data class Hand(val cards: String, val bid: Int, val jokers: Char): Comparable<Hand> {
         // strength is an Int that we use to sort with. The most important component of strength  is the strength 
         // of the type of hand. After that, the value of the cards, in order. E.g., 98 > 8A, because 9 is greater 
         // than 8. We can represent this all as an Int concatenating them all, giving 4 bits for each value.
         private val strength: Int = let {
             // number of jokers gets added to the most numerous of the other cards to make the most powerful hand
-            val jokers = cards.count { it == 'ĵ' }
+            val numberOfJokers = cards.count { it == jokers }
 
             // groups cards together, for use in determining handTypeStrength. Sorted because the relative size 
             // of the groups is used to determine what kind of hand we have. Then take the two most populous groups 
             // in the hand, then deliver ordered ranking of each hand type.
-            val groups: List<Int> = cards.filter { it != 'ĵ' }
+            val groups: List<Int> = cards.filter { it != jokers }
                 .groupingBy { it }
                 .eachCount()        // count the number of each card we have
                 .values             // we only care about biggest group size, not contents of group
@@ -41,16 +39,18 @@ class Y2023D7(private val input: String) : Day {
 
             // this gives a strength, from weakest to strongest, of 3, 5, 6, 7, 8, 9, 10
             // we use getOrElse for the second group because some combinations have no second group
-            val handTypeStrength = (groups.getOrElse(0) { 0 } + jokers) * 2 + groups.getOrElse(1) { 0 }
+            val handTypeStrength = 
+                (groups.getOrElse(0) { 0 } + numberOfJokers) * 2 + groups.getOrElse(1) { 0 }
 
             // calculate overall strength by building an int with bitshifts
-            cards.fold(handTypeStrength) { acc, card -> (acc shl 4) + CARD_ORDER.indexOf(card) }
+            fun Char.value() = if (this == jokers) 0 else CARD_ORDER.indexOf(this) + 1
+            cards.fold(handTypeStrength) { acc, card -> (acc shl 4) + card.value() }
         }
 
         override fun compareTo(other: Hand): Int = this.strength - other.strength
 
         companion object {
-            private const val CARD_ORDER = "ĵ23456789TJQKA"
+            private const val CARD_ORDER = "23456789TJQKA"
         }
     }
 }
