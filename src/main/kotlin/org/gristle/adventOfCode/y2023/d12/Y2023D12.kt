d11ba97f038bbc3ceff717df33094af0a1a51848
Neil Banman <neil.banman@gmail.com>
2024-02-02 12:19:18 -0800

    cleanup

--- src/main/kotlin/org/gristle/adventOfCode/y2023/d12/Y2023D12.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2023/d12/Y2023D12.kt
@@ -4,31 +4,35 @@ import org.gristle.adventOfCode.Day
 
 class Y2023D12(input: String) : Day {
 
-    data class State(val conditionsIndex: Int, val damageReportIndex: Int)
-
     data class SpringRow(val conditions: String, val damageReport: List<Int>) {
+        // expands the SpringRow in accordance with Part 2 rules
+        fun expand() = SpringRow(
+            List(5) { conditions }.joinToString("?"),
+            List(5) { damageReport }.flatten()
+        )
+        
         // cache used for DP function 'arrangements,' below. 
-        private val cache = mutableMapOf<State, Long>()
+        private val cache = mutableMapOf<Pair<Int, Int>, Long>()
         
         // DP function that uses state objects to delve deeper and deeper into the string using DFS, until a base case 
         // is found. Cache is updated along the way, so that subsequent dives down the string become instant as soon as
         // there is any repetition of remaining state.
-        fun arrangements(s: State = State(0, 0)): Long = cache.getOrPut(s) {
-            // the # of consecutive broken springs in the damage report that we will try to place along the row. If 
-            // every broken spring in the damage report has been assigned, return 0.
-            val fulfillment = damageReport.getOrNull(s.damageReportIndex) ?: 0
+        fun arrangements(state: Pair<Int, Int> = 0 to 0): Long = cache.getOrPut(state) {
 
+            val (conditionsIndex, damageReportIndex) = state
+            
             // Base case. Takes states that have assigned broken springs corresponding to the entire damage report and 
             // returns 1 if valid, 0 if invalid. Valid states are those with no remaining '#' in the string.
-            if (fulfillment == 0) {
-                val damagedSpringRemaining = (s.conditionsIndex..conditions.lastIndex).any { conditions[it] == '#' }
+            if (damageReportIndex == damageReport.size) {
+                val damagedSpringRemaining = (conditionsIndex..conditions.lastIndex).any { conditions[it] == '#' }
                 return@getOrPut if (damagedSpringRemaining) 0 else 1
             }
             
-            // Otherwise, we go recursive by trying to fit the fulfillment in every place along the block
-            // This starts as a sequence of indexes, from 0 until the length of the block minus the fulfillment size
-            // (to account for the size of the fulfillment itself in the string).
-            (s.conditionsIndex..conditions.length - fulfillment)
+            // Otherwise, we go recursive by trying to fit the next group of broken springs in every place along the 
+            // block. This starts as a sequence of indexes, from 0 until the length of the block minus the fulfillment 
+            // size (to account for the size of the fulfillment itself in the string).
+            val brokenGroup = damageReport[damageReportIndex]
+            (conditionsIndex..conditions.length - brokenGroup)
                 .asSequence()
                 // stop the sequence if a '#' precedes the index b/c '#' cannot be skipped
                 .takeWhile { index -> conditions.getOrNull(index - 1) != '#' }
@@ -39,39 +43,25 @@ class Y2023D12(input: String) : Day {
                     // if the character following the placement is '#', invalid b/c that extra '#' would overfulfill
                     // otherwise valid
                     when {
-                        '.' in conditions.substring(index, index + fulfillment) -> false
-                        index + fulfillment == conditions.length -> true
-                        conditions[index + fulfillment] == '#' -> false
+                        '.' in conditions.substring(index, index + brokenGroup) -> false
+                        index + brokenGroup == conditions.length -> true
+                        conditions[index + brokenGroup] == '#' -> false
                         else -> true
                     }
-                }.sumOf { index ->
-                    val newState = State(
-                        index + fulfillment + 1,
-                        s.damageReportIndex + 1
-                    )
-                    arrangements(newState)
-                }
+                }.sumOf { index -> arrangements(index + brokenGroup + 1 to damageReportIndex + 1) }
         }
     }
 
-    // intermediate parsing step. Each part parses the reports differently.
-    private val springReports: List<Pair<String, List<Int>>> = input.lines().map { line ->
+    // parsing input to SpringRows
+    private val springRows: List<SpringRow> = input.lines().map { line ->
         val (conditions, damageReportStr) = line.split(' ')
         val damageReport: List<Int> = damageReportStr.split(',').map(String::toInt)
-        conditions to damageReport
+        SpringRow(conditions, damageReport)
     }
-    
-    // parses the reports to rows, then sums the number of arrangements possible in each row
-    private inline fun solve(springRow: (String, List<Int>) -> SpringRow): Long = springReports
-        .sumOf { (conditions, damageReport) -> springRow(conditions, damageReport).arrangements() }
 
-    override fun part1(): Long = solve { conditions, damageReport -> SpringRow(conditions, damageReport) } 
+    override fun part1(): Long = springRows.sumOf { it.arrangements() }
     
-    override fun part2(): Long = solve { conditions, damageReport ->
-        val expandedConditions = List(5) { conditions }.joinToString("?")
-        val expandedDamageReport = List(5) { damageReport }.flatten()
-        SpringRow(expandedConditions, expandedDamageReport)
-    }
+    override fun part2(): Long = springRows.sumOf { it.expand().arrangements() }
 }
 
 fun main() = Day.runDay(Y2023D12::class)
