a3b66eda6c5bac4d53f3a6c4b5341a96149afc2e
Neil Banman <neil.banman@gmail.com>
2024-06-06 15:34:22 -0700

    ~6x speedup using Ross McArthur's approach, using an IntArray for most numbers and a HashMap for high numbers.

--- src/main/kotlin/org/gristle/adventOfCode/y2020/d15/Y2020D15.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2020/d15/Y2020D15.kt
@@ -6,40 +6,30 @@ class Y2020D15(input: String) : Day {
 
     val start = input.split(',').map(String::toInt)
 
-    data class Spokenses(val first: Int? = null, val last: Int? = null) {
-
-        companion object {
-            val DEFAULT = Spokenses()
-        }
-
-        fun update(n: Int): Spokenses {
-            return when {
-                first == null -> Spokenses(n, null)
-                last == null -> Spokenses(first, n)
-                else -> Spokenses(last, n)
-            }
-        }
-    }
-
     private fun lastNumberSpoken(iterations: Int): Int {
 
-        val log = start
-            .mapIndexed { index, i -> i to Spokenses(index + 1) }
-            .toTypedArray()
-            .let { mutableMapOf(*it) }
-
-        var lastNumberSpoken = start.last()
-
-        for (turn in (start.size + 1)..iterations) {
-            val spoke = log.getValue(lastNumberSpoken)
-            lastNumberSpoken = if (spoke.last == null || spoke.first == null) {
-                0
-            } else {
-                spoke.last - spoke.first
-            }
-            log[lastNumberSpoken] = log.getOrDefault(lastNumberSpoken, Spokenses.DEFAULT).update(turn)
+        val bounds = iterations / 10
+        
+        val low = IntArray(bounds)
+        val high: MutableMap<Int, Int> = HashMap(bounds / 2)
+        
+        for ((turn, n) in start.withIndex()) {
+            low[n] = turn + 1
         }
-        return lastNumberSpoken
+        
+        var current = 0
+        for (turn in start.size + 1 until iterations) {
+            if (current < bounds) {
+                val prev = low[current]
+                low[current] = turn
+                current = if (prev == 0) 0 else turn - prev
+            } else {
+                current = high.put(current, turn)
+                    ?.let { prev -> turn - prev }
+                    ?: 0
+            }
+        }
+        return current
     }
 
     override fun part1() = lastNumberSpoken(2020)
@@ -49,7 +39,7 @@ class Y2020D15(input: String) : Day {
 
 fun main() = Day.runDay(Y2020D15::class)
 
-//    Class creation: 17ms
-//    Part 1: 929 (2ms)
-//    Part 2: 16671510 (5581ms)
-//    Total time: 5601ms
+//    Class creation: 3ms
+//    Part 1: 929 (0ms)
+//    Part 2: 16671510 (878ms)
+//    Total time: 882ms
