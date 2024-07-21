4b21d39bf91b0613664c66760096d091e0ebb3fb
Neil Banman <neil.banman@gmail.com>
2024-07-21 12:57:18 -0700

    changed width to be consistent with Grid

--- src/main/kotlin/org/gristle/adventOfCode/utilities/StringGrid.kt
+++ src/main/kotlin/org/gristle/adventOfCode/utilities/StringGrid.kt
@@ -1,23 +1,33 @@
 package org.gristle.adventOfCode.utilities
 
 class StringGrid(val string: String) {
-    val width = string.indexOf('\n') + 1
-    val height = (string.length + if (string.last() == '\n') 0 else 1) / width
+    val width = string.indexOf('\n')
+    val height = (string.length + if (string.last() == '\n') 0 else 1) / (width + 1)
+    val xIndices = 0 until width
+    val yIndices = 0 until height
 
-    fun isValid(index: Int): Boolean = index in string.indices && (index + 1) % width != 0
+    fun isValid(index: Int): Boolean = index in string.indices && (index + 1) % (width + 1) != 0
     
     operator fun get(index: Int): Char = if (isValid(index)) string[index] else 
         throw IndexOutOfBoundsException("Index $index out of bounds.")
     
+    operator fun get(pos: Coord): Char {
+        val index = pos.y * (width + 1) + pos.x
+        return this[index]
+    }
+    
     fun getOrNull(index: Int): Char? = if (isValid(index)) string[index] else null
     
-    fun move(index: Int, direction: Nsew, distance: Int = 1): Int = moveOrNull(index, direction, distance)
-        ?: throw IndexOutOfBoundsException("Moved index out of bounds.")
+    fun move(index: Int, direction: Nsew, distance: Int = 1): Int {
+        val new = moveOrNull(index, direction, distance)
+        if (new != null) return new
+        throw IndexOutOfBoundsException("Index $index out of bounds.")
+    }
     
     fun moveOrNull(index: Int, direction: Nsew, distance: Int = 1): Int? {
         val movement = when (direction) {
-            Nsew.NORTH -> -(width * distance)
-            Nsew.SOUTH -> width * distance
+            Nsew.NORTH -> -(width + 1) * distance
+            Nsew.SOUTH -> (width + 1) * distance
             Nsew.EAST -> distance
             Nsew.WEST -> -distance
         }
