aec5e441ee0202c3fc91ccd380102af3125b6fe0
Neil Banman <neil.banman@gmail.com>
2024-10-04 12:32:25 -0700

    changed Room object creation

--- src/main/kotlin/org/gristle/adventOfCode/y2016/d4/Y2016D4.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2016/d4/Y2016D4.kt
@@ -6,26 +6,32 @@ import org.gristle.adventOfCode.utilities.groupValues
 
 class Y2016D4(input: String) : Day {
 
-    data class Room(val encryptedName: String, val id: Int, val checkSum: String) {
-        val isReal = checkSum == encryptedName
-            .replace("-", "") // don't count dashes
-            .eachCount() // deliver size of each group of characters
-            .entries // grab both key (the char) and the value (the size)
-            // sort the entries by size then alphabetical
-            .sortedWith(compareByDescending<Map.Entry<Char, Int>> { it.value }.thenBy { it.key })
-            .take(5) // take 1st 5
-            .map { it.key } // use only the char
-            .joinToString("") // join them to string
-
-        private fun Char.shift() = if (this == '-') ' ' else ((code - 97 + id) % 26 + 97).toChar()
-
-        val name: String by lazy { encryptedName.map { it.shift() }.joinToString("") }
+    class Room private constructor(val name: String, val id: Int) {
+        companion object {
+            fun from(encryptedName: String, id: Int, checkSum: String): Room? {
+                val isReal = checkSum == encryptedName
+                    .replace("-", "") // don't count dashes
+                    .eachCount() // deliver size of each group of characters
+                    .entries // grab both key (the char) and the value (the size)
+                    // sort the entries by size then alphabetical
+                    .sortedWith(compareByDescending<Map.Entry<Char, Int>> { it.value }.thenBy { it.key })
+                    .take(5) // take 1st 5
+                    .map { it.key } // use only the char
+                    .joinToString("") // join them to string
+                return if (isReal) {
+                    fun Char.shift() = if (this == '-') ' ' else ((code - 97 + id) % 26 + 97).toChar()
+                    val name = encryptedName.map { it.shift() }.joinToString("") 
+                    Room(name, id)
+                } else {
+                    null
+                }
+            }
+        }
     }
 
     private val rooms = input
         .groupValues("""([a-z-]+)-(\d+)\[([a-z]+)\]""")
-        .map { (encryptedName, idString, checkSum) -> Room(encryptedName, idString.toInt(), checkSum) }
-        .filter(Room::isReal)
+        .mapNotNull { (encryptedName, idString, checkSum) -> Room.from(encryptedName, idString.toInt(), checkSum) }
 
     override fun part1() = rooms.sumOf(Room::id)
 
@@ -34,7 +40,7 @@ class Y2016D4(input: String) : Day {
 
 fun main() = Day.runDay(Y2016D4::class)
 
-//    Class creation: 61ms
+//    Class creation: 42ms
 //    Part 1: 158835 (0ms)
-//    Part 2: 993 (2ms)
-//    Total time: 63ms
+//    Part 2: 993 (0ms)
+//    Total time: 42ms
