b14aaadf125fe5637b31f7fec7e9166884c8c613
neil <neil.banman@gmail.com>
2024-10-26 21:56:07 -0700

    light refactor

--- src/main/kotlin/org/gristle/adventOfCode/y2019/d1/Y2019D1.kt
+++ src/main/kotlin/org/gristle/adventOfCode/y2019/d1/Y2019D1.kt
@@ -1,32 +1,27 @@
 package org.gristle.adventOfCode.y2019.d1
 
 import org.gristle.adventOfCode.Day
+import org.gristle.adventOfCode.utilities.getIntList
 import kotlin.math.max
 
 class Y2019D1(input: String) : Day {
-    private val modules = input.lineSequence().map(String::toInt).toList()
-
-    private tailrec fun fuel(remaining: Int, total: Int = 0): Int {
+    private val modules = input.getIntList()
+    private fun baseFuel(weight: Int): Int = weight / 3 - 2
+    private tailrec fun totalFuel(remaining: Int, total: Int = 0): Int {
         return if (remaining == 0) {
             total
         } else {
-            val fuel = max(0, remaining / 3 - 2)
-            fuel(fuel, total + fuel)
+            val fuel = max(0, baseFuel(remaining))
+            totalFuel(fuel, total + fuel)
         }
     }
-
-    override fun part1() = modules.sumOf { it / 3 - 2 }
-
-    override fun part2() = modules.sumOf { fuel(it) }
-
+    override fun part1() = modules.sumOf(::baseFuel)
+    override fun part2() = modules.sumOf(::totalFuel)
 }
 
 fun main() = Day.runDay(Y2019D1::class)
-//    var time = System.nanoTime()
-//    val c = Y2019D1(readRawInput("y2019/d1"))
-//    println("Class creation: ${elapsedTime(time)}ms")
-//    time = System.nanoTime()
-//    println("Part 1: ${c.part1()} (${elapsedTime(time)}ms)") // 3325347
-//    time = System.nanoTime()
-//    println("Part 2: ${c.part2()} (${elapsedTime(time)}ms)") // 4985145
-//}
+
+//    Class creation: 2ms
+//    Part 1: 3325347 (0ms)
+//    Part 2: 4985145 (0ms)
+//    Total time: 2ms
